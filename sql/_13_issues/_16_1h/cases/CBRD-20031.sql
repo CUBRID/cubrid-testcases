b039c1537d75ffbcd671d4bba96717479e9b78e0
slamdunkorchid <zhanlanlan@navercorp.com>
2016-06-27 20:09:15 +0900

    [CBRD-20031] add test points

--- sql/_13_issues/_16_1h/cases/CBRD-20031.sql
+++ sql/_13_issues/_16_1h/cases/CBRD-20031.sql
@@ -6,4 +6,22 @@ insert into t value (concat(chr(255), chr(255)));
 prepare st from 'select count(*) from t where c like ?';
 execute st using '%';
 
+prepare st from 'select * from t where c like ? order by 1';
+execute st using '%';
 drop t;
+
+create table t(c char(255));
+insert into t value ('3');
+insert into t value ('');
+insert into t value (concat(chr(255), chr(255)));
+prepare st from 'select count(*) from t where c like ?';
+execute st using '%';
+drop t;
+create table t(c char(10));
+insert into t value ('3');
+insert into t value (chr(14909886));
+select count(*) from t where c like '%';
+prepare st from 'select count(*) from t where c like ?';
+execute st using '%';
+drop t;
+
