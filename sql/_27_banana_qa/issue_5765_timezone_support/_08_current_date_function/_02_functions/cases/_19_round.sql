8f78f49b39810a5b91937f1483f7c8ed2a2ee36d
cubridqa <dl_cubridqa_bj_internal@navercorp.com>
2016-07-01 01:14:00 +0900

    [CUBRIDQA-12] delete this test case, since there is no much meaning.

--- sql/_27_banana_qa/issue_5765_timezone_support/_08_current_date_function/_02_functions/cases/_19_round.sql
+++
@@ -1,8 +0,0 @@
---+ holdcas on;
-set timezone '+01:00';
-set @x1=(select round(current_date,'yy'));
-set timezone '+14:00';
-set @x2=(select round(current_date,'yy'));
-select if(timediff(@x1,@x2)='12:00:00 AM ','ok','nok');
-set timezone 'Asia/Seoul';
---+ holdcas off;
