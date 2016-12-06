cd7319a139b1fb2fcbff9d75a8cbcbfb42cff1ec
cubridqa <dl_cubridqa_bj_internal@navercorp.com>
2016-12-06 11:33:08 +0900

    [CUBRIDQA-12] add order by clause to make test case stable.

--- sql/_27_banana_qa/issue_11387_composed_index_scan/cases/qa_GE_order_by.sql
+++ sql/_27_banana_qa/issue_11387_composed_index_scan/cases/qa_GE_order_by.sql
@@ -28,56 +28,56 @@ insert into foo values ( 3, 3, 2147483647, null );
 insert into foo values ( 3, 3, 2147483647, '2013-10-10 12:12:12.111' );
 
 
-select * from foo where a = 1 and b >= 3 using index none;
-select * from foo where a = 1 and b >= 3 using index none;
-select * from foo where a = 1 and b >= 3 using index none;
+select * from foo where a = 1 and b >= 3 using index none order by b, c, d;
+select * from foo where a = 1 and b >= 3 using index none order by b, c, d;
+select * from foo where a = 1 and b >= 3 using index none order by b, c, d;
 
 create index idx1 on foo ( a , b , c );
-select * from foo where a = 1 and b >= 3 using index idx1 order by b;
-select * from foo where a = 2 and b >= 3 using index idx1 order by b;
-select * from foo where a = 3 and b >= 3 using index idx1 order by b;
+select * from foo where a = 1 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 2 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 3 and b >= 3 using index idx1 order by b, c, d;
 
 drop index idx1 on foo;
 create index idx1 on foo ( a , b , c desc);
-select * from foo where a = 1 and b >= 3 using index idx1 order by b;
-select * from foo where a = 2 and b >= 3 using index idx1 order by b;
-select * from foo where a = 3 and b >= 3 using index idx1 order by b;
+select * from foo where a = 1 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 2 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 3 and b >= 3 using index idx1 order by b, c, d;
 
 drop index idx1 on foo;
 create index idx1 on foo ( a , b desc, c );
-select * from foo where a = 1 and b >= 3 using index idx1 order by b;
-select * from foo where a = 2 and b >= 3 using index idx1 order by b;
-select * from foo where a = 3 and b >= 3 using index idx1 order by b;
+select * from foo where a = 1 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 2 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 3 and b >= 3 using index idx1 order by b, c, d;
 
 drop index idx1 on foo;
 create index idx1 on foo ( a , b desc, c desc);
-select * from foo where a = 1 and b >= 3 using index idx1 order by b;
-select * from foo where a = 2 and b >= 3 using index idx1 order by b;
-select * from foo where a = 3 and b >= 3 using index idx1 order by b;
+select * from foo where a = 1 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 2 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 3 and b >= 3 using index idx1 order by b, c, d;
 
 drop index idx1 on foo;
 create index idx1 on foo ( a desc, b , c );
-select * from foo where a = 1 and b >= 3 using index idx1 order by b;
-select * from foo where a = 2 and b >= 3 using index idx1 order by b;
-select * from foo where a = 3 and b >= 3 using index idx1 order by b;
+select * from foo where a = 1 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 2 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 3 and b >= 3 using index idx1 order by b, c, d;
 
 drop index idx1 on foo;
 create index idx1 on foo ( a desc, b , c desc);
-select * from foo where a = 1 and b >= 3 using index idx1 order by b;
-select * from foo where a = 2 and b >= 3 using index idx1 order by b;
-select * from foo where a = 3 and b >= 3 using index idx1 order by b;
+select * from foo where a = 1 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 2 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 3 and b >= 3 using index idx1 order by b, c, d;
 
 drop index idx1 on foo;
 create index idx1 on foo ( a desc, b desc, c );
-select * from foo where a = 1 and b >= 3 using index idx1 order by b;
-select * from foo where a = 2 and b >= 3 using index idx1 order by b;
-select * from foo where a = 3 and b >= 3 using index idx1 order by b;
+select * from foo where a = 1 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 2 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 3 and b >= 3 using index idx1 order by b, c, d;
 
 drop index idx1 on foo;
 create index idx1 on foo ( a desc, b desc, c desc);
-select * from foo where a = 1 and b >= 3 using index idx1 order by b;
-select * from foo where a = 2 and b >= 3 using index idx1 order by b;
-select * from foo where a = 3 and b >= 3 using index idx1 order by b;
+select * from foo where a = 1 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 2 and b >= 3 using index idx1 order by b, c, d;
+select * from foo where a = 3 and b >= 3 using index idx1 order by b, c, d;
 
 
 drop table foo;
