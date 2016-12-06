e461f91681d3940560227bacb7fe665a74114820
Meihua Jin <jinmeihua0707@navercorp.com>
2016-12-06 12:59:01 +0800

    [CUBRIDQA-12] add order by clause to make test case stable. (#55)

--- sql/_06_manipulation/_01_select/_012_except/cases/1008.sql
+++ sql/_06_manipulation/_01_select/_012_except/cases/1008.sql
@@ -5,15 +5,15 @@ create class sub_t1 as subclass of t1
 
 insert into t1 values(1, 'x');
 insert into t1 values(2, 'y');
-select * from t1;
-select * from sub_t1;
+select * from t1 order by 1;
+select * from sub_t1 order by 1;
 insert into sub_t1 values(1, 'x', 2,'a');
 insert into sub_t1 values(2, 'y', 3,'c');
 insert into sub_t1 values(3, 'z', 4,'b');
 
 
-select * from t1;
-select * from all t1 (except sub_t1);
+select * from t1 order by 1;
+select * from all t1 (except sub_t1) order by 1;
 
 drop class t1;
 drop class sub_t1;
