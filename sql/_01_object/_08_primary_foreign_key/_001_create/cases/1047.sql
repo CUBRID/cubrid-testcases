8afbba513f5ce453f7abe0ad6482e0a0768b3afc
Meihua Jin <jinmeihua0707@navercorp.com>
2016-12-19 10:34:11 +0800

    [CUBRIDQA-12] add order by clause. (#58)

--- sql/_01_object/_08_primary_foreign_key/_001_create/cases/1047.sql
+++ sql/_01_object/_08_primary_foreign_key/_001_create/cases/1047.sql
@@ -4,17 +4,17 @@ create class aoo ( a time primary key, b int, c int );
 
 select attr_name, is_nullable from db_attribute where class_name = 'aoo' order by 1,2;
 
-select * from db_index where class_name = 'aoo';
+select * from db_index where class_name = 'aoo' order by 1,2;
 
-select * from aoo;
+select * from aoo order by 1,2;
 
 create class boo (b time ,a time ,FOREIGN KEY (a) REFERENCES aoo(a));
 
-select attr_name, is_nullable from db_attribute where class_name = 'boo';
+select attr_name, is_nullable from db_attribute where class_name = 'boo' order by 1,2;
 
-select * from db_index where class_name = 'boo';
+select * from db_index where class_name = 'boo' order by 1,2;
 
-select * from boo;
+select * from boo order by 1,2;
 drop boo;
 
 drop aoo;
