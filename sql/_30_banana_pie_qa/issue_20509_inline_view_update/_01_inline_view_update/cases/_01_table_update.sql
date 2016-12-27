1356ec7af667b50496de646bc84f268df13f9202
Nam, Hong-Woo <hwnam@cubrid.com>
2016-12-27 12:26:44 +0900

    [CBRD-20509] new TCs are added to test newly added inline view update function

---
+++ sql/_30_banana_pie_qa/issue_20509_inline_view_update/_01_inline_view_update/cases/_01_table_update.sql
@@ -0,0 +1,33 @@
+--inline view update test 
+
+drop table if exists t1;
+drop table if exists t2;
+drop table if exists t3;
+
+create table t1(f1 int, f2 int);
+create table t2(f1 int, f2 int);
+create table t3(f1 int, f2 int);
+
+insert into t1 values(1,1);
+insert into t2 values(1,1), (2,2);
+insert into t3 values(1,1), (2,2), (3,3);
+
+--[OK] inline view update 
+update (select f2 from t1) set f2=f2+1;
+update (select t1.f2 from t1, t2 where t1.f1=t2.f1) set f2=f2+1;
+
+--[NOK] (inline) view update restriction  
+update (select distinct f2 from t1) set f2=f2+1;
+update (select f1, count(f2) from t1 group by f1) set f1=f1+1;
+update (values (1), (2)) as at1(f2) set at1.f2=at1.f2+1;
+
+--[OK] table alias update 
+update (select f2 from t1) as at1 set at1.f2=at1.f2+1;
+
+--[NOK] column alias error, to be fixed later
+update (select f2 as t1_f2 from t1) set t1_f2=t1_f2+1;
+
+drop table t1;
+drop table t2;
+drop table t3;
+
---
+++ sql/_30_banana_pie_qa/issue_20509_inline_view_update/_01_inline_view_update/cases/_02_view_update.sql
@@ -0,0 +1,53 @@
+-- (inline) view update test 
+
+drop table if exists t1;
+drop table if exists t2;
+drop table if exists t3;
+
+drop view if exists vw_select;
+drop view if exists vw_diff;
+drop view if exists vw_inter;
+drop view if exists vw_union;
+drop view if exists vw_unall;
+drop view if exists vw_unall_3tables;
+drop view if exists vw_alias;
+
+create table t1 (f1 int, f2 int);
+create table t2 (f1 int, f2 int);
+create table t3 (f1 int, f2 int);
+
+create view vw_select as select f2 from t1;
+create view vw_diff as select f2 from t1 difference select f2 from t2;
+create view vw_inter as select f2 from t1 intersection select f2 from t2;
+create view vw_union as select f2 from t1 union select f2 from t2;
+create view vw_unall as select f2 from t1 union all select f2 from t2;
+create view vw_unall_3tables as select f2 from t1 union all select f2 from t2 union all select f2 from t3;
+create view vw_alias as select f2 t1_f2 from t1;
+
+insert into t1 values(1,1);
+insert into t2 values(1,1), (2,2);
+insert into t3 values(1,1), (2,2), (3,3);
+
+--[OK] view update 
+update vw_select set f2=f2+1;
+update vw_alias set t1_f2=t1_f2+1;
+update vw_unall set f2=f2+1;
+
+--[NOK] view update restriction  : set operators
+update vw_diff set f2=f2+1;
+update vw_inter set f2=f2+1;
+update vw_union set f2=f2+1;
+update vw_unall_3tables set f2=f2+1;
+
+
+drop table t1;
+drop table t2;
+drop table t3;
+
+drop view vw_select;
+drop view vw_diff;
+drop view vw_inter;
+drop view vw_union;
+drop view vw_unall;
+drop view vw_unall_3tables;
+
