8c99746a148ceab594786e7b1e13b3a8437ace4a
cubridqa <dl_cubridqa_bj_internal@navercorp.com>
2017-02-06 17:34:31 +0900

    [CUBRIDQA-12]add order by clause

--- sql/_13_issues/_17_1h/cases/cbrd_20594.sql
+++ sql/_13_issues/_17_1h/cases/cbrd_20594.sql
@@ -10,9 +10,9 @@ insert into tb values(repeat('a',100));
 
 insert into tb values(repeat('abc',100));
 
-select x.a,y.a from tb x,tb y where x.a>1 and y.a>1;
+select x.a,y.a from tb x,tb y where x.a>1 and y.a>1 order by 1, 2;
 
-select x.a,y.a from tb x,tb y where x.a=y.a and (x.a>1);
+select x.a,y.a from tb x,tb y where x.a=y.a and (x.a>1) order by 1, 2;
 
 
 drop table if exists t1;
@@ -27,7 +27,7 @@ insert into t1 values('test',repeat('abc',100),'chartest');
 
 insert into t1 values(repeat('test',100),repeat('abc',100),repeat('chartest',50));
 
-select s3,count(*) from t1 where s1>'0' group by s1 desc having count(*) >0; 
+select s3,count(*) from t1 where s1>'0' group by s1 desc having count(*) >0 order by 1, 2; 
 
 
 drop table if exists t;
@@ -44,9 +44,9 @@ create index i_t_a_b on t (a,b);
 
 create index i_t_b_a on t (b,a);
 
-select * from t a,t b where a.a=b.b using index a.i_t_a_b;
+select * from t a,t b where a.a=b.b using index a.i_t_a_b order by 1, 2, 3, 4;
 
-select * from (select * from t where a>0 using index i_t_a_b keylimit 0,3) x, (select * from t where b>'0' using index i_t_b_a keylimit 0,2) y where x.a=y.a;
+select * from (select * from t where a>0 using index i_t_a_b keylimit 0,3) x, (select * from t where b>'0' using index i_t_b_a keylimit 0,2) y where x.a=y.a order by 1, 2, 3, 4;
 
 drop table tb;
 
