05d05bbd6909d91e8810fa9c8c30f07217fdd8e9
Meihua Jin <jinmeihua0707@navercorp.com>
2017-02-09 10:42:23 +0800

    [CUBRIDQA-12]add order by clause. (#76)

--- sql/_01_object/_08_primary_foreign_key/_001_create/cases/1038.sql
+++ sql/_01_object/_08_primary_foreign_key/_001_create/cases/1038.sql
@@ -13,7 +13,7 @@ select * from aoo;
 
 create class boo (b varchar(10) ,a varchar(10) ,FOREIGN KEY (a) REFERENCES aoo(a));
 
-select attr_name, is_nullable from db_attribute where class_name = 'boo';
+select attr_name, is_nullable from db_attribute where class_name = 'boo' order by 1,2;
 
 select * from db_index where class_name = 'boo';
 
--- sql/_01_object/_08_primary_foreign_key/_001_create/cases/1043.sql
+++ sql/_01_object/_08_primary_foreign_key/_001_create/cases/1043.sql
@@ -10,7 +10,7 @@ select * from aoo;
 
 create class boo (b MONETARY ,a MONETARY ,FOREIGN KEY (a) REFERENCES aoo(a));
 
-select attr_name, is_nullable from db_attribute where class_name = 'boo';
+select attr_name, is_nullable from db_attribute where class_name = 'boo' order by 1,2;
 
 select * from db_index where class_name = 'boo';
 
