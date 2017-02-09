0eadbe28d4f9e94c39424b6d2c754328e87b90f4
cubridqa <dl_cubridqa_bj_internal@navercorp.com>
2017-02-09 11:03:50 +0900

    [CUBRIDQA-12]add order by clause.

--- sql/_01_object/_08_primary_foreign_key/_001_create/cases/1052.sql
+++ sql/_01_object/_08_primary_foreign_key/_001_create/cases/1052.sql
@@ -10,7 +10,7 @@ select * from aoo;
 
 create class boo (b char(10) ,a char(10) ,FOREIGN KEY (a) REFERENCES aoo(a));
 
-select attr_name, is_nullable from db_attribute where class_name = 'boo';
+select attr_name, is_nullable from db_attribute where class_name = 'boo' order by 1,2;
 
 select * from db_index where class_name = 'boo';
 
