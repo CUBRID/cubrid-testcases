402502a79355725a4bcdafc694aad4dfb072c380
slamdunkorchid <zhanlanlan@navercorp.com>
2017-02-16 17:02:39 +0900

    [CBRD-20935] add order by

--- sql/_13_issues/_17_1h/cases/cbrd_20935.sql
+++ sql/_13_issues/_17_1h/cases/cbrd_20935.sql
@@ -5,7 +5,7 @@ with c1 as (
 	union all 
 	select 2 n from db_root
 )
-select * from c1;
+select * from c1 order by 1;
 
 with c1 as (    
 	select 1 n from db_root
@@ -14,7 +14,7 @@ with c1 as (
 	union all
 	select n+1 from c1 where n<10
 )
-select * from c1;
+select * from c1 order by 1;
 
 drop if exists t;
 create table t(i int);
@@ -25,11 +25,11 @@ with c1 as (
 	union all
 	select m+1,n+1 from c1 where n<10
 )
-select * from c1;    
+select * from c1 order by 1,2;    
 with c1 as (    
 	select t1.i as m ,t2.i as n from t t1,t t2
 	union all
 	select rownum,n+1 from c1 where n<10
 )
-select * from c1;                     
+select * from c1 order by 1,2;                     
 drop if exists t;
