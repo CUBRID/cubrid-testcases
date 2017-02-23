baae3dc31c45ca941fe3849c3ab8a9b959d03e65
Meihua Jin <jinmeihua0707@navercorp.com>
2017-02-23 13:44:28 +0900

    [CBRD-20945]revise the quries for the fix of collate modifier with ENUM type.

--- sql/_24_aprium_qa/_01_i18n/issue_8700_enum/cases/004_prepare.sql
+++ sql/_24_aprium_qa/_01_i18n/issue_8700_enum/cases/004_prepare.sql
@@ -14,13 +14,13 @@ drop prepare stmt1;
 drop variable @v1;
 
 prepare stmt2 from 'select * from t1 where e2 = ?';
-select @v2:=e2 collate iso88591_bin from t1 where t1.i = 1 order by 1;
+select @v2:=cast(e2 as varchar collate iso88591_bin) from t1 where t1.i = 1 order by 1;
 execute stmt2 using @v2;
 drop prepare stmt2;
 drop variable @v2;
 
 prepare stmt3 from 'select * from t1 where e3 = ?';
-select @v3:=e3 collate iso88591_bin from t1 where t1.i = 1 order by 1;
+select @v3:=cast(e3 as varchar collate iso88591_bin) from t1 where t1.i = 1 order by 1;
 execute stmt3 using @v3;
 drop prepare stmt3;
 drop variable @v3;
