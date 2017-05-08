eaab033e663b26b2d3cce9c96dddbfedf3b2858a
jinmeihua0707 <jinmeihua0707@navercorp.com>
2017-05-08 18:53:15 +0900

    [CBRD-21235]Add test case for 'incorrect result returned with CTE'.

---
+++ sql/_30_banana_pie_qa/issue_20567_limit/cases/cbrd_21235.sql
@@ -0,0 +1,18 @@
+--+ holdcas on;
+
+drop table if exists t1;
+create table t1 (a int);
+insert into t1 select rownum from db_class limit 15;
+with cte as (select * from t1) select * from cte limit 5;
+with cte as (select * from t1) select * from cte limit 0, 10; 
+
+with cte as (select * from t1) select * from cte limit 5*1+(0-0/1)*0;
+with cte as (select * from t1) select * from cte limit 0*1+(0-0/1)*0, 10*1/1+1-1;
+
+
+
+drop table if exists t1;
+
+
+
+--+ holdcas off;
