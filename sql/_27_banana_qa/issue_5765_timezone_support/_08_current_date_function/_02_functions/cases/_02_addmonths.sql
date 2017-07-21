661b3791dbb69130ae9bd98d5dfddfceaca9d3a7
Meihua Jin <jinmeihua0707@navercorp.com>
2017-07-21 14:50:09 +0800

    [CUBRIDQA-69]enhance query to resolving timezone issue of the test case. (#219)

--- sql/_27_banana_qa/issue_5765_timezone_support/_08_current_date_function/_02_functions/cases/_02_addmonths.sql
+++ sql/_27_banana_qa/issue_5765_timezone_support/_08_current_date_function/_02_functions/cases/_02_addmonths.sql
@@ -15,56 +15,113 @@ set timezone '+12:00';
 set @x1=(select adddate(current_date,interval 1 month));
 set timezone '-12:00:00';
 set @x2=(select add_months(current_date,1));
-select if(month(@x1)-month(@x2) in (0,1,-11),'ok','nok');
+--select if(month(@x1)-month(@x2) in (0,1,-11),'ok','nok');
+select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from
+(
+  select @x1_year:=year(@x1), @x1_month:=month(@x1),
+    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    ELSE 0 END expect_value
+) t;
 drop variable @x1,@x2;
 
 set timezone 'America/Chicago';
 set @x1=(select adddate(current_date,interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(current_date,1));
-select if(month(@x1)-month(@x2)=0,'ok','nok');
+--select if(month(@x1)-month(@x2)=0,'ok','nok');
+select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+(
+  select @x1_year:=year(@x1), @x1_month:=month(@x1), 
+    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    ELSE 0 END expect_value
+) t;
+
 drop variable @x1,@x2;
 
 set timezone 'America/Chicago';
 set @x1=(select adddate(current_datetime,interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(current_datetime,1));
-select if(month(@x1)-month(@x2)=0,'ok','nok');
+--select if(month(@x1)-month(@x2)=0,'ok','nok');
+select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from
+(
+  select @x1_year:=year(@x1), @x1_month:=month(@x1),
+    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    ELSE 0 END expect_value
+) t;
 drop variable @x1,@x2;
 
 set timezone 'America/Chicago';
 set @x1=(select adddate(curdate(),interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(curdate(),1));
-select if(month(@x1)-month(@x2)=0,'ok','nok');
+--select if(month(@x1)-month(@x2)=0,'ok','nok');
+select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+(
+  select @x1_year:=year(@x1), @x1_month:=month(@x1), 
+    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    ELSE 0 END expect_value
+) t;
 drop variable @x1,@x2;
 
 set timezone 'America/Chicago';
 set @x1=(select adddate(current_date(),interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(current_date(),1));
-select if(month(@x1)-month(@x2)=0,'ok','nok');
+--select if(month(@x1)-month(@x2)=0,'ok','nok');
+select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+(
+  select @x1_year:=year(@x1), @x1_month:=month(@x1), 
+    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    ELSE 0 END expect_value
+) t;
 drop variable @x1,@x2;
 
 et timezone 'America/Chicago';
 set @x1=(select adddate(now(),interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(now(),1));
-select if(month(@x1)-month(@x2)=0,'ok','nok');
+--select if(month(@x1)-month(@x2)=0,'ok','nok');
+select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+(
+  select @x1_year:=year(@x1), @x1_month:=month(@x1), 
+    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    ELSE 0 END expect_value
+) t;
 drop variable @x1,@x2;
 
 set timezone 'America/Chicago';
 set @x1=(select adddate(current_datetime(),interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(current_datetime(),1));
-select if(month(@x1)-month(@x2)=0,'ok','nok');
+--select if(month(@x1)-month(@x2)=0,'ok','nok');
+select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+(
+  select @x1_year:=year(@x1), @x1_month:=month(@x1), 
+    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    ELSE 0 END expect_value
+) t;
 drop variable @x1,@x2;
 
 set timezone 'America/Chicago';
 set @x1=(select adddate(current_timestamp,interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(current_timestamp,1));
-select if(month(@x1)-month(@x2)=0,'ok','nok');
+--select if(month(@x1)-month(@x2)=0,'ok','nok');
+select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+(
+  select @x1_year:=year(@x1), @x1_month:=month(@x1), 
+    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    ELSE 0 END expect_value
+) t;
 drop variable @x1,@x2;
 
 
@@ -72,21 +129,42 @@ set timezone 'America/Chicago';
 set @x1=(select adddate(current_timestamp(),interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(current_timestamp(),1));
-select if(month(@x1)-month(@x2)=0,'ok','nok');
+--select if(month(@x1)-month(@x2)=0,'ok','nok');
+select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+(
+  select @x1_year:=year(@x1), @x1_month:=month(@x1), 
+    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    ELSE 0 END expect_value
+) t;
 drop variable @x1,@x2;
 
 set timezone 'America/Chicago';
 set @x1=(select adddate(localtimestamp,interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(localtimestamp,1));
-select if(month(@x1)-month(@x2)=0,'ok','nok');
+--select if(month(@x1)-month(@x2)=0,'ok','nok');
+select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+(
+  select @x1_year:=year(@x1), @x1_month:=month(@x1), 
+    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    ELSE 0 END expect_value
+) t;
 drop variable @x1,@x2;
 
 set timezone 'America/Chicago';
 set @x1=(select adddate(localtimestamp(),interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(localtimestamp(),1));
-select if(month(@x1)-month(@x2)=0,'ok','nok');
+--select if(month(@x1)-month(@x2)=0,'ok','nok');
+select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+(
+  select @x1_year:=year(@x1), @x1_month:=month(@x1), 
+    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    ELSE 0 END expect_value
+) t;
 drop variable @x1,@x2;
 
 
@@ -94,14 +172,28 @@ set timezone 'America/Chicago';
 set @x1=(select adddate(localtime,interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(localtime,1));
-select if(month(@x1)-month(@x2)=0,'ok','nok');
+--select if(month(@x1)-month(@x2)=0,'ok','nok');
+select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+(
+  select @x1_year:=year(@x1), @x1_month:=month(@x1), 
+    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    ELSE 0 END expect_value
+) t;
 drop variable @x1,@x2;
 
 set timezone 'America/Chicago';
 set @x1=(select adddate(localtime(),interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(localtime(),1));
-select if(month(@x1)-month(@x2)=0,'ok','nok');
+--select if(month(@x1)-month(@x2)=0,'ok','nok');
+select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+(
+  select @x1_year:=year(@x1), @x1_month:=month(@x1), 
+    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    ELSE 0 END expect_value
+) t;
 drop variable @x1,@x2;
 
 set timezone 'Asia/Seoul';
