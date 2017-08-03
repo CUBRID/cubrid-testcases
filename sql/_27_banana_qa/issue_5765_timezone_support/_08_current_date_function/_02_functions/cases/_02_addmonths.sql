26488cc6348bdeaa5174ae8782e57bcdde003bd5
Meihua Jin <jinmeihua0707@navercorp.com>
2017-08-03 10:45:21 +0800

    [CUBRIDQA-69]enhance query to resolving timezone issue of the test case. (#220)

--- sql/_27_banana_qa/issue_5765_timezone_support/_08_current_date_function/_02_functions/cases/_02_addmonths.sql
+++ sql/_27_banana_qa/issue_5765_timezone_support/_08_current_date_function/_02_functions/cases/_02_addmonths.sql
@@ -1,40 +1,42 @@
 --+ holdcas on;
 set @x1=(select adddate(current_date,interval 1 month));
 set @x2=(select add_months(current_date,1));
-select if(month(@x1)-month(@x2)=0,'ok','nok');
+select if(month(@x1)-month(@x2)=0, 'ok',  concat('nok, ',  month(@x1)- month(@x2),', ',now()) );
 drop variable @x1,@x2;
 
 set timezone '+00:00';
 set @x1=(select adddate(current_date,interval 1 month));
 set timezone '+00:00:01';
 set @x2=(select add_months(current_date,1));
-select if(month(@x1)-month(@x2)=0,'ok','nok');
+select if(month(@x1)-month(@x2)=0, 'ok',  concat('nok, ',  month(@x1)- month(@x2),', ',now() ));
 drop variable @x1,@x2;
 
 set timezone '+12:00';
 set @x1=(select adddate(current_date,interval 1 month));
+set @x_cur=(select adddate(now(),interval 1 month));
 set timezone '-12:00:00';
 set @x2=(select add_months(current_date,1));
 --select if(month(@x1)-month(@x2) in (0,1,-11),'ok','nok');
-select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from
+select if(month(@x1)-month(@x2)=expect_value, 'ok', concat('nok, ',month(@x1)-month(@x2),', ', expect_value,', ',now())) from
 (
   select @x1_year:=year(@x1), @x1_month:=month(@x1),
-    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
-    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    CASE WHEN @x_cur < TIMESTAMP(@x1_year||'-1-2 0:00') THEN -11
+    WHEN @x_cur < TIMESTAMP(@x1_year||'-'||@x1_month||'-2 0:00') THEN 1
     ELSE 0 END expect_value
 ) t;
 drop variable @x1,@x2;
 
 set timezone 'America/Chicago';
 set @x1=(select adddate(current_date,interval 1 month));
+set @x_cur=(select adddate(now(),interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(current_date,1));
 --select if(month(@x1)-month(@x2)=0,'ok','nok');
-select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+select if(month(@x1)-month(@x2)=expect_value, 'ok', concat('nok, ',month(@x1)-month(@x2),', ', expect_value,', ',now())) from 
 (
   select @x1_year:=year(@x1), @x1_month:=month(@x1), 
-    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
-    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    CASE WHEN @x_cur <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x_cur <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
     ELSE 0 END expect_value
 ) t;
 
@@ -45,7 +47,7 @@ set @x1=(select adddate(current_datetime,interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(current_datetime,1));
 --select if(month(@x1)-month(@x2)=0,'ok','nok');
-select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from
+select if(month(@x1)-month(@x2)=expect_value, 'ok', concat('nok, ',month(@x1)-month(@x2),', ', expect_value,', ',now())) from
 (
   select @x1_year:=year(@x1), @x1_month:=month(@x1),
     CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
@@ -56,38 +58,40 @@ drop variable @x1,@x2;
 
 set timezone 'America/Chicago';
 set @x1=(select adddate(curdate(),interval 1 month));
+set @x_cur=(select adddate(now(),interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(curdate(),1));
 --select if(month(@x1)-month(@x2)=0,'ok','nok');
-select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+select if(month(@x1)-month(@x2)=expect_value, 'ok', concat('nok, ',month(@x1)-month(@x2),', ', expect_value,', ',now())) from 
 (
   select @x1_year:=year(@x1), @x1_month:=month(@x1), 
-    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
-    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    CASE WHEN @x_cur <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x_cur <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
     ELSE 0 END expect_value
 ) t;
 drop variable @x1,@x2;
 
 set timezone 'America/Chicago';
 set @x1=(select adddate(current_date(),interval 1 month));
+set @x_cur=(select adddate(now(),interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(current_date(),1));
 --select if(month(@x1)-month(@x2)=0,'ok','nok');
-select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+select if(month(@x1)-month(@x2)=expect_value, 'ok', concat('nok, ',month(@x1)-month(@x2),', ', expect_value,', ',now())) from 
 (
   select @x1_year:=year(@x1), @x1_month:=month(@x1), 
-    CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
-    WHEN @x1 <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
+    CASE WHEN @x_cur <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
+    WHEN @x_cur <= TIMESTAMP(@x1_year||'-'||@x1_month||'-1 2:00') THEN 1
     ELSE 0 END expect_value
 ) t;
 drop variable @x1,@x2;
 
-et timezone 'America/Chicago';
+set timezone 'America/Chicago';
 set @x1=(select adddate(now(),interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(now(),1));
 --select if(month(@x1)-month(@x2)=0,'ok','nok');
-select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+select if(month(@x1)-month(@x2)=expect_value, 'ok', concat('nok, ',month(@x1)-month(@x2),', ', expect_value,', ',now())) from 
 (
   select @x1_year:=year(@x1), @x1_month:=month(@x1), 
     CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
@@ -101,7 +105,7 @@ set @x1=(select adddate(current_datetime(),interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(current_datetime(),1));
 --select if(month(@x1)-month(@x2)=0,'ok','nok');
-select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+select if(month(@x1)-month(@x2)=expect_value, 'ok', concat('nok, ',month(@x1)-month(@x2),', ', expect_value,', ',now())) from 
 (
   select @x1_year:=year(@x1), @x1_month:=month(@x1), 
     CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
@@ -115,7 +119,7 @@ set @x1=(select adddate(current_timestamp,interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(current_timestamp,1));
 --select if(month(@x1)-month(@x2)=0,'ok','nok');
-select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+select if(month(@x1)-month(@x2)=expect_value, 'ok', concat('nok, ',month(@x1)-month(@x2),', ', expect_value,', ',now())) from 
 (
   select @x1_year:=year(@x1), @x1_month:=month(@x1), 
     CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
@@ -130,7 +134,7 @@ set @x1=(select adddate(current_timestamp(),interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(current_timestamp(),1));
 --select if(month(@x1)-month(@x2)=0,'ok','nok');
-select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+select if(month(@x1)-month(@x2)=expect_value, 'ok', concat('nok, ',month(@x1)-month(@x2),', ', expect_value,', ',now())) from 
 (
   select @x1_year:=year(@x1), @x1_month:=month(@x1), 
     CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
@@ -144,7 +148,7 @@ set @x1=(select adddate(localtimestamp,interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(localtimestamp,1));
 --select if(month(@x1)-month(@x2)=0,'ok','nok');
-select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+select if(month(@x1)-month(@x2)=expect_value, 'ok', concat('nok, ',month(@x1)-month(@x2),', ', expect_value,', ',now())) from 
 (
   select @x1_year:=year(@x1), @x1_month:=month(@x1), 
     CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
@@ -158,7 +162,7 @@ set @x1=(select adddate(localtimestamp(),interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(localtimestamp(),1));
 --select if(month(@x1)-month(@x2)=0,'ok','nok');
-select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+select if(month(@x1)-month(@x2)=expect_value, 'ok', concat('nok, ',month(@x1)-month(@x2),', ', expect_value,', ',now())) from 
 (
   select @x1_year:=year(@x1), @x1_month:=month(@x1), 
     CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
@@ -173,7 +177,7 @@ set @x1=(select adddate(localtime,interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(localtime,1));
 --select if(month(@x1)-month(@x2)=0,'ok','nok');
-select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+select if(month(@x1)-month(@x2)=expect_value, 'ok', concat('nok, ',month(@x1)-month(@x2),', ', expect_value,', ',now())) from 
 (
   select @x1_year:=year(@x1), @x1_month:=month(@x1), 
     CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
@@ -187,7 +191,7 @@ set @x1=(select adddate(localtime(),interval 1 month));
 set timezone 'America/Los_Angeles';
 set @x2=(select add_months(localtime(),1));
 --select if(month(@x1)-month(@x2)=0,'ok','nok');
-select if(month(@x1)-month(@x2)=expect_value, 'ok', 'nok') from 
+select if(month(@x1)-month(@x2)=expect_value, 'ok', concat('nok, ',month(@x1)-month(@x2),', ', expect_value,', ',now())) from 
 (
   select @x1_year:=year(@x1), @x1_month:=month(@x1), 
     CASE WHEN @x1 <= TIMESTAMP(@x1_year||'-1-1 2:00') THEN -11
