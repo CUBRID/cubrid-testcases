35a0dfbb66f33f696cd524a90bbd0affc52171d1
Meihua Jin <jinmeihua0707@navercorp.com>
2017-08-03 16:32:41 +0800

    [CBRD-21525]revise required, revise query to ignore precision difference. (#221)

--- sql/_12_mysql_compatibility/_04_numeric_functions/_01_trigonometrical_functions/cases/_q2004_cos.sql
+++ sql/_12_mysql_compatibility/_04_numeric_functions/_01_trigonometrical_functions/cases/_q2004_cos.sql
@@ -1,8 +1,8 @@
 SELECT COS(0) from db_root;
-SELECT COS(1) from db_root;
-SELECT COS(-1) from db_root;
+SELECT to_char(COS(1), '9.999999') from db_root;
+SELECT to_char(COS(-1), '9.999999') from db_root;
 SELECT COS(PI()) from db_root;
-SELECT COS(PI()*pi()) from db_root;
+SELECT to_char(COS(PI()*pi()), '9.999999999') from db_root;
 SELECT to_char(9223372036854775807, '9.999EEEE') from db_root;
 SELECT to_char(-9223372036854775807, '9.999EEEE') from db_root;
-SELECT COS(COS(COS(COS(COS(COS(COS(COS(COS(1))))))))) from db_root;
+SELECT to_char(COS(COS(COS(COS(COS(COS(COS(COS(COS(1))))))))), '9.999999999') from db_root;
