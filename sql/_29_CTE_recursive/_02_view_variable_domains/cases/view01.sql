40ef3cd02279cf7eda51bd90203c1c746ff35f01
jinmeihua0707 <jinmeihua0707@navercorp.com>
2017-08-15 19:57:25 +0900

    [CUBRIDQA-12]enhance order by.

--- sql/_29_CTE_recursive/_02_view_variable_domains/cases/view01.sql
+++ sql/_29_CTE_recursive/_02_view_variable_domains/cases/view01.sql
@@ -8,11 +8,11 @@ insert into foo values (3);
 insert into foo values (4);
 select * from foo order by 1;  
 create view fff( b foo, c int )
-as select x, y.a from foo x, foo y where x.a < y.a;
-with recursive cte as (select * from fff) select * from cte order by 1,2;
-with recursive cte as (select * from fff) select b.a, c  from cte order by 1,2;             
-with recursive cte as (select * from fff) select b.a from cte order by 1;        
-with recursive cte as (select * from fff) select c from cte order by 1;            
+as select x, y.a from foo x, foo y where x.a < y.a order by x.a , y.a;
+with recursive cte as (select * from fff order by 1,2) select * from cte order by 1,2;
+with recursive cte as (select * from fff order by 1,2) select b.a, c  from cte order by 1,2;             
+with recursive cte as (select * from fff order by 1 desc , 2 desc) select b.a from cte order by 1;        
+with recursive cte as (select * from fff order by 1 desc , 2 desc) select c from cte order by 1;            
 drop foo;
 drop fff;
 commit;
