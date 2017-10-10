6e0ad047c4af99748461e29cfdd2216353794c5b
cubridqa <dl_cubridqa_bj_internal@navercorp.com>
2017-10-10 19:56:29 +0900

    [CUBRIDQA-253]remove/modify duplicated test cases.

--- sql/_23_apricot_qa/_02_performance/_02_function_based_index/cases/function_based_index_function_DRANDOM.sql
+++ sql/_23_apricot_qa/_02_performance/_02_function_based_index/cases/function_based_index_function_DRANDOM.sql
@@ -3,7 +3,7 @@ set  system parameters 'dont_reuse_heap_file=yes';
 CREATE TABLE bit_tbl(id int);
 INSERT INTO bit_tbl VALUES (1), (2), (3), (4), (5);
 --Test create index failed;
-create index idx_id on bit_tbl(DRAND(id));
+create index idx_id on bit_tbl(DRANDOM(id));
 drop table bit_tbl ;
 
 set  system parameters 'dont_reuse_heap_file=no';
--- sql/_23_apricot_qa/_02_performance/_03_index_skip_scan/cases/function_index_skip_nchar.sql
+++ sql/_23_apricot_qa/_02_performance/_03_index_skip_scan/cases/function_index_skip_nchar.sql
@@ -2,7 +2,7 @@
 set  system parameters 'dont_reuse_heap_file=yes';
 create table mille as select 0 as i from table({1,2,3,4,5,6,7,8,9,0}) t1, table({1,2,3,4,5,6,7,8,9,0}) t2, table({1,2,3,4,5,6,7,8,9,0}) t3,table({1,2,3,4,5,6,7,8,9,0}) t4;
 
-create table t (i bit(1), j int, k int, l int);
+create table t (i nchar(1), j int, k int, l int);
 
 insert into t(i,j,k,l) select B'1', rownum,rownum,rownum from mille;
 insert into t(i,j,k,l) select null, rownum,rownum,rownum from mille;
