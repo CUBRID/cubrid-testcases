a5e89ede0c78ed602ed87030745d82fc91e4966c
cubridqa <dl_cubridqa_bj_internal@navercorp.com>
2017-10-13 17:43:57 +0900

    [CUBRIDQA-12]add order by clause.

--- sql/_06_manipulation/_01_select/_011_all/cases/1013.sql
+++ sql/_06_manipulation/_01_select/_011_all/cases/1013.sql
@@ -6,8 +6,8 @@ create class sub_t1 as subclass of t1
 
 insert into t1 values(1, 'x');
 insert into t1 values(2, 'y');
-select * from t1;
-select * from sub_t1;
+select * from t1 order by 1;
+select * from sub_t1 order by 1;
 insert into sub_t1 values(1, 'x', 2,'a');
 insert into sub_t1 values(2, 'y', 3,'c');
 insert into sub_t1 values(3, 'z', 4,'b');
@@ -16,7 +16,7 @@ insert into sub_t1 values(3, 'z', 4,'b');
 
 select col1, col2 from t1
 union all
-select col1, col2 from sub_t1;
+select col1, col2 from sub_t1 order by 1,2;
 
 drop class t1;
 drop class sub_t1;
