b765ef9c1ca8236e64ef1012a37b2b17b7e36832
Zhan Lanlan <zhanlanlan@navercorp.com>
2017-11-03 18:42:39 +0800

    [CUBRIDQA-12] enhace cases:avoid -21003 error (#273)

--- sql/_01_object/_09_partition/_006_prunning/cases/1054.sql
+++ sql/_01_object/_09_partition/_006_prunning/cases/1054.sql
@@ -24,7 +24,7 @@ create table range_test2(id int not null primary key,
 				test_real real,
 				test_double double,
 				test_monetary monetary,
-				test_datetime timestamp)
+				test_datetime timestamp);
 			insert into range_test2 values( 1,1,2,1.952491373,1.040967569,1.8889608756,1.438915955,1,'2007-01-01 09:00:00');
 	insert into range_test2 values( 2,2,3,2.102201596,2.448271838,2.309828743,2.862653912,2,'2007-01-01 09:00:00');
 	insert into range_test2 values( 3,3,4,3.500607386,3.2760446668,3.861596361,3.511043858,3,'2007-01-01 09:00:00');
--- sql/_01_object/_09_partition/_006_prunning/cases/1056.sql
+++ sql/_01_object/_09_partition/_006_prunning/cases/1056.sql
@@ -33,8 +33,8 @@ create table range_test2(id int,
 	insert into range_test2 values( 2,2,3,2.102201596,2.448271838,2.309828743,2.862653912,2,'2007-01-01 09:00:00');
 	insert into range_test2 values( 3,3,4,3.500607386,3.2760446668,3.861596361,3.511043858,3,'2007-01-01 09:00:00');
 
-select * from range_test A, range_test2 B order by a.id;
-select a.test_int, b.test_smallint from range_test A, range_test2 B where a.test_int = 2.0 order by a.id;
+select * from range_test A, range_test2 B order by a.id,b.id;
+select a.test_int, b.test_smallint from range_test A, range_test2 B where a.test_int = 2.0 order by a.id,b.test_smallint;
 
 
 
