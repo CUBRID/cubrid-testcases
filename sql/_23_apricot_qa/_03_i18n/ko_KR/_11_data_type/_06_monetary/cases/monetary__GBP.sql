92ec6fd9873751ef5d8e0c4ebc7e97e71c5ba5b5
cubridqa <dl_cubridqa_bj_internal@navercorp.com>
2017-11-07 20:49:56 +0900

    [CBRD-21626]take a few significant digits to stablilize test cases.

--- sql/_23_apricot_qa/_03_i18n/ko_KR/_11_data_type/_06_monetary/cases/monetary__GBP.sql
+++ sql/_23_apricot_qa/_03_i18n/ko_KR/_11_data_type/_06_monetary/cases/monetary__GBP.sql
@@ -6,8 +6,8 @@ INSERT INTO t1 VALUES (\GBP -2);
 INSERT INTO t1 VALUES (\GBP 1.1);
 INSERT INTO t1 VALUES (\GBP -2.2);
 INSERT INTO t1 VALUES (\GBP 123456789012345678901234567890123456789012345678901234567890);
-select * from t1 order by m1;
-select m1,cast(m1 as string) from t1 order by 2;
+select to_char (m1, '9.999999EEEE') from t1 order by m1;
+select to_char (m1, '9.999999EEEE') m1, left(cast(m1 as string), 18)  from t1 order by 2;
 
 drop table t1;
 CREATE TABLE t (m1 monetary default \GBP1, m1_1 monetary default \GBP-1, m100 monetary default $100, m100_1 monetary default $-100)
@@ -15,5 +15,5 @@ insert into t(m1)values(\GBP 10);
 insert into t(m1_1)values(\GBP 10);
 insert into t(m100)values(\GBP 10);
 insert into t(m100_1)values(\GBP 123456789012345678901234567890123456789012345678901234567890);
-select * from t order by m1;
+select m1, m1_1, m100, to_char (m100_1, '9.999999EEEE') m100_1 from t order by 1,2,3,4;
 drop table t;
