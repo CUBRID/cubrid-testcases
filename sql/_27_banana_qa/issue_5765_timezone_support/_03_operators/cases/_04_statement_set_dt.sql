9eb15dce0bff0412ca15e4d630553d408d7c31e2
Zhan Lanlan <zhanlanlan@navercorp.com>
2017-11-21 17:34:20 +0800

    [CUBRIDQA-12] add order by (#290)
    * [CUBRIDQA-12] add order by
    
    * [CUBRIDQA-12] add order by

--- sql/_27_banana_qa/issue_5765_timezone_support/_03_operators/cases/_04_statement_set_dt.sql
+++ sql/_27_banana_qa/issue_5765_timezone_support/_03_operators/cases/_04_statement_set_dt.sql
@@ -35,10 +35,11 @@ insert into t2 values(6, '1987-7-1 08:30:00.999 +9:00');
 set timezone 'Asia/Seoul';
 --test: union, ts+dttz
 select id,to_char(ts,'YYYY-MM-DD HH24:MI:SS.FF TZR TZD') from (select id, ts from t1 union all select id, dttz as ts from t2 order by 1, 2) order by 1,2;
-select id,to_char(ts,'YYYY-MM-DD HH24:MI:SS.FF TZR TZD') from (select id, dttz as ts from t2 union select id, ts from t1 order by 1, 2) order by 1,2;
+select id,to_char(ts,'YYYY-MM-DD HH24:MI:SS.FF TZR TZD') from (select id, dttz as ts from (select id, dttz,to_char(dttz) from t2 order by 1,3 desc ) union select id, ts from t1 order by 1, 2) order by 1,2;
 --test: union, dtltz+dttz
 select id,to_char(ts,'YYYY-MM-DD HH24:MI:SS.FF TZR TZD') from (select id, dtltz as ts from t1 union all select id, dttz as ts from t2 order by 1, 2) order by 1,2;
-select id,to_char(ts,'YYYY-MM-DD HH24:MI:SS.FF TZR TZD') from (select id, dttz as ts from t2 union select id, dtltz as ts from t1 order by 1, 2) order by 1,2;
+select id,to_char(ts,'YYYY-MM-DD HH24:MI:SS.FF TZR TZD') from (select id, dttz as ts from (select id, dttz,to_char(dttz) from t2 order by 1,3
+ desc ) union select id, dtltz as ts from t1 order by 1, 2) order by 1,2;
 
 
 
@@ -59,7 +60,7 @@ select id,to_char(ts,'YYYY-MM-DD HH24:MI:SS TZR TZD') from (select id, dtltz as 
 --test: intersect, ts+dttz
 select id,to_char(ts,'YYYY-MM-DD HH24:MI:SS.FF TZR TZD') from (select id, ts from t1 where id<10 intersect select id, dttz as ts from t2 order by 1, 2);
 --test: intersect, dtltz+dttz
-select id,to_char(ts,'YYYY-MM-DD HH24:MI:SS.FF TZR TZD') from (select id, dttz as ts from t2 intersect select id, dtltz as ts from t1 order by 1, 2);
+select id,to_char(ts,'YYYY-MM-DD HH24:MI:SS.FF TZR TZD') from (select id, dttz as ts from (select id, dttz,to_char(dttz) from t2 order by 1,3 desc ) intersect select id, dtltz as ts from t1 order by 1, 2);
 --test: intersect, dtltz+ts
 select id,to_char(ts,'YYYY-MM-DD HH24:MI:SS TZR TZD') from (select id, ts from t1 intersect select id, dtltz as ts from t1 order by 1, 2);
 
