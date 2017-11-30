981ae163157a637ac1eac1af08dae902f9fc71b6
cubridqa <dl_cubridqa_bj_internal@navercorp.com>
2017-11-30 14:17:17 +0900

    [CBRD-21697]add order by column to make result stable.

--- sql/_31_cherry/issue_21522_json/cases/json_varchar_cast.sql
+++ sql/_31_cherry/issue_21522_json/cases/json_varchar_cast.sql
@@ -1,16 +1,16 @@
 drop table if exists t5;
-create table t5(vals varchar(1000));
-insert into t5 values('{\"period\":90,\"amount\":5.45}');
-insert into t5 values('{\"period\":90,\"amount\":3.99}');
-select vals from t5;
+create table t5(vals varchar(1000),i int);
+insert into t5 values('{\"period\":90,\"amount\":5.45}', 1);
+insert into t5 values('{\"period\":90,\"amount\":3.99}', 2);
+select vals,i from t5 order by i;
 select json_extract(vals, '/period') from t5; 
 select json_extract(replace(vals, '\'), '/period') from t5;
-select * from t5 where json_extract(replace(vals, '\'), '/period') >0;
+select * from t5 where json_extract(replace(vals, '\'), '/period') >0 order by i;
 select json_extract(replace(vals, '\'), '/period') from t5 order by 1;
 select distinct json_extract(replace(vals, '\'), '/period') from t5 order by 1;
 select sum(json_extract(replace(vals, '\'), '/period')) from t5 order by 1;
 --invalid json path
 select json_extract(replace(vals, '\'), '$.period') from t5;
-select * from t5 where cast(json_extract(replace(vals, '\'), '/period') as integer) > cast(json_extract(replace(vals, '\'), '/amount')as integer);
+select * from t5 where cast(json_extract(replace(vals, '\'), '/period') as integer) > cast(json_extract(replace(vals, '\'), '/amount')as integer) order by i;
 
 drop table if exists t5;
