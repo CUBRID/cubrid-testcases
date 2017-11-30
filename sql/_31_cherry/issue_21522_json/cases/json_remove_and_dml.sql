73ca92fd4458698e56dbe4a28a3e9a6432db81c0
cubridqa <neyeng@navercorp.com>
2017-11-30 18:49:15 +0900

    [CBRD-21714] refine case to make sure order stable

--- sql/_31_cherry/issue_21522_json/cases/json_remove_and_dml.sql
+++ sql/_31_cherry/issue_21522_json/cases/json_remove_and_dml.sql
@@ -12,27 +12,26 @@ select json_remove(json_remove(json_merge(name, de), _iso88591'/1'), _iso88591'/
 select json_remove(json_remove(json_merge(name, de), '/1'), '/0/'||json_extract(json_object('a', 'bar'), '/a')) from t10;
 drop table if exists t10;
 drop table if exists t;
-create table t(a json);
+create table t(id int primary key auto_increment not null, a json);
 show create table t;
 alter table t modify a json not null;
 show create table t;
-create trigger tr_t  BEFORE STATEMENT INSERT ON t execute insert into t values(json_array('a'));
-insert into t values(json_object('a', 'b'))
-select * from t order by json_extract(a, '/a');
+create trigger tr_t  BEFORE STATEMENT INSERT ON t execute insert into t(a) values(json_array('a'));
+insert into t(a) values(json_object('a', 'b'));
+select * from t order by id;
 delete  from t where json_extract(a, '/a') is null ;
 select * from t;
 alter table t change a a json default json_object('c', 'c');
-update t set a=default(a) where cast(json_extract(a, '/a') as varchar) = 'b' 
-select * from t;
+update t set a=default(a) where cast(json_extract(a, '/a') as varchar) = 'b';
+select * from t order by id;
 
-replace into t values(json_merge(default(a), json_array('a')));
-select * from t order by json_extract(a, '/a');
-
+replace into t(a) values(json_merge(default(a), json_array('a')));
+select * from t order by id ;
 
 prepare st from 'delete from t where json_extract(a, ?) is null';
 execute st using '/a';
  
-DROP TRIGGER tr_t
+DROP TRIGGER tr_t;
 drop table t;
 deallocate prepare st;
 set names iso88591;
