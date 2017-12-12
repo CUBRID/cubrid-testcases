0694b8ba7c60ffc68d710f3f54277b6f001b6d7e
Meihua Jin <jinmeihua0707@navercorp.com>
2017-12-12 14:46:51 +0800

    [CUBRIDQA-12]add order by (#322)

--- sql/_06_manipulation/_01_select/_008_oid/cases/1002.sql
+++ sql/_06_manipulation/_01_select/_008_oid/cases/1002.sql
@@ -19,7 +19,7 @@ select tb2 into oid2 from tb2 where id = 2;
 insert into tb1 (id, name, address) values(1, 'aaa', oid1);
 insert into tb1 (id, name, address) values(2, 'bbb', oid2);
 
-select * from tb1;
+select * from tb1 order by 1,2;
 
 drop class tb1;
 drop class tb2;
