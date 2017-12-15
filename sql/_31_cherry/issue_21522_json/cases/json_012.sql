7c6f0455480e822be11e66bf3040532bd81b416f
cubridqa <dl_cubridqa_bj_internal@navercorp.com>
2017-12-15 11:32:17 +0900

    [CUBRIDQA-12]add order by clause.

--- sql/_31_cherry/issue_21522_json/cases/json_012.sql
+++ sql/_31_cherry/issue_21522_json/cases/json_012.sql
@@ -9,7 +9,7 @@ insert into t1(a,b) values ('"\a\b\c\d\e\f\g\h\i\j\k\l\m\n\o\p\q\r\s\t\u\v\w\x\y
 select * from t1 order by id;
 set system parameters 'no_backslash_escapes=no';
 insert into t1(a,b) select json_object(b,b),b from t1;
-insert into t1(a,b) select json_array(b,b),b from t1;
+insert into t1(a,b) select json_array(b,b),b from t1 order by b desc;
 select * from t1 order by id;
 drop table if exists t1;
 
