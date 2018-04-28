373c5a98bc676fcbe7697050ba39d50029c83508
Meihua Jin <jinmeihua0707@navercorp.com>
2018-04-28 11:02:24 +0800

    [CUBRIDQA-732]add hex() to order the case insensitive columns. (#434)

--- sql/_28_features_930/issue_12129_HV_collation/cases/_09_union.sql
+++ sql/_28_features_930/issue_12129_HV_collation/cases/_09_union.sql
@@ -3,7 +3,7 @@ set names iso88591;
 
 create table t1(a string collate iso88591_en_ci);
 insert into t1 values ('A'), ('a'), (null);
-prepare stmt from 'select * from t1 where a in (select ?)';
+prepare stmt from 'select a, hex(a) from t1 where a in (select ?) order by 1,2';
 set names iso88591 collate iso88591_en_cs;
 execute stmt using 'A';
 deallocate prepare stmt;
@@ -11,7 +11,7 @@ drop table t1;
 
 create table t1(a string collate iso88591_en_ci);
 insert into t1 values ('A'), ('a'), (null);
-prepare stmt from 'select * from t1 where a in (select ? union select ?)';
+prepare stmt from 'select a, hex(a) from t1 where a in (select ? union select ?) order by 1,2';
 set names iso88591 collate iso88591_en_cs;
 execute stmt using 'A', 'x';
 deallocate prepare stmt;
