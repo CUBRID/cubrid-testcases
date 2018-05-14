2db1b0ae7d9cc6e89a2a97b79e7a8d2ad7451ce4
Meihua Jin <jinmeihua0707@navercorp.com>
2018-05-14 13:32:58 +0800

    [CUBRIDQA-20] add order by clause. (#446)

--- sql/_13_issues/_12_1h/cases/bug_bts_7211.sql
+++ sql/_13_issues/_12_1h/cases/bug_bts_7211.sql
@@ -3,34 +3,34 @@ insert into foo values ('%ccab');
 insert into foo values ('%_cab');
 insert into foo values ('acab');
 insert into foo values ('cab');
-select * from foo where a like '%%_cab' escape '%';
+select * from foo where a like '%%_cab' escape '%' order by a desc;
 
 create table foo1 (a char(10));
 insert into foo1 values ('%cab');
 insert into foo1 values ('%_ab'); 
 insert into foo1 values('acab');
 insert into foo1 values ('cab');
-select * from foo1 where a like '%%cab' escape '%';
-select * from foo1 where a like '%%_ab' escape '%';
-select * from foo1 where a like '%cab' escape '';
+select * from foo1 where a like '%%cab' escape '%' order by a desc;
+select * from foo1 where a like '%%_ab' escape '%' order by a desc;
+select * from foo1 where a like '%cab' escape '' order by a desc;
 
 create table foo2 (a char(10));
 insert into foo2 values ('xyz%cab');
 insert into foo2 values ('xyz%_ab');
 insert into foo2 values ('xyz%_cab');
-select * from foo2 where a like 'xyz%%cab' escape '%';
-select * from foo2 where a like 'xyz%%_ab' escape '%';
-select * from foo2 where a like 'xyz_%cab' escape '_';
+select * from foo2 where a like 'xyz%%cab' escape '%' order by a desc;
+select * from foo2 where a like 'xyz%%_ab' escape '%' order by a desc;
+select * from foo2 where a like 'xyz_%cab' escape '_' order by a desc;
 
-select * from foo2 where a like 'x%u%_ab' escape 'u';
-select * from foo2 where a like 'x%%cab' escape '';
+select * from foo2 where a like 'x%u%_ab' escape 'u' order by a desc;
+select * from foo2 where a like 'x%%cab' escape '' order by a desc;
 
 create table foo3 (a char(10));
 insert into foo3 values ('xyz_cab');
 insert into foo3 values ('xyz_%ab');
 insert into foo3 values ('xyz%_cab');
-select * from foo3 where a like 'xyz_cab' escape 'z';
-select * from foo3 where a like 'xyz__%ab' escape '_';
+select * from foo3 where a like 'xyz_cab' escape 'z' order by a desc;
+select * from foo3 where a like 'xyz__%ab' escape '_' order by a desc;
 
 drop foo;
 drop foo1;
