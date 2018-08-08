2b72a53daf4d84a9b711daf4620747939ab06e8a
Meihua Jin <jinmeihua0707@navercorp.com>
2018-08-08 19:30:00 +0800

    [CBRD-21761]DB_TYPE_TIMETZ, DB_TYPE_TIMELTZ were removed, update test cases. (#475)

--- sql/_27_banana_qa/issue_5765_timezone_support/_00_dev_cases/_02_operator/_35_date_with_tz/cases/date_010.sql
+++ sql/_27_banana_qa/issue_5765_timezone_support/_00_dev_cases/_02_operator/_35_date_with_tz/cases/date_010.sql
@@ -21,7 +21,7 @@ select date(to_datetime_tz('01:23:33 am 10/23/1988 Europe/London'));
 --server side
 create table too(str1 string, str2 char(30));
 
-insert into too values('19:39:45 PM 12/12/2012 Europe/London', '10:10:10 Europe/London');
+insert into too values('19:39:45 PM 12/12/2012 Europe/London', '10:10:10');
 
 select date(to_char(to_timestamp_tz(str1))) from too;
 select date(to_datetime_tz(str1)) from too;
