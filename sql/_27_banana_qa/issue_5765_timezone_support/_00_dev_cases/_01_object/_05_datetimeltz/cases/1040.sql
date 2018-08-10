46ca30f4e504ddad2b03e3801dac4ac56d8407c7
Meihua Jin <jinmeihua0707@navercorp.com>
2018-08-10 13:56:06 +0800

    [CUBRIDQA-12]add order by column. (#482)

--- sql/_27_banana_qa/issue_5765_timezone_support/_00_dev_cases/_01_object/_05_datetimeltz/cases/1040.sql
+++ sql/_27_banana_qa/issue_5765_timezone_support/_00_dev_cases/_01_object/_05_datetimeltz/cases/1040.sql
@@ -5,10 +5,10 @@ insert into test_class values (DATE '01/02/2009', TIME '19:10:00', TIMESTAMP '01
 insert into test_class values (DATE '01/02/2009', TIME '19:10:00', TIMESTAMP '01/11/2009 10:31:00', datetimeltz '2008-03-12 08:08:08.999', datetimeltz '2008-01-12 08:08:08.999');
 insert into test_class(date_col) values (DATE '02/02/2009');
 
-select * from test_class where datetime_col in (datetimeltz '1970-01-01 00:00:00.000', datetimeltz '1970-01-01 00:00:00.001')  order by 1;
-select * from test_class where datetime_col not in (datetimeltz '1970-01-01 00:00:00.000', datetimeltz '1970-01-01 00:00:00.001') order by 1;
-select * from test_class where datetime_col between datetimeltz '1970-01-01 00:00:00.000' AND datetimeltz '1970-01-01 00:00:00.001' order by 1;
-select * from test_class where datetime_col is null order by 1;
-select * from test_class where datetime_col is not null order by 1;
+select * from test_class where datetime_col in (datetimeltz '1970-01-01 00:00:00.000', datetimeltz '1970-01-01 00:00:00.001')  order by 1,4;
+select * from test_class where datetime_col not in (datetimeltz '1970-01-01 00:00:00.000', datetimeltz '1970-01-01 00:00:00.001') order by 1,4;
+select * from test_class where datetime_col between datetimeltz '1970-01-01 00:00:00.000' AND datetimeltz '1970-01-01 00:00:00.001' order by 1,4;
+select * from test_class where datetime_col is null order by 1,4;
+select * from test_class where datetime_col is not null order by 1,4;
 
 drop class test_class;
