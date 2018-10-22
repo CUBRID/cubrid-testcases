9f1e7030764a62ecf3608e9811e23e2978a1c927
cubridqa <dl_cubridqa_bj_internal@navercorp.com>
2018-10-22 19:49:12 +0900

    [CBRD-21869]add test case for bigint type in json.

---
+++ sql/_31_cherry/issue_22162_more_json_functions/cases/json_bigint.sql
@@ -0,0 +1,16 @@
+--CBRD-21869
+drop table if exists t1;
+CREATE TABLE t1 (a json);
+insert into t1(a) values ( '1'),( '9223372036854775805');
+select * from t1 order by a;
+select typeof(9223372036854775805);   
+
+select  * from t1,  
+json_table( a, '$'        
+columns (                 
+id for ordinality,        
+b bigint path '$',        
+c json path '$'           
+))as t2 where a=b and b=c;
+
+drop table if exists t1;
