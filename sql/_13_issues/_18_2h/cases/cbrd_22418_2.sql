8215aec48867696d001971382804bada47578bcf
Zhan Lanlan <zhanlanlan@navercorp.com>
2018-11-06 17:28:48 +0800

    [CBRD-22418] add test cases: null value with online index (#520)

---
+++ sql/_13_issues/_18_2h/cases/cbrd_22418_2.sql
@@ -0,0 +1,26 @@
+drop table if exists t1;
+create table t1(  a char(1200),   b varchar(1200),  c nchar(1200),  d NCHAR VARYING(1200),  e BIT(1200),  f BIT VARYING(1200),  g int,  h SMALLINT,  i BIGINT,  j NUMERIC,  k FLOAT,  l DOUBLE,  m MONETARY,  n DATE,  o TIME,  p TIMESTAMP,  q DATETIME);
+insert into t1 values (
+  '1234567890',
+  '1234567890',
+  N'abc',
+  N'ABC',
+  B'1111111111',
+  B'1111111111',
+  10,
+  255,
+  9223372036854775807,
+  0,
+  0,
+  0,
+  -100,
+  DATE '2008-10-31',
+  TIME '00:00:00',
+  TIMESTAMP '2010-10-31 01:15:45',
+  DATETIME  '2008-10-31 13:15:45');
+insert into t1 values (null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null);
+
+create index i_t1_a2q on t1(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q);
+create index i_t1_a2p on t1(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p) with online;
+show index from t1;
+drop table if exists t1;
