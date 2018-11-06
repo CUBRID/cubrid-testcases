0d486686918b7b09cafb3d36bf1d9884494a843b
Zhan Lanlan <zhanlanlan@navercorp.com>
2018-11-06 17:28:33 +0800

    [CBRD-22420] add test cases: filter index with online (#521)

---
+++ sql/_13_issues/_18_2h/cases/cbrd_22420_2.sql
@@ -0,0 +1,87 @@
+drop table if exists t1;
+create table t1 (FirstName varchar(20),LastName varchar(20),Age int,gender char (1));
+insert into t1 value('yin', 'jian ye',31,'M');
+--TEST Create successfully
+CREATE INDEX first_name_lower ON t1 (LOWER(FirstName)) with online;
+--Test should use index first_name_lower
+SELECT /*+ recompile */* FROM t1 WHERE LOWER(FirstName)='yin';
+--TEST Alter successfully
+ALTER  INDEX first_name_lower on t1 REBUILD;
+--TEST Alter failed 
+ALTER  INDEX first_name_lower on t1 (trim(FirstName));
+--TEST Alter successfully 
+ALTER  INDEX first_name_lower on t1 (upper(FirstName))REBUILD;
+--Test should not use index first_name_lower
+SELECT /*+ recompile */* FROM t1 WHERE LOWER(FirstName)='yin';
+--Test should use index first_name_lower
+SELECT /*+ recompile */* FROM t1 WHERE upper(FirstName)='yin';
+--TEST Create successfully
+CREATE INDEX first_name_lower1 ON t1 (LOWER(FirstName));
+--TEST Alter failed 
+ALTER  INDEX first_name_lower on t1 (LOWER(FirstName)) REBUILD;
+drop  table t1 ;
+
+create table t1 (FirstName varchar(20),LastName varchar(20),Age int,gender char (1));
+insert into t1 value('yin', 'jian ye',31,'M');
+--TEST Create successfully
+CREATE INDEX first_name_lower ON t1 (LOWER(FirstName)) with online;
+--Test should use index first_name_lower
+SELECT /*+ recompile */* FROM t1 WHERE LOWER(FirstName)='yin';
+--TEST Alter fail 
+ALTER  INDEX first_name_lower on t1 where FirstName>'aaa' REBUILD;
+--TEST Alter successfully 
+ALTER  INDEX first_name_lower on t1(firstname) WHERE LOWER(FirstName)='yin' REBUILD;
+--Test should not use index first_name_lower
+SELECT /*+ recompile */* FROM t1 WHERE LOWER(FirstName)='yin';
+--Test should use index first_name_lower
+SELECT /*+ recompile */* FROM t1 WHERE LOWER(FirstName)='yin' using index first_name_lower(+);
+--TEST Alter successfully 
+ALTER  INDEX first_name_lower on t1(LOWER(FirstName)) REBUILD;
+--Test should not use index first_name_lower
+SELECT /*+ recompile */* FROM t1 WHERE FirstName ='yin';
+--Test should use index first_name_lower
+SELECT /*+ recompile */* FROM t1 WHERE LOWER(FirstName)='yin';
+--TEST Alter successfully 
+ALTER  INDEX first_name_lower on t1(FirstName) REBUILD;
+--Test should not use index first_name_lower
+SELECT /*+ recompile */* FROM t1 WHERE LOWER(FirstName)='yin';
+--Test should  use index first_name_lower
+SELECT /*+ recompile */* FROM t1 WHERE FirstName ='yin';
+--TEST Alter successfully 
+ALTER  INDEX first_name_lower on t1(LOWER(FirstName)) REBUILD;
+--Test should not use index first_name_lower
+SELECT /*+ recompile */* FROM t1 WHERE FirstName ='yin';
+--Test should use index first_name_lower
+SELECT /*+ recompile */* FROM t1 WHERE LOWER(FirstName)='yin';
+drop  table t1 ;
+
+create table t1 (FirstName varchar(20),LastName varchar(20),Age int,gender char (1));
+insert into t1 value('yin', 'jian ye',31,'M');
+--TEST Create successfully
+CREATE INDEX first_name_lower ON t1 (LOWER(FirstName)) with online;
+--TEST Alter successfully 
+ALTER  INDEX first_name_lower ON t1 (LOWER(LastName)) REBUILD;
+--TEST Alter successfully 
+ALTER  INDEX first_name_lower ON t1 (CONCAT(FirstName,LastName)) REBUILD;
+--TEST Alter successfully 
+ALTER  INDEX first_name_lower ON t1(FirstName) where FirstName>'aaa' and LastName>'aaa' REBUILD;
+--TEST Alter successfully 
+ALTER  INDEX first_name_lower ON t1 (LOWER(FirstName)) REBUILD;
+--TEST Alter successfully 
+ALTER  INDEX first_name_lower ON t1 (LastName) REBUILD;
+--TEST Alter successfully 
+ALTER  INDEX first_name_lower ON t1 (LOWER(FirstName)) REBUILD;
+drop  table t1 ;
+
+create table t1 (FirstName varchar(20),LastName varchar(20),Age int,gender char (1));
+insert into t1 value('yin', 'jian ye',31,'M');
+--TEST Create successfully
+CREATE INDEX first_name_lower ON t1 (LOWER(FirstName)) with online;
+--TEST Alter successfully 
+ALTER  REVERSE INDEX first_name_lower ON t1 (LOWER(LastName)) REBUILD;
+--TEST Alter successfully 
+ALTER  REVERSE INDEX first_name_lower ON t1 (LOWER(FirstName)) REBUILD;
+--TEST Alter successfully 
+ALTER  INDEX first_name_lower ON t1 (LOWER(LastName)) REBUILD;
+drop  table t1 ;
+
