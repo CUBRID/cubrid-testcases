fe8ccce2744143f2e08c32a947a7694e80b1d5f4
cubridqa <dl_cubridqa_bj_internal@navercorp.com>
2018-11-15 19:30:41 +0900

    [CBRD-22478]revise for fix of JSON_MERGE_PRESERVE.

--- sql/_31_cherry/issue_22162_more_json_functions/cases/json_merge_patch_preserve.sql
+++ sql/_31_cherry/issue_22162_more_json_functions/cases/json_merge_patch_preserve.sql
@@ -19,10 +19,6 @@ SELECT
 JSON_MERGE_PATCH   ('{"Name":"Mark"}','{"Age":40}','{"Job":"CEO"}'),
 JSON_MERGE_PRESERVE('{"Name":"Mark"}','{"Age":40}','{"Job":"CEO"}'); 
 
-SELECT JSON_MERGE_PATCH   ('{"Alphabet":["a","b"]}','{"Alphabet":["c"]}');
--- CBRD-22478
--- SELECT JSON_MERGE_PRESERVE('{"Alphabet":["a","b"]}','{"Alphabet":["c"]}'); 
-
 SELECT 
 JSON_MERGE_PATCH   ('{"Alphabet":["a","b"]}','{"Alphabet":["c"]}'),
 JSON_MERGE_PRESERVE('{"Alphabet":["a","b"]}','{"Alphabet":["c"]}'); 
@@ -31,7 +27,7 @@ set @jd1 = '{ "Alphabet": { "A": "a", "B": ["b", "c"] } }';
 set @jd2 = '{ "Alphabet": { "C": 3,   "D": ["d", "e"] } }';
 SELECT JSON_MERGE_PATCH(@jd1,@jd2); 
 -- CBRD-22478
--- SELECT JSON_MERGE_PRESERVE(@jd1,@jd2); 
+SELECT JSON_MERGE_PRESERVE(@jd1,@jd2); 
 drop variable @jd1,@jd2;
 
 
