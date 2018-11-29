20f14eadceaa8687036acf5509e6458e64137b7b
cubridqa <dl_cubridqa_bj_internal@navercorp.com>
2018-11-29 12:52:53 +0900

    [CBRD-22394]add test case for pt_to_aggregate_node memory leak.

---
+++ sql/_13_issues/_18_2h/cases/cbrd_22394.sql
@@ -0,0 +1,61 @@
+drop table if exists simple;
+create table simple (a int);
+insert into simple values (1);
+insert into simple values (1);
+insert into simple values (NULL);
+insert into simple values (2);
+insert into simple values (3);
+
+select group_concat (a separator ':') from simple;
+
+drop table if exists simple;
+create table simple (a int);
+insert into simple values (1);
+insert into simple values (1);
+insert into simple values (NULL);
+insert into simple values (2);
+insert into simple values (3);
+
+select group_concat (a separator ':') from simple;
+
+drop table if exists simple;
+create table simple (a int);
+insert into simple values (1);
+insert into simple values (1);
+insert into simple values (NULL);
+insert into simple values (2);
+insert into simple values (3);
+
+select group_concat (a separator ':') from simple;
+
+drop table if exists simple;
+create table simple (a int);
+insert into simple values (1);
+insert into simple values (1);
+insert into simple values (NULL);
+insert into simple values (2);
+insert into simple values (3);
+
+select group_concat (a separator ':') from simple;
+
+drop table if exists simple;
+create table simple (a int);
+insert into simple values (1);
+insert into simple values (1);
+insert into simple values (NULL);
+insert into simple values (2);
+insert into simple values (3);
+
+select group_concat (a separator ':') from simple;
+
+drop table if exists simple;
+create table simple (a int);
+insert into simple values (1);
+insert into simple values (1);
+insert into simple values (NULL);
+insert into simple values (2);
+insert into simple values (3);
+
+select group_concat (a separator ':') from simple;
+
+drop table if exists simple;
