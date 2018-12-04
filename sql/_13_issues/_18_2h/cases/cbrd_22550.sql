ef9caf9797a175855b10ce5c2db00ef08a9ea819
Zhan Lanlan <zhanlanlan@navercorp.com>
2018-12-04 17:21:24 +0800

    [CBRD-22550] add test cases for width_bucket in cte select query (#564)

---
+++ sql/_13_issues/_18_2h/cases/cbrd_22550.sql
@@ -0,0 +1,13 @@
+drop if exists foo,yoo;
+create table foo(a varchar);
+insert into foo values('0:12:23');
+insert into foo values('23:34:45');
+insert into foo values(NULL);
+create table yoo as 
+WITH cte AS 
+(
+select width_bucket(a, '0:0:0', '23:59:59', 3) from foo order by 1
+)
+select * from cte;
+select * from yoo order by 1;
+drop if exists foo,yoo;
