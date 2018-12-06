8c9f9097281e50a1c9bb6123467970e0d67f000f
cubridqa <dl_cubridqa_bj_internal@navercorp.com>
2018-12-06 15:40:12 +0900

    [CUBRIDQA-12]add order by column to make result stable.

--- sql/_31_cherry/issue_22161_CTE_extensions/_04_replace/cases/to_number.sql
+++ sql/_31_cherry/issue_22161_CTE_extensions/_04_replace/cases/to_number.sql
@@ -4,29 +4,29 @@ insert into xoo values(1, '111');
 insert into xoo values(2, '222');
 prepare st from 'with cte as (select a from xoo where a = to_number(?) and a = ?) select *from cte' ;
 execute st using '1', '1';
-select * from xoo order by 1;
+select * from xoo order by 1,2;
 
 drop prepare st;
 prepare st from 'with cte as (select a from xoo where a = to_number(?) and a = ?) select *from cte' ;
 execute st using '1', 1 ;
-select * from xoo order by 1;
+select * from xoo order by 1,2;
 
 prepare st from 'with cte as (select a from xoo where a = to_number(?) and a = ?) delete from xoo where a in (select a from cte)' ;
 execute st using '1', 1 ;
-select * from xoo order by 1;
+select * from xoo order by 1,2;
 
 prepare st from 'with cte as (select a from xoo where a = to_number(?) and a = ?) update xoo set a=3 where a in (select a from cte)' ;
 execute st using '2', 2 ;
-select * from xoo order by 1;
+select * from xoo order by 1,2;
 
 prepare st from 'insert into xoo with cte as (select a,-1 from xoo where a = to_number(?) and a = ?) select * from cte' ;
 execute st using '3', 3 ;
-select * from xoo order by 1;
+select * from xoo order by 1,2 desc;
 
 
 prepare st from 'replace into xoo with cte as (select a,-1 from xoo where a = to_number(?) and a = ?) select * from cte' ;
 execute st using '3', 3 ;
-select * from xoo order by 1;
+select * from xoo order by 1,2 desc;
 
 prepare st from 'create table foo as with cte as (select a from xoo where a = to_number(?) and a = ?) select * from cte' ;
 execute st using '3', 3 ;
