e2b170abf3f3842a5ffe5a0c237c31b6fb9b29d0
cubridqa <dl_cubridqa_bj_internal@navercorp.com>
2019-02-11 14:43:50 +0900

    [CUBRIDQA-12] add order by and modify the error message for json.

--- sql/_31_cherry/issue_22161_CTE_extensions/_03_insert/cases/inst_num.sql
+++ sql/_31_cherry/issue_22161_CTE_extensions/_03_insert/cases/inst_num.sql
@@ -11,22 +11,22 @@ create table foo as
 WITH cte1 AS (
        SELECT t.*
             , t.id + 5 AS id2
-         FROM test1 AS t
+         FROM test1 AS t order by 1
      )
 SELECT t.*
      ,  INST_NUM() AS n
-  FROM cte1 AS t
+  FROM cte1 AS t order by 1
 ;
 
 replace into foo
 WITH cte1 AS (
        SELECT t.*
             , t.id + 5 AS id2
-         FROM test1 AS t
+         FROM test1 AS t order by 1
      )
 SELECT t.*
      , INST_NUM()  AS n
-  FROM cte1 AS t;
+  FROM cte1 AS t order by 1;
 
 select * from foo order by 1,2,3;
 
@@ -34,24 +34,24 @@ replace into foo
 WITH cte1 AS (
        SELECT t.*
             , t.id + 5 AS id2
-         FROM test1 AS t
+         FROM test1 AS t order by 1
      ),
 cte2 as (
 SELECT t.*
      , INST_NUM()  AS n
-  FROM cte1 as t) select * from cte2
+  FROM cte1 as t) select * from cte2 order by 1
 ;
 select * from foo order by 1,2,3;
 
 WITH cte1 AS (
        SELECT t.*
             , t.id + 5 AS id2
-         FROM test1 AS t
+         FROM test1 AS t order by 1
      ),
 cte2 as (
 SELECT t.*
      , INST_NUM()  AS n
-  FROM cte1 as t) delete from foo where n in (
+  FROM cte1 as t order by 1) delete from foo where n in (
 SELECT INST_NUM()  AS n
   FROM cte1 )
 ;
@@ -62,21 +62,21 @@ replace into foo
 WITH cte1 AS (
 SELECT t.*
 , t.id + 5 AS id2
-FROM test1 AS t
+FROM test1 AS t order by 1
 )
 SELECT t.*
 , INST_NUM()  AS n
-FROM cte1 AS t;
+FROM cte1 AS t order by 1;
 
 WITH cte1 AS (
        SELECT t.*
             , t.id + 5 AS id2
-         FROM test1 AS t
+         FROM test1 AS t order by 1
      ),
 cte2 as (
 SELECT t.*
      , INST_NUM()  AS n
-  FROM cte1 as t) update foo set id=id+10 where n  in (
+  FROM cte1 as t order by 1) update foo set id=id+10 where n  in (
 SELECT INST_NUM()  AS n
   FROM cte1 )
 ;
@@ -85,12 +85,12 @@ select * from foo order by 1,2,3;
 WITH cte1 AS (
        SELECT t.*
             , t.id + 5 AS id2
-       FROM test1 AS t
+       FROM test1 AS t order by 1
 ),
 cte2 as (
        SELECT t.*
        , INST_NUM()  AS n
-       FROM cte1 as t
+       FROM cte1 as t order by 1
 ) update foo set id=id+10 where INST_NUM()<=3 ;
 select * from foo order by 1,2,3;
 
@@ -98,14 +98,14 @@ select * from foo order by 1,2,3;
 insert into foo
 with cte as
 (
- select * from test1 where inst_num() between 1 and 2
-) select id,id+5,inst_num() from cte;
+ select * from test1 where inst_num() between 1 and 2 order by 1
+) select id,id+5,inst_num() from cte order by 1;
 
 select * from foo order by 1,2,3;
 
 with cte as
 (
- select * from test1 where inst_num() between 1 and 2
+ select * from test1 where inst_num() between 1 and 2 order by 1
 ) delete from foo where id in (select id from cte) and inst_num()<=2;
 
 select * from foo order by 1,2,3;
@@ -113,12 +113,12 @@ select * from foo order by 1,2,3;
 insert into foo
 with cte as
 (
- select * from test1 where inst_num() between 1 and 2
-) select id,id+5,inst_num() from cte where inst_num()<=5;
+ select * from test1 where inst_num() between 1 and 2 order by 1
+) select id,id+5,inst_num() from cte where inst_num()<=5 order by 1;
 
 with cte as
 (
- select * from test1 where inst_num() between 1 and 2
+ select * from test1 where inst_num() between 1 and 2 order by 1
 ) update foo set id=id-10 where id in (select id from cte) and inst_num()<=2;
 
 select * from foo order by 1,2,3;
