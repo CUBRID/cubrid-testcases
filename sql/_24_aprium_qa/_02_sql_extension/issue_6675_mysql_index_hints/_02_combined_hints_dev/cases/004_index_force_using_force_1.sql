6f77ddf64a2acad3a666dea2fda6e3b911ef0170
Zhaojia2019 <46517193+Zhaojia2019@users.noreply.github.com>
2019-04-15 15:19:25 +0800

    [CUBRIDQA-12]add order by (#658)

--- sql/_24_aprium_qa/_02_sql_extension/issue_6675_mysql_index_hints/_02_combined_hints_dev/cases/004_index_force_using_force_1.sql
+++ sql/_24_aprium_qa/_02_sql_extension/issue_6675_mysql_index_hints/_02_combined_hints_dev/cases/004_index_force_using_force_1.sql
@@ -14,21 +14,21 @@ insert into t41 values (2, 3, 2), (2, 2, 3), (2, 1, 1);
 commit;
 update statistics on t41;
 
-select /*+ recompile */ i1 from t41 where i1 < 4;
-select /*+ recompile */ * from t41 where i1 < 4;
+select /*+ recompile */ i1 from t41 where i1 < 4 order by 1;
+select /*+ recompile */ * from t41 where i1 < 4 order by 1,2,3;
 
-select /*+ recompile */ i1 from t41 force index (t41_idx_one) where i1 < 4 using index t41_idx_one(+);
-select /*+ recompile */ i1 from t41 force index (t41_idx_one_two) where i1 < 4 using index t41_idx_one_two(+);
-select /*+ recompile */ i1 from t41 force index (t41_idx_one_three) where i1 < 4 using index t41_idx_one_three(+);
-select /*+ recompile */ i1 from t41 force index (t41_idx_all) where i1 < 4 using index t41_idx_all(+);
-select /*+ recompile */ i1 from t41 force index (t41_idx_two) where i1 < 4 using index t41_idx_two(+);
-select /*+ recompile */ i1 from t41 force index (t41_idx_two_three) where i1 < 4 using index t41_idx_two_three(+);
+select /*+ recompile */ i1 from t41 force index (t41_idx_one) where i1 < 4 using index t41_idx_one(+) order by 1;
+select /*+ recompile */ i1 from t41 force index (t41_idx_one_two) where i1 < 4 using index t41_idx_one_two(+) order by 1;
+select /*+ recompile */ i1 from t41 force index (t41_idx_one_three) where i1 < 4 using index t41_idx_one_three(+) order by 1;
+select /*+ recompile */ i1 from t41 force index (t41_idx_all) where i1 < 4 using index t41_idx_all(+) order by 1;
+select /*+ recompile */ i1 from t41 force index (t41_idx_two) where i1 < 4 using index t41_idx_two(+) order by 1;
+select /*+ recompile */ i1 from t41 force index (t41_idx_two_three) where i1 < 4 using index t41_idx_two_three(+) order by 1;
 
-select /*+ recompile */ i1 from t41 force index (t41_idx_one, t41_idx_two) where i1 < 4 using index t41_idx_one(+), t41_idx_two(+);
-select /*+ recompile */ i1 from t41 force index (t41_idx_one, t41_idx_one_two) where i1 < 4 using index t41_idx_one(+), t41_idx_one_two(+);
-select /*+ recompile */ i1 from t41 force index (t41_idx_one, t41_idx_one_three) where i1 < 4 using index t41_idx_one(+), t41_idx_one_three(+);
-select /*+ recompile */ i1 from t41 force index (t41_idx_one, t41_idx_all) where i1 < 4 using index t41_idx_one(+), t41_idx_all(+);
-select /*+ recompile */ i1 from t41 force index (t41_idx_one, t41_idx_two_three) where i1 < 4 using index t41_idx_one(+), t41_idx_two_three(+);
+select /*+ recompile */ i1 from t41 force index (t41_idx_one, t41_idx_two) where i1 < 4 using index t41_idx_one(+), t41_idx_two(+) order by 1;
+select /*+ recompile */ i1 from t41 force index (t41_idx_one, t41_idx_one_two) where i1 < 4 using index t41_idx_one(+), t41_idx_one_two(+) order by 1;
+select /*+ recompile */ i1 from t41 force index (t41_idx_one, t41_idx_one_three) where i1 < 4 using index t41_idx_one(+), t41_idx_one_three(+) order by 1;
+select /*+ recompile */ i1 from t41 force index (t41_idx_one, t41_idx_all) where i1 < 4 using index t41_idx_one(+), t41_idx_all(+) order by 1;
+select /*+ recompile */ i1 from t41 force index (t41_idx_one, t41_idx_two_three) where i1 < 4 using index t41_idx_one(+), t41_idx_two_three(+) order by 1;
 
 drop index t41_idx_one on t41;
 drop index t41_idx_two on t41;
