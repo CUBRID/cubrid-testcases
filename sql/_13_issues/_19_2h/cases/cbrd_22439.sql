998e45b64b7b096c2f95cd14b5377e04dcbb937a
slamdunkorchid <zhanlanlan@navercorp.com>
2019-08-02 16:17:37 +0900

    [CBRD-22439] add test cases: core dumped in pt_to_update_xasl

---
+++ sql/_13_issues/_19_2h/cases/cbrd_22439.sql
@@ -0,0 +1,19 @@
+drop if exists test_class;
+create class test_class (bigint_col bigint, char_col char(10));
+
+insert into test_class(bigint_col, char_col) values (-100, 'minus');
+
+insert into test_class(bigint_col, char_col) values (0, 'zero');
+
+insert into test_class(bigint_col, char_col) values (100, 'plus');
+
+insert into test_class(bigint_col, char_col) values (109, 'plus');
+
+insert into test_class(bigint_col, char_col) values (NULL, 'null');
+select * from test_class where bigint_col is NULL WITH INCREMENT FOR bigint_col ;
+
+drop table if exists t;
+create table t(i int);
+insert into t values(-1),(-2),(-3);
+update t set i=(select count(*) from (select * from test_class where bigint_col = 101 WITH INCREMENT FOR bigint_col) cte);
+drop if exists t,test_class;
