ad4a1d92f1df7925c998017cb855e10849019de2
jinmeihua <jinmeihua.0707@navercorp.com>
2019-08-07 15:11:05 +0900

    [CBRD-22574]add test case for csql crash in do_select.

---
+++ sql/_13_issues/_19_2h/cases/cbrd_22574.sql
@@ -0,0 +1,8 @@
+drop table if exists t1;
+create table t1 ( a set(int) );
+insert into t1 values( {1,2,3,4,5} );
+select a into set1 from t1;
+
+--test: csql crashed for below:
+select avg(set1) from table(set1);
+drop table if exists t1;
