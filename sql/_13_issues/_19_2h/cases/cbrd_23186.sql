b3243e32e4ad8b6a8caa01e8adb30fd9860e4758
mandy2016 <cui.man@navercorp.com>
2019-10-18 17:24:36 +0900

    [CBRD-23186] Add test case for this issue

---
+++ sql/_13_issues/_19_2h/cases/cbrd_23186.sql
@@ -0,0 +1,7 @@
+drop table if exists t;
+create table t (a string, b string);
+insert into t values ('c',null);
+create index idx2 on t(nvl(a,b)) invisible;
+show index from t;
+commit;
+update t set a = null;
