838df444ae7fd750f1c607582c3a117e0cd67b1c
cubridqa <dl_cubridqa_bj_internal@navercorp.com>
2019-10-31 17:49:43 +0900

    [CBRD-22714]add about core happen test

--- sql/_13_issues/_19_2h/cases/cbrd_22714.sql
+++ sql/_13_issues/_19_2h/cases/cbrd_22714.sql
@@ -9,4 +9,10 @@ create table t2 as
     SELECT id + 1, 'test title' FROM cte where id<10
   ) SELECT  * FROM cte;
   
+WITH RECURSIVE cte AS (
+    SELECT id, title FROM t1
+    UNION ALL
+    SELECT id + 1, 'test title' FROM cte where id<10
+  ) SELECT  * FROM cte order by 1;
+select * from t2 order by 1;
 drop table if exists t1, t2;
