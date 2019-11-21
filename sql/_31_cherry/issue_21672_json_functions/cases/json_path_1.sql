6b975103352aa293a43e5389dfb5467c6e461e99
Meihua Jin <jinmeihua0707@navercorp.com>
2019-11-21 15:32:00 +0800

    [CBRD-23554]revise for the change of default JSON domain charset as UTF-8. (#748)

--- sql/_31_cherry/issue_21672_json_functions/cases/json_path_1.sql
+++ sql/_31_cherry/issue_21672_json_functions/cases/json_path_1.sql
@@ -170,8 +170,8 @@ select @str, json_get_all_paths(@str);
 select @str, json_keys(@str);
 
 
-SELECT JSON_ARRAY('1. ě 2. š 3. č 4. ř 5. ž 6. ý 7. á 8. í 9. é 10. ů 11. ú') AS json_data;
-SELECT JSON_OBJECT('user','Jožko Mrkvičká') as json_data;
+SELECT JSON_ARRAY(_utf8'1. ě 2. š 3. č 4. ř 5. ž 6. ý 7. á 8. í 9. é 10. ů 11. ú') AS json_data;
+SELECT JSON_OBJECT('user',_utf8'Jožko Mrkvičká') as json_data;
 
 select JSON_VALID(0x36f0c8dccd83c5eac156da);
 
