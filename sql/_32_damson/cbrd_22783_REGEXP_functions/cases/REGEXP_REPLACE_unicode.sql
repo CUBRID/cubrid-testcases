b0d4fa7101447b237ddcc6d22aa8cb4885d227fa
tw-kang <54791480+tw-kang@users.noreply.github.com>
2020-04-08 12:47:46 +0900

    [CBRD-23641] Change category of test case #786

--- sql/_32_damson/cbrd_22783_REGEXP_functions/cases/REGEXP_REPLACE_unicode.sql
+++ sql/_32_damson/cbrd_22783_REGEXP_functions/cases/REGEXP_REPLACE_unicode.sql
@@ -12,13 +12,15 @@ SELECT REGEXP_REPLACE('가나다라', '[가-라]{4}', '한글');
 SELECT REGEXP_REPLACE('가나다라', '[[:alpha:]]', '한글')
 SELECT REGEXP_REPLACE('a1가b2나다라', '[가-다]', '#', 6);
 SELECT REGEXP_REPLACE('a1가b2나다라', '[가-다]', '#', 1, 3);
-SELECT REGEXP_REPLACE(_euckr'가나다라' collate euckr_bin, _utf8' [가-나]{4}', _utf8'다른 문자셋');
 SELECT REGEXP_REPLACE(_euckr'가나다라', _utf8' [가-나]{4}', _utf8'다른 문자셋');
+--According to CBRD-23641 do not include
+--SELECT REGEXP_REPLACE(_euckr'가나다라' collate euckr_bin, _utf8' [가-나]{4}', _utf8'다른 문자셋');
 
 SET NAMES utf8; 
 SELECT REGEXP_REPLACE('Kłak Aleksander', '[[:alpha:]]+','#', 1, 1);
 SELECT REGEXP_REPLACE(_euckr'가나다라', _utf8' [가-나]{4}', _utf8'다른 문자셋');
 SELECT REGEXP_REPLACE('가나다라' COLLATE utf8_ko_cs,  _utf8' [த]' COLLATE utf8_tr_cs, 'INVALID');
 
-SET NAMES iso88591;
-SELECT REGEXP_REPLACE('Kłak Aleksander', '[[:alpha:]]+','#', 1, 1);
+--According to CBRD-23641 do not include
+--SET NAMES iso88591;
+--SELECT REGEXP_REPLACE('Kłak Aleksander', '[[:alpha:]]+','#', 1, 1);
