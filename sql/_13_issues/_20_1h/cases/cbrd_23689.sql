b796759aa8aef00fef369c2b2a99ffa15976160b
tw-kang <twkang@cubrid.com>
2020-06-24 10:43:28 +0900

    improve case

--- sql/_13_issues/_20_1h/cases/cbrd_23689.sql
+++ sql/_13_issues/_20_1h/cases/cbrd_23689.sql
@@ -4,8 +4,8 @@ drop table if exists tmp2;
 
 CREATE TABLE tmp1 (col1 VARCHAR(10), col2 VARCHAR(10));
 CREATE TABLE tmp2 (col1 VARCHAR(10), col2 VARCHAR(10));
-INSERT INTO tmp1 SELECT ROWNUM, (ROWNUM +1)/10000 FROM db_class a, db_class b, db_class c LIMIT 10000;
-INSERT INTO tmp2 SELECT ROWNUM, ROWNUM FROM db_class a, db_class b, db_class c LIMIT 10000;
+INSERT INTO tmp1 SELECT ROWNUM, (ROWNUM +1)/100000 FROM table({0,1,2,3,4,5,6,7,8,9}) a, table({0,1,2,3,4,5,6,7,8,9}) b, table({0,1,2,3,4,5,6,7,8,9}) c, table({0,1,2,3,4,5,6,7,8,9}) d LIMIT 10000;
+INSERT INTO tmp2 SELECT ROWNUM, ROWNUM from table({0,1,2,3,4,5,6,7,8,9}) a, table({0,1,2,3,4,5,6,7,8,9}) b, table({0,1,2,3,4,5,6,7,8,9}) c, table({0,1,2,3,4,5,6,7,8,9}) d LIMIT 100000;
 CREATE INDEX cub11 ON tmp1(col1);
 CREATE INDEX cub22 ON tmp1(col1,col2);
 CREATE INDEX cub11 ON tmp2(col1);
@@ -16,6 +16,7 @@ set optimization level 513;
 
 select /*+ recompile */ count(1)
 from tmp1 a, tmp1 b, tmp2 c
+--where a.col1 = b.col1
 where a.col1 = b.col1(+)
 and a.col1 = c.col1
 and c.col2 = 2;
