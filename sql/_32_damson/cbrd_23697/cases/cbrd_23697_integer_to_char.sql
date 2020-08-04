04e78c59c568534cda87eefeaf9cd8d4237e7ba3
Song <joykevin@cubrid.com>
2020-08-04 10:32:05 +0900

    Added the new test cases.

---
+++ sql/_32_damson/cbrd_23697/cases/cbrd_23697_integer_to_char.sql
@@ -0,0 +1,23 @@
+drop if exists t1;
+
+create table t1 (a int, b char(3));
+
+insert into t1 values(1, 'ab');
+
+update t1 set b = 1 where a = 1;
+update t1 set b = 'abc' where a = 1;
+
+select * from t1 order by a;
+
+drop table t1;
+
+create table t1 (a int, b char(3));
+
+insert into t1 values(1, 'ab');
+
+update t1 set b = 1 where a = 1;
+update /*+ recompile */ t1 set b = 'abc' where a = 1;
+
+select * from t1 order by a;
+
+drop table t1;
