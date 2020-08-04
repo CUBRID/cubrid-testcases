99c9901368d3bd29f2fbc8a7e8f715306520d1f0
Song <joykevin@cubrid.com>
2020-08-04 14:18:27 +0900

    Modified the testcase

--- sql/_32_damson/cbrd_23697/cases/cbrd_23697_integer_to_char.sql
+++ sql/_32_damson/cbrd_23697/cases/cbrd_23697_integer_to_char.sql
@@ -21,3 +21,26 @@ update /*+ recompile */ t1 set b = 'abc' where a = 1;
 select * from t1 order by a;
 
 drop table t1;
+
+create table t1 (a int, b char(10));
+
+insert into t1 values(1, 'ab');
+
+update t1 set b = 1 where a = 1;
+update t1 set b = 'abc' where a = 1;
+
+select * from t1 order by a;
+
+drop table t1;
+
+create table t1 (a int, b char(10));
+
+insert into t1 values(1, 'ab');
+
+update t1 set b = 1 where a = 1;
+update /*+ recompile */ t1 set b = 'abc' where a = 1;
+
+select * from t1 order by a;
+
+drop table t1;
+
