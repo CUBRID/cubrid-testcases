a04037c5eb22161e1abd13d4ab85c58e2ac280a9
Song <joykevin@cubrid.com>
2020-08-05 17:53:14 +0900

    Added the new test case

---
+++ sql/_32_damson/cbrd-23650/cases/cbrd_23650_rownum_into_aggregate_with_over.sql
@@ -0,0 +1,11 @@
+drop table if exists t1;
+
+create table t1 (a int primary key, b int);
+
+insert into t1 values (1, 1), (2, 1), (3, 1), (4, 1), (5, 2), (6, 2), (7, 2), (8, 2);
+
+select rownum, sum(rownum) over (partition by a) from t1;
+select sum(rownum) over (partition by b) from t1;
+select count(*) over () from t1 limit 2;
+
+drop table t1;
