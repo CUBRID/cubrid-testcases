f05b2ef35c0eb92db14193d79623b99951bd41dd
ssihil <ssihil@cubrid.com>
2020-08-05 16:51:29 +0900

    using prepare and execute

--- sql/_13_issues/_20_2h/cases/cbrd_22925.sql
+++ sql/_13_issues/_20_2h/cases/cbrd_22925.sql
@@ -2,19 +2,22 @@ drop table if exists tmp1;
 
 CREATE TABLE tmp1(col1 INT, col2 INT, col3 int);
 CREATE INDEX idx ON tmp1(col1,col2);
+INSERT INTO tmp1 VALUES(1, 2, 1);
 
-select 1
+PREPARE stmt from 'select 1
   from tmp1
  where col3 = 1
    and col3 <> 2
    and col2 in (?,?)
-   and col1 in ('1','2');
+   and col1 in (1,2)';
+EXECUTE stmt using 1,2;
 
-select 1
+PREPARE stmt from 'select 1
   from tmp1
  where col2 in (?,?)
-   and col1 in ('1','2')
+   and col1 in (1,2)
    and col3 = 1
-   and col3 <> 2;
+   and col3 <> 2';
+EXECUTE stmt using 1,2;
 
 drop table tmp1;
