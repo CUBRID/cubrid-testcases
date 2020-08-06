3da9eaecd5f66c8216b3d4bf454594d5e1580ee0
Song <joykevin@cubrid.com>
2020-08-06 09:41:24 +0900

    Modified the test case

--- sql/_32_damson/cbrd-23650/cases/cbrd_23650_rownum_into_aggregate_with_over.sql
+++ sql/_32_damson/cbrd-23650/cases/cbrd_23650_rownum_into_aggregate_with_over.sql
@@ -7,5 +7,8 @@ insert into t1 values (1, 1), (2, 1), (3, 1), (4, 1), (5, 2), (6, 2), (7, 2), (8
 select rownum, sum(rownum) over (partition by a) from t1;
 select sum(rownum) over (partition by b) from t1;
 select count(*) over () from t1 limit 2;
+select rownum, sum(rownum) over (partition by a) from t1;
+select sum(rownum) over (partition by b) from t1;
+select count(*) over () from t1 limit 2;
 
 drop table t1;
