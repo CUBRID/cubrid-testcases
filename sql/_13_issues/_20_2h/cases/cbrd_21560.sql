d71a4be9ff3db401111cfe597ef9eb4e1eb07b18
soyeon <soyeon@cubrid.com>
2020-10-15 15:11:01 +0900

    add default parameter to TRUNC and ROUND functions.

--- sql/_13_issues/_20_2h/cases/cbrd_21560.sql
+++ sql/_13_issues/_20_2h/cases/cbrd_21560.sql
@@ -14,3 +14,11 @@ create view foo_v_2 as select round(sysdate-b) d from foo;
 select * from foo_v_2;
 
 drop table foo;
+
+select round(datetime'2012-12-26 12:10:10', 'dd');
+select round(datetime'2012-12-26 12:10:10', 'default');
+select trunc(datetime'2012-12-26 12:10:10', 'dd');
+select TRUNC(datetime'2012-12-26 12:10:10', 'default');
+select round(34567.34567, 'default'), round(34567.34567, 0);
+select round(34567.94567, 'default'), round(34567.94567, 0);
+select TRUNC(34567.34567, 'default'), TRUNC(34567.34567, 0);
