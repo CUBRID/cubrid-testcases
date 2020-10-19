c59535a221429ff9142d367e99098d507112f341
soyeon <38178158+sy052@users.noreply.github.com>
2020-10-19 10:25:35 +0900

    [CBRD-21560] Add new test case for Syntax errors when a view containing TRUNC() or ROUND() function is fetched. (#883)
    * [CBRD-21560] Add new test case for Syntax errors when a view containing TRUNC() or ROUND() function is fetched.
    
    * add default parameter to TRUNC and ROUND functions.
    
    * modified sysdate of view to fixed date
    
    * added select round() and trunc() function with no factor
    
    * modified select round() and select trunc().

---
+++ sql/_13_issues/_20_2h/cases/cbrd_21560.sql
@@ -0,0 +1,33 @@
+drop table if exists foo;
+
+create table foo (a int, b date);
+
+insert into foo values (1, '20170821');
+insert into foo values (2, NULL);
+
+drop view if exists foo_v;
+
+create view foo_v as select trunc(date'2020-10-14'-b) d from foo;
+
+select * from foo_v;
+
+drop view if exists foo_v_2;
+
+create view foo_v_2 as select round(date'2020-10-14'-b) d from foo;
+
+select * from foo_v_2;
+
+drop table foo;
+drop view foo_v;
+drop view foo_v_2;
+
+select round(datetime'2012-12-26 12:10:10', 'dd');
+select round(datetime'2012-12-26 12:10:10', 'default');
+select round(datetime'2012-12-26 12:10:10');
+select trunc(datetime'2012-12-26 12:10:10', 'dd');
+select trunc(datetime'2012-12-26 12:10:10', 'default');
+select trunc(datetime'2012-12-26 12:10:10');
+select round(34567.34567, 'default'), round(34567.34567, 0), round(34567.34567);
+select round(34567.94567, 'default'), round(34567.94567, 0), round(34567.94567);
+select trunc(34567.34567, 'default'), trunc(34567.34567, 0), trunc(34567.34567);
+select trunc(34567.94567, 'default'), trunc(34567.94567, 0), trunc(34567.94567);
