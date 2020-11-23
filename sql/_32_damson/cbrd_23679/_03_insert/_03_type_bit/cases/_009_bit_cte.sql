23c0b2aa02061e1606774ef3475b10710c8f662e
tw-kang <twkang@cubrid.com>
2020-11-23 14:38:52 +0900

    add new cases for insert bit

---
+++ sql/_32_damson/cbrd_23679/_03_insert/_03_type_bit/cases/_009_bit_cte.sql
@@ -0,0 +1,49 @@
+set system parameters 'allow_truncated_string=no';
+---------- INSERT INTO ...
+---------- WITH cte AS (SELECT ...)
+---------- SELECT * FROM cte ORDERBY 1;
+
+drop table if exists t2;
+drop table if exists t1;
+
+create table t2 (parent_id int, code bit(20));
+insert into t2 values (1, b'11');
+insert into t2 values (-1, b'1111');
+insert into t2 values (1, b'101010');
+insert into t2 values (5, b'10101010');
+insert into t2 values (1, b'1010111110101111');
+
+-- precision > value : OK
+drop table if exists t1;
+create table t1 (col1 bit(20));
+show columns from t1;
+insert into t1 with 
+a AS (SELECT code FROM t2 WHERE parent_id = 1),
+b AS (SELECT code FROM t2 WHERE parent_id = 5)
+SELECT * FROM a UNION ALL SELECT * FROM b ORDER BY 1;
+select * from t1 order by 1;
+
+-- precision = value : OK
+drop table if exists t1;
+create table t1 (col1 bit(16));
+show columns from t1;
+insert into t1 with 
+a AS (SELECT code FROM t2 WHERE parent_id = 1),
+b AS (SELECT code FROM t2 WHERE parent_id = 5)
+SELECT * FROM a UNION ALL SELECT * FROM b ORDER BY 1;
+select * from t1 order by 1;
+
+-- precision < value :data overflow
+drop table if exists t1;
+create table t1 (col1 bit(4));
+show columns from t1;
+insert into t1 with 
+a AS (SELECT code FROM t2 WHERE parent_id = 1),
+b AS (SELECT code FROM t2 WHERE parent_id = 5)
+SELECT * FROM a UNION ALL SELECT * FROM b ORDER BY 1;
+select * from t1 order by 1;
+
+drop table t1;
+drop table t2;
+
+set system parameters 'allow_truncated_string=no';
