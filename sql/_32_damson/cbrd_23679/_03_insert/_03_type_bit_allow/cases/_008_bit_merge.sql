7e45a24c1f7cb359b06ee0501e455033c9119ea1
tw-kang <twkang@cubrid.com>
2020-11-25 17:24:05 +0900

    modified cases

--- sql/_32_damson/cbrd_23679/_03_insert/_03_type_bit_allow/cases/_008_bit_merge.sql
+++ sql/_32_damson/cbrd_23679/_03_insert/_03_type_bit_allow/cases/_008_bit_merge.sql
@@ -23,7 +23,7 @@ create table s1 (col1 int, col2 bit(10));
 insert into s1 values (2, b'1010111110');
 merge into t1 using s1 on t1.col1 = s1.col1
 when matched then update set t1.col2 = s1.col2
-ween not matched then insert values (s1.col1, s1.col2);
+when not matched then insert values (s1.col1, s1.col2);
 select * from t1 order by 1;
 
 -- precision < value :truncated string
--- sql/_32_damson/cbrd_23679/_03_insert/_03_type_bit_allow/cases/_009_bit_cte.sql
+++ sql/_32_damson/cbrd_23679/_03_insert/_03_type_bit_allow/cases/_009_bit_cte.sql
@@ -8,10 +8,12 @@ drop table if exists t1;
 
 create table t2 (parent_id int, code bit(20));
 insert into t2 values (1, b'11');
-insert into t2 values (-1, b'1111');
-insert into t2 values (1, b'101010');
-insert into t2 values (5, b'10101010');
-insert into t2 values (1, b'1010111110101111');
+insert into t2 values (1, b'1111');
+insert into t2 values (2, b'101010');
+insert into t2 values (2, b'10101010');
+insert into t2 values (3, b'1010111110101111');
+insert into t2 values (3, b'1111111111111111');
+insert into t2 values (4, b'1110111011101110');
 
 -- precision > value : OK
 drop table if exists t1;
@@ -19,8 +21,11 @@ create table t1 (col1 bit(20));
 show columns from t1;
 insert into t1 with 
 a AS (SELECT code FROM t2 WHERE parent_id = 1),
-b AS (SELECT code FROM t2 WHERE parent_id = 5)
-SELECT * FROM a UNION ALL SELECT * FROM b ORDER BY 1;
+b AS (SELECT code FROM t2 WHERE parent_id = 2),
+c AS (SELECT code FROM t2 WHERE parent_id = 3)
+SELECT * FROM a 
+UNION ALL SELECT * FROM b
+UNION ALL SELECT * FROM c ORDER BY 1;
 select * from t1 order by 1;
 
 -- precision = value : OK
@@ -28,9 +33,10 @@ drop table if exists t1;
 create table t1 (col1 bit(16));
 show columns from t1;
 insert into t1 with 
-a AS (SELECT code FROM t2 WHERE parent_id = 1),
-b AS (SELECT code FROM t2 WHERE parent_id = 5)
-SELECT * FROM a UNION ALL SELECT * FROM b ORDER BY 1;
+c AS (SELECT code FROM t2 WHERE parent_id = 3),
+d AS (SELECT code FROM t2 WHERE parent_id = 4)
+SELECT * FROM c 
+UNION ALL SELECT * FROM d ORDER BY 1;
 select * from t1 order by 1;
 
 -- precision < value :truncated string
@@ -38,9 +44,12 @@ drop table if exists t1;
 create table t1 (col1 bit(4));
 show columns from t1;
 insert into t1 with 
-a AS (SELECT code FROM t2 WHERE parent_id = 1),
-b AS (SELECT code FROM t2 WHERE parent_id = 5)
-SELECT * FROM a UNION ALL SELECT * FROM b ORDER BY 1;
+b AS (SELECT code FROM t2 WHERE parent_id = 2),
+c AS (SELECT code FROM t2 WHERE parent_id = 3),
+d AS (SELECT code FROM t2 WHERE parent_id = 4)
+SELECT * FROM b 
+UNION ALL SELECT * FROM c 
+UNION ALL SELECT * FROM d ORDER BY 1;
 select * from t1 order by 1;
 
 drop table t1;
