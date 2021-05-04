720da22391f85ae4373718cf513653e0992b8c6b
tw-kang <twkang@cubrid.com>
2021-05-04 17:56:02 +0900

    modified testcase

--- sql/_13_issues/_21_1h/cases/cbrd_23878.sql
+++ sql/_13_issues/_21_1h/cases/cbrd_23878.sql
@@ -7,11 +7,15 @@ create table t1 (col1 varchar, col2 varchar(1073741823), col3 varchar(10));
 create table tbl1 as select * from t1;
 select attr_name, data_type, prec from db_attribute where attr_name like '%col%' and class_name='tbl1' order by 1;
 show columns from tbl1;
+show create table tbl1;
+desc tbl1;
 
 create table t2 (col1 char, col2 char(268435455), col3 char(10));
 create table tbl2 as select * from t2;
 select attr_name, data_type, prec from db_attribute where attr_name like '%col%' and class_name='tbl2' order by 1;
 show columns from tbl2;
+show create table tbl2;
+desc tbl2;
 
 drop table t1;
 drop table t2;
