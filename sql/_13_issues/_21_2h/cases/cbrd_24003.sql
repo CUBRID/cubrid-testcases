f3ae98c61a4b6827efde1e9db60ead3bf0f6325a
zionyun <seeon0001@naver.com>
2021-07-21 14:52:19 +0900

    modify testcase

--- sql/_13_issues/_21_2h/cases/cbrd_24003.sql
+++ sql/_13_issues/_21_2h/cases/cbrd_24003.sql
@@ -6,3 +6,9 @@ create view v1 as
 SELECT count(*) cnt
 FROM tmp A
 WHERE col1 = ( SELECT col1 FROM tmp ORDER BY col2 LIMIT 1 );
+show tables;
+
+SELECT * FROM v1;
+
+drop table if exists tmp;
+drop view if exists v1;
