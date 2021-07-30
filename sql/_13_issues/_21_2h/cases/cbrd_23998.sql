7c32adfbf5fe98f8019f7e446e48282d7072035c
zionyun <seeon0001@naver.com>
2021-07-30 17:34:49 +0900

    [CBRD-23998] Add new case for Wrong repr_id with the select_record_info hint

---
+++ sql/_13_issues/_21_2h/cases/cbrd_23998.sql
@@ -0,0 +1,14 @@
+drop table if exists tbl;
+create table tbl (a int);
+insert into tbl values(3);
+alter table tbl add column b int;
+insert into tbl values(4,4);
+select /*+ select_record_info */ * from tbl;
+drop table if exists tbl;
+
+create table tbl (a char(20000));
+insert into tbl values('abcde');
+alter table tbl add column b char(20000);
+insert into tbl values('abc','abc');
+select /*+ select_record_info */ * from tbl;
+drop table if exists tbl;
