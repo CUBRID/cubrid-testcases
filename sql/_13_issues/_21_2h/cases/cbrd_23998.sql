0963922f36bfa5021deac3b4ea7d0f4afbaafa09
zionyun <seeon0001@naver.com>
2021-08-02 16:03:48 +0900

    Delete columns except t_reprid

--- sql/_13_issues/_21_2h/cases/cbrd_23998.sql
+++ sql/_13_issues/_21_2h/cases/cbrd_23998.sql
@@ -3,12 +3,12 @@ create table tbl (a int);
 insert into tbl values(3);
 alter table tbl add column b int;
 insert into tbl values(4,4);
-select /*+ select_record_info */ * from tbl;
+select /*+ select_record_info */ t_reprid from tbl;
 drop table if exists tbl;
 
 create table tbl (a char(20000));
 insert into tbl values('abcde');
 alter table tbl add column b char(20000);
 insert into tbl values('abc','abc');
-select /*+ select_record_info */ * from tbl;
+select /*+ select_record_info */ t_reprid from tbl;
 drop table if exists tbl;
