46ececec427cffb6f4ecba155f38d76be79c1869
soyeon <soyeon@cubrid.com>
2021-08-10 15:35:38 +0900

    modified view name

--- sql/_13_issues/_21_2h/cases/cbrd_24009.sql
+++ sql/_13_issues/_21_2h/cases/cbrd_24009.sql
@@ -9,7 +9,7 @@ create or replace view tmp_view as select * from (select 1 from tmp where col1 i
 select count(*) from tmp_view;
 
 drop table if exists tmp;
-drop view if exists vi;
+drop view if exists v1;
 create table tmp (col1 int) DONT_REUSE_OID;
 create or replace view v1 as select * from tmp;
 
@@ -17,4 +17,4 @@ INSERT INTO v1 SELECT * FROM v1 LIMIT 0;
 
 drop table if exists tmp;
 drop view if exists tmp_view;
-drop view if exists vi;
+drop view if exists v1;
