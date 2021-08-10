11aff3a5ef7c301769e5c07c7a0fa440aae67b56
soyeon <soyeon@cubrid.com>
2021-08-10 14:06:41 +0900

    [CBRD-24009] modified test case for Plan cache is not updated when view changes including inline view.

---
+++ sql/_13_issues/_21_2h/cases/cbrd_24009.sql
@@ -0,0 +1,21 @@
+drop table if exists tmp;
+create table tmp (col1 int);
+insert into tmp value(1),(1),(2);
+drop view if exists tmp_view;
+create or replace view tmp_view as select * from (select 1 from tmp where col1 = 2);
+select count(*) from tmp_view;
+
+drop view if exists tmp_view;
+create or replace view tmp_view as select * from (select 1 from tmp where col1 in ('1','2'));
+select count(*) from tmp_view;
+
+drop table if exists tmp;
+drop view if exists vi;
+create table tmp (col1 int) DONT_REUSE_OID;
+create or replace view v1 as select * from tmp;
+
+INSERT INTO v1 SELECT * FROM v1 LIMIT 0;
+
+drop table if exists tmp;
+drop view if exists tmp_view;
+drop view if exists vi;
