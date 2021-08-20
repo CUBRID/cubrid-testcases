67e2a00fc561741926c17cfa5795977e9265b278
soyeon <soyeon@cubrid.com>
2021-08-20 19:26:11 +0900

    added scalar subquery and inline view query type.

--- sql/_13_issues/_21_2h/cases/cbrd_24013.sql
+++ sql/_13_issues/_21_2h/cases/cbrd_24013.sql
@@ -1,8 +1,9 @@
-drop table if exists t1;
+drop table if exists t;
 
-create table t1 (col1 int, col2 varchar(3), col3 int, col4 int);
-insert into t1 values (1, 'ABC', 2, 3), (2, 'BCD', 3, 4), (3, 'CDE', 4, 5), (4, 'DEF', 5, 6), (5, 'EFG', 6, 7);
-set optimization level 513;
-select /*+ recompile */ * from t1 where (col1,col2) = (select col1,col2 from t1 where col1 in (3,4) order by col4 desc limit 1);
+create table t (cola int, colb varchar(3), colc int, cold int);
+insert into t values (1, 'ABC', 2, 3), (2, 'BCD', 3, 4), (3, 'CDE', 4, 5), (4, 'DEF', 5, 6), (5, 'EFG', 6, 7);
+select /*+ recompile */ * from t where (cola,colb) = (select cola,colb from t where cola in (3,4) order by cold desc limit 1);
+select cola, (select colb from t where cola = a.cola order by cold desc limit 1) from t a where cola=4;
+select * from (select colb from t where cola in (3,4) order by cold desc limit 1) where colb='DEF';
 
-drop table t1;
+drop table t;
