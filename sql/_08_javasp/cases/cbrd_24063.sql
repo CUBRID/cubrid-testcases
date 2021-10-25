82952b0fe0d27337d1fd395aad27d135282e8ff7
zionyun <seeon0001@naver.com>
2021-10-25 14:42:38 +0900

    modfiy script to make stable

--- sql/_08_javasp/cases/cbrd_24063.sql
+++ sql/_08_javasp/cases/cbrd_24063.sql
@@ -1,5 +1,5 @@
-CREATE FUNCTION sp() RETURN string as language java name 'SpTest8.SP() return java.lang.String';
-CREATE FUNCTION sp2() RETURN string as language java name 'SpTest8.SP2() return java.lang.String';
+CREATE OR REPLACE FUNCTION sp() RETURN string as language java name 'SpTest8.SP() return java.lang.String';
+CREATE OR REPLACE FUNCTION sp2() RETURN string as language java name 'SpTest8.SP2() return java.lang.String';
 call sp();
 call sp2();
 drop FUNCTION sp;
