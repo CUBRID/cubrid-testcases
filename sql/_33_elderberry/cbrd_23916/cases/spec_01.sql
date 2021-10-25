9071f0c3b7a060b1ce821673853206d23dd5b8e8
sjkim777 <42533491+sjkim777@users.noreply.github.com>
2021-10-25 09:59:39 +0900

    [CUBRIDQA-1127] Backport from dev (#987)

---
+++ sql/_33_elderberry/cbrd_23916/cases/spec_01.sql
@@ -0,0 +1,25 @@
+drop table if exists tbl;
+create table tbl (a int);
+insert into tbl values(3);
+truncate tbl;
+select count(*) from tbl;
+truncate tbl cascade;
+select count(*) from tbl;
+drop tbl;
+
+create table tbl (a int primary key);
+insert into tbl values(3);
+truncate tbl;
+select count(*) from tbl;
+truncate tbl cascade;
+select count(*) from tbl;
+drop tbl;
+
+create table tbl (a int primary key);
+alter table tbl add column b int references tbl(a);
+insert into tbl values(3,3);
+truncate tbl;
+select count(*) from tbl;
+truncate tbl cascade;
+select count(*) from tbl;
+drop tbl;
