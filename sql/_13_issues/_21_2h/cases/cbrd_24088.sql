6455ec7708fb49ce7dcb09f15c324afbdc7c8a36
soyeon <soyeon@cubrid.com>
2021-10-26 15:19:34 +0900

    modified test case.

--- sql/_13_issues/_21_2h/cases/cbrd_24088.sql
+++ sql/_13_issues/_21_2h/cases/cbrd_24088.sql
@@ -5,7 +5,7 @@ create table tt (c1 varchar(50), c2 int);
 insert into tt (
 select cast(rownum as varchar(50)), rownum from _db_class a, _db_class b, _db_class c
 union
-select cast(rownum || '  ' as varchar(50)), rownum from _db_class a, _db_class b
+select cast(rownum || '  ' as varchar(50)), rownum from _db_class a, _db_class b limit 10000
 );
 create index idx_tt on tt(c1);
 create index idx_tt_d on tt(c1 desc);
