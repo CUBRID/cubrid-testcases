9046031490ca3309a125c4571d6c9c15798b10de
soyeon <soyeon@cubrid.com>
2021-11-25 22:57:48 +0900

    add about correlated and no push predicate test cases

---
+++ sql/_33_elderberry/cbrd_24011/cases/correlated_const_value.sql
@@ -0,0 +1,13 @@
+drop table if exists subquery_big;
+create table subquery_big (col_a int, col_b int, col_c int);
+set optimization level 513;
+
+select /*+ recompile */ count(*) 
+	from (select col_b, avg(col_c) 
+		from subquery_big group by col_b) a,(select col_b, min(col_c), max(col_c) 
+			from subquery_big where col_b=1 group by col_b
+			) b
+		where a.col_b=b.col_b;
+
+drop table if exists subquery_big;
+
