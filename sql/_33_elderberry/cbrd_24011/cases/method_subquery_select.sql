3dee410a76052ca82dbf5ec70dca127e8e7ef195
soyeon <38178158+sy052@users.noreply.github.com>
2021-12-20 13:00:32 +0900

    [CBRD-24011] Add new test cases for predicate push. (#1019) (#1027)
    * [CBRD-24011] Add new test cases for predicate push.
    
    * deleted unnecessary file
    
    * add on condition term test case
    
    * add about method and nullable outer join test cases
    
    * add about query, subquery select test cases
    
    * add about aggregate, method subquery select test cases
    
    * add the answer files of aggregate, method subquery select TC
    
    * modified the answer file of method subquery select TC
    
    * add about correlated and no push predicate test cases
    
    * deleted console log file
    
    * add nested subqueries test case
    
    * modified test cases
    
    * modified inst_num test case
    
    * deleted the drop index phrase
    
    * revised the test cases
    
    * reflected the review
    
    * modified the inline view creation query

---
+++ sql/_33_elderberry/cbrd_24011/cases/method_subquery_select.sql
@@ -0,0 +1,30 @@
+drop table if exists tab_a, tab_b;
+create table tab_a (col_a varchar(100), col_b varchar(100));
+create table tab_b (col_a varchar(100), col_b varchar(100));
+insert into tab_a select to_char(rownum mod 100) col_a, to_char(rownum) col_b from table({0,1,2,3,4,5,6,7,8,9}) a, table({0,1,2,3,4,5,6,7,8,9}) b,table({0,1,2,3,4,5,6,7,8,9}) c,table({0,1,2,3,4,5,6,7,8,9}) d,table({0,1,2,3,4,5,6,7,8,9}) e,table({0,1,2,3,4,5,6,7,8,9}) f limit 1000;
+insert into tab_b select to_char(rownum mod 100) col_a, to_char(rownum) col_b from table({0,1,2,3,4,5,6,7,8,9}) a, table({0,1,2,3,4,5,6,7,8,9}) b,table({0,1,2,3,4,5,6,7,8,9}) c,table({0,1,2,3,4,5,6,7,8,9}) d,table({0,1,2,3,4,5,6,7,8,9}) e,table({0,1,2,3,4,5,6,7,8,9}) f limit 1000;
+create index idx on tab_a(col_a,col_b);
+create index idx on tab_b(col_a,col_b);
+
+create function test() return int
+as language java
+name 'SpTest.testInt10() return int';
+
+select /*+ recompile */ count(*)
+from tab_a a
+      ,(select col_a, test() col_b from tab_b b group by col_a ) b
+where a.col_a = b.col_a
+  and b.col_b = 10;
+
+create or replace view v_a as select col_a, test() col_b from tab_b b group by col_a;
+
+select /*+ recompile */ count(*)
+from tab_a a
+      ,v_a b
+where a.col_a = b.col_a
+  and b.col_b = 10;
+
+drop function test;
+drop view v_a;
+drop table if exists tab_a, tab_b;
+
