2e7fc2c9413e7887cfe7d1c64bfb83b9ad2614c3
zionyun <seeon0001@naver.com>
2022-01-06 18:13:57 +0900

    revise tc and answers

--- sql/_10_connect_by/_12_oid_subclass/cases/oo_cycle_nocycle.sql
+++ sql/_10_connect_by/_12_oid_subclass/cases/oo_cycle_nocycle.sql
@@ -3,7 +3,7 @@ drop tree, tree1, tree2, tree3;
 CREATE TABLE tree(ID INT, ParentID INT, text VARCHAR(32));
 create table tree1 under tree;
 create table tree2 under tree;
-create table tree3 under tree2
+create table tree3 under tree2;
 
 INSERT INTO tree1 VALUES(1,NULL,'A');
 INSERT INTO tree2 VALUES (2,NULL,'B');
