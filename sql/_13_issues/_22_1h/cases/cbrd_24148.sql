2fa73bbe3b0694a5c444871f920cad8e044e5ae4
tw-kang <twkang@cubrid.com>
2022-01-06 19:58:44 +0900

    modified testcase for optimize data size and running time, add trace for hash list scan result

--- sql/_13_issues/_22_1h/cases/cbrd_24148.sql
+++ sql/_13_issues/_22_1h/cases/cbrd_24148.sql
@@ -1,7 +1,10 @@
 DROP TABLE IF EXISTS tbl_hls ;
 CREATE TABLE tbl_hls (a INTEGER, b INTEGER, c INTEGER);
-insert into tbl_hls select r,r,r from (select rand(1)%5000 as r from db_class a, db_class b, db_class c, db_class d limit 1000000);
+insert into tbl_hls select r,r,r from (select rand(1)%5000 as r from db_class a, db_class b, db_class c, db_class d limit 410000);
 
-select /*+ recompile ordered */ count(*) from (select * from tbl_hls limit 0,100000) a, (select * from tbl_hls limit 0,1000000) b where a.a = b.a and a.b = b.b and a.c = b.c;
+set trace on;
+select /*+ recompile ordered */ count(*) from (select * from tbl_hls limit 0,10000) a, (select * from tbl_hls limit 0,410000) b where a.a = b.a and a.b = b.b and a.c = b.c;
+show trace;
+set trace off;
 
 drop table tbl_hls;
