9924c22acd163f0f15f91f89f424a4b91c423631
zionyun <seeon0001@naver.com>
2022-01-10 17:22:25 +0900

    revise tc and answers

--- sql/_09_64bit/_02_bigint/cases/1052.sql
+++ sql/_09_64bit/_02_bigint/cases/1052.sql
@@ -8,7 +8,7 @@ insert into test_class(bigint_col) values (100);
 insert into test_class(bigint_col) values (-100);
 insert into test_class(bigint_col) values (0);
 
-select * from test_class where bigint_col > 0  using index none  order by 1 ;
+select * from test_class where bigint_col > 0  using index none  order by 1;
 select * from test_class where bigint_col > 0  using index idx_bigint_col  order by 1; 
 select * from test_class where bigint_col > 0  using index idx_bigint_col(+);
 
--- sql/_13_issues/_11_1h/cases/bug_bts_4208.sql
+++ sql/_13_issues/_11_1h/cases/bug_bts_4208.sql
@@ -1,8 +1,8 @@
 create table t ( a timestamp, b timestamp);
 insert into t values ('2011-02-08 01:00:00', '2011-02-08 04:00:00');
 
-prepare s from 'select * from t where a between ? and ? or b between ? and ?' 
-execute s using '2011-02-08 00:00:00' , '2011-02-08 01:00:00', '2011-02-08 03:00:00', '2011-02-08 06:00:00' 
+prepare s from 'select * from t where a between ? and ? or b between ? and ?';
+execute s using '2011-02-08 00:00:00' , '2011-02-08 01:00:00', '2011-02-08 03:00:00', '2011-02-08 06:00:00';
 drop prepare s;
 
 drop table t;
