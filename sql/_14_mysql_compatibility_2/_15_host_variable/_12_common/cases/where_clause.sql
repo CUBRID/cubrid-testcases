0bb78a768cc0143ff5ce45e186c72bdc49396c3d
zionyun <seeon0001@naver.com>
2022-01-21 15:07:19 +0900

    revise tc and answers 255~274

--- sql/_14_mysql_compatibility_2/_15_host_variable/_12_common/cases/where_clause.sql
+++ sql/_14_mysql_compatibility_2/_15_host_variable/_12_common/cases/where_clause.sql
@@ -14,25 +14,25 @@ create index i_t1_i1 on t1(i1);
 
 select * from t1 where i1=i2+31.0 order by 1;
 	
-prepare st from 'select * from t1 where i1=i2+?  order by 1'
+prepare st from 'select * from t1 where i1=i2+?  order by 1';
 execute st using 31.0;	
 
 	
 select /*+ recompile ordered */ * from t1 where i1=(select max(i1 + 0.0 ) from t1)  order by 1;
 	
-prepare st from 'select * from t1 where i1=(select max(i1 + ? ) from t1)  order by 1'
+prepare st from 'select * from t1 where i1=(select max(i1 + ? ) from t1)  order by 1';
 execute st using 0.0;	
 
 
 select * from t1 where i1=(select max(i1 + 0.0 ) from t1) or i1=(select min(i1 + 0.0 ) from t1)  order by 1;
 
-prepare st from 'select * from t1 where i1=(select max(i1 + ? ) from t1) or i1=(select min(i1 + ? ) from t1)  order by 1'
+prepare st from 'select * from t1 where i1=(select max(i1 + ? ) from t1) or i1=(select min(i1 + ? ) from t1)  order by 1';
 execute st using 0.0,0.0;	
 
 
 select 1 from t1,table(set{i2}) as t(i) where i1 in set{t.i+2};
 
-prepare st from 'select 1 from t1,table(set{i2}) as t(i) where i1 in set{t.i+?}'
+prepare st from 'select 1 from t1,table(set{i2}) as t(i) where i1 in set{t.i+?}';
 execute st using 2.0;	
 
 drop table t1;
@@ -51,24 +51,24 @@ insert into t1 values(2,-1);
 
 select * from t1 where i1=i2+31.0 order by 1;
 	
-prepare st from 'select * from t1 where i1=i2+?  order by 1'
+prepare st from 'select * from t1 where i1=i2+?  order by 1';
 execute st using 31.0;	
 
 	
 select /*+ recompile ordered */ * from t1 where i1=(select max(i1 + 0.0 ) from t1)  order by 1;
 	
-prepare st from 'select * from t1 where i1=(select max(i1 + ? ) from t1)  order by 1'
+prepare st from 'select * from t1 where i1=(select max(i1 + ? ) from t1)  order by 1';
 execute st using 0.0;
 
 select * from t1 where i1=(select max(i1 + 0.0 ) from t1) or i1=(select min(i1 + 0.0 ) from t1)  order by 1;
 
-prepare st from 'select * from t1 where i1=(select max(i1 + ? ) from t1) or i1=(select min(i1 + ? ) from t1)  order by 1'
+prepare st from 'select * from t1 where i1=(select max(i1 + ? ) from t1) or i1=(select min(i1 + ? ) from t1)  order by 1';
 execute st using 0.0,0.0;	
 
 
 select 1 from t1,table(set{i2}) as t(i) where i1 in set{t.i+2};
 
-prepare st from 'select 1 from t1,table(set{i2}) as t(i) where i1 in set{t.i+?}'
+prepare st from 'select 1 from t1,table(set{i2}) as t(i) where i1 in set{t.i+?}';
 execute st using 2.0;	
 
 drop prepare st;
