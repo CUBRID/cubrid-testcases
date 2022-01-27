ffd0a0b3fcdedfc3a40ac650246326e699e24816
soyeon <soyeon@cubrid.com>
2022-01-27 15:01:15 +0900

    revised mysql_compatibility test cases

--- sql/_12_mysql_compatibility/_11_code_coverage/_02_boolean_constructs/_02_is_construct/cases/_005_is.sql
+++ sql/_12_mysql_compatibility/_11_code_coverage/_02_boolean_constructs/_02_is_construct/cases/_005_is.sql
@@ -2,19 +2,19 @@ create table num_t(one int, zero int, null_c int, unk_c int);
 
 insert into num_t values (1, 0, NULL, UNKNOWN);
 
-select 1 from num_t where one<>0 IS TRUE;
-select 1 from num_t where one<>0 IS FALSE;
-select 1 from num_t where one<>0 IS UNKNOWN;
-select 1 from num_t where zero<>0 IS TRUE;
-select 1 from num_t where zero<>0 IS FALSE;
-select 1 from num_t where zero<>0 IS UNKNOWN;
-select 1 from num_t where null_c<>0 IS TRUE;
-select 1 from num_t where null_c<>0 IS FALSE;
-select 1 from num_t where null_c<>0 IS UNKNOWN;
-select 1 from num_t where unk_c<>0 IS TRUE;
-select 1 from num_t where unk_c<>0 IS FALSE;
-select 1 from num_t where unk_c<>0 IS UNKNOWN;
+select 1 from num_t where one IS TRUE;
+select 1 from num_t where one IS FALSE;
+select 1 from num_t where one IS UNKNOWN;
+select 1 from num_t where zero IS TRUE;
+select 1 from num_t where zero IS FALSE;
+select 1 from num_t where zero IS UNKNOWN;
+select 1 from num_t where null_c IS TRUE;
+select 1 from num_t where null_c IS FALSE;
+select 1 from num_t where null_c IS UNKNOWN;
+select 1 from num_t where unk_c IS TRUE;
+select 1 from num_t where unk_c IS FALSE;
+select 1 from num_t where unk_c IS UNKNOWN;
 
-select 1 from num_t where not(one<>0 IS TRUE);
+select 1 from num_t where not(one IS TRUE);
 
 drop table num_t;
--- sql/_14_mysql_compatibility_2/_11_logical_operations/cases/003_hostvar.sql
+++ sql/_14_mysql_compatibility_2/_11_logical_operations/cases/003_hostvar.sql
@@ -26,10 +26,10 @@ create index i_t_l on t(l);
 
 
 $int, $1, $int, $0, $int, $1, $int, $0
-select (1<>0 and ?:0<>0), (1<>0 and ?:1<>0), (?:2<>0 and 0<>0), (0<>0 and ?:3<>0) from db_root;
+select (1 and ?), (1 and ?), (? and 0), (0 and ?) from db_root;
 
 $int, $1, $int, $0, $int, $1, $int, $0
-select (1<>0 or ?:0<>0), (1<>0 or ?:1<>0), (?:2<>0 or 0<>0), (0<>0 or ?:3<>0) from db_root;
+select (1 or ?), (1 or ?), (? or 0), (0 or ?) from db_root;
 
 -- should fail, hostvar not allowed as truth constant
 $int, $1
@@ -37,38 +37,38 @@ select * from t where (i or j) is ?;
 
 -- some of these should fail, depending on type conversion rules
 $varchar, $testing123
-select 'ok' as verdict from db_root where ?:0<>0;
+select 'ok' as verdict from db_root where ?;
 
 $varchar, $1
-select 'ok' as verdict from db_root where ?:0<>0;
+select 'ok' as verdict from db_root where ?;
 
 $varchar, $000
-select 'not-ok' as verdict_empty from db_root where ?:0<>0;
+select 'not-ok' as verdict_empty from db_root where ?;
 
 $numeric, $23.071983
-select 'ok' as verdict from db_root where ?:0<>0;
+select 'ok' as verdict from db_root where ?;
 
 $double, $23.071983
-select 'ok' as verdict from db_root where ?:0<>0;
+select 'ok' as verdict from db_root where ?;
 
 -- this will still yield 0 because of integer conversion
 $numeric, $0.01
-select 'huh?-not-ok' as verdict_empty from db_root where ?:0<>0;
+select 'huh?-not-ok' as verdict_empty from db_root where ?;
 
 $double, $0.49
-select 'not-ok' as verdict_empty from db_root where ?:0<>0;
+select 'not-ok' as verdict_empty from db_root where ?;
 
 $date, $2010-07-23
-select 'ok' as verdict from db_root where ?:0<>0;
+select 'ok' as verdict from db_root where ?;
 
 $datetime, $2010-07-23 11:21
-select 'ok' as verdict from db_root where ?:0<>0;
+select 'ok' as verdict from db_root where ?;
 
 $time, $11:21:00
-select 'ok' as verdict from db_root where ?:0<>0;
+select 'ok' as verdict from db_root where ?;
 
 $timestamp, $2010-07-23 11:21:00
-select 'ok' as verdict from db_root where ?:0<>0;
+select 'ok' as verdict from db_root where ?;
 
 
 drop table t;
