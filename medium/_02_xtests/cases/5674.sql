79572c85a7419aebc7ad3299ccf696d1bba7bcae
zionyun <seeon0001@naver.com>
2022-02-15 15:16:48 +0900

    fix so that OID does not appear

--- medium/_02_xtests/cases/5674.sql
+++ medium/_02_xtests/cases/5674.sql
@@ -7,7 +7,8 @@ select self(p1), self(p1).product_code, self(y).product_code from product_c y wh
 insert into product_c(product_code) values(add_int(p1, p1.product_code, self(p1).product_code));
 select * from product_c where product_code > 7;
 select x into p2 from product_c x where product_code = 3;
-evaluate {p1, p2} to p3;
+--evaluate {p1, p2} to p3;
+select c into p3 from (select cast({p1,p2} as set) c);
 select p, p.product_code from table(p3) as t(p);
 rollback;
 
