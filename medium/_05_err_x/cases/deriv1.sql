16d0dc416e4b38a35f02bf650a286d741b09e183
zionyun <seeon0001@naver.com>
2022-02-15 13:07:24 +0900

    revise medium tc and answers in 05errx

--- medium/_05_err_x/cases/deriv1.sql
+++ medium/_05_err_x/cases/deriv1.sql
@@ -1,4 +1,4 @@
 autocommit off;
-select ssn1 from (select ssn from faculty1) f (ssn1)
-select ssn from faculty1 f, (select ssn from faculty g where f = g) h (ssn)
+select ssn1 from (select ssn from faculty1) f (ssn1);
+select ssn from faculty1 f, (select ssn from faculty g where f = g) h (ssn);
 rollback;
--- medium/_05_err_x/cases/savep.sql
+++ medium/_05_err_x/cases/savep.sql
@@ -1,5 +1,5 @@
 autocommit off;
-savepoint "foo"
-rollback work to savepoint "bar"
-select * from faculty
+savepoint "foo";
+rollback work to savepoint "bar";
+select * from faculty;
 rollback;
