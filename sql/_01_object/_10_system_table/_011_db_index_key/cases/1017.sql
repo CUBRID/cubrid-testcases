63d6ba0eca5f89f7a3e5b565435e201b69aed213
tw-kang <54791480+tw-kang@users.noreply.github.com>
2022-02-21 15:43:07 +0900

    [CUBRIDQA-1136] Add order by phrase to fix the result order (#1063)

--- sql/_01_object/_10_system_table/_011_db_index_key/cases/1017.sql
+++ sql/_01_object/_10_system_table/_011_db_index_key/cases/1017.sql
@@ -6,7 +6,8 @@ create reverse unique index ddl_0001_index on ddl_0001(col1);
 
 select index_name, class_name, key_attr_name, key_order
 from   db_index_key
-where  class_name = 'ddl_0001';
+where  class_name = 'ddl_0001'
+order by 1;
 
 drop class ddl_0001;
 
