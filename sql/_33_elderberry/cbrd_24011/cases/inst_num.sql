737b462fe0a9b500ae3f1f8c6ff21c3550faaa15
zionyun <seeon0001@naver.com>
2022-03-07 14:10:10 +0900

    modify case

--- sql/_33_elderberry/cbrd_24011/cases/inst_num.sql
+++ sql/_33_elderberry/cbrd_24011/cases/inst_num.sql
@@ -17,7 +17,7 @@ from tab_a a
       ,v_a b
 where a.col_a = b.col_a
   and b.col_a = 2;
-select /*+ recompile */ count(b.col_b)
+select /*+ recompile */ count(*)
 from tab_a a
       ,(select col_a, col_b from tab_b limit 3) b
 where a.col_a = b.col_a
@@ -28,19 +28,19 @@ from tab_a a
       ,v_a b
 where a.col_a = b.col_a
   and b.col_a = 2;
-select /*+ recompile */ count(b.col_b)
+select /*+ recompile */ count(*)
 from tab_a a
       ,(select col_a,(select 1 from tab_b limit 1) col_b from tab_b group by col_a) b
 where a.col_a = b.col_a
   and b.col_a = 1;
 create or replace view v_a as select col_a,(select 1 from tab_b limit 1) col_b from tab_b group by col_a;
-select /*+ recompile */ count(b.col_b)
+select /*+ recompile */ count(*)
 from tab_a a
       ,v_a b
 where a.col_a = b.col_a
   and b.col_a = 1;
 
-select /*+ recompile */ count(b.col_b)
+select /*+ recompile */ count(*)
 from tab_a a
       ,(select col_a from tab_b group by col_a union all select col_a from tab_b limit 10) b
 where a.col_a = b.col_a
