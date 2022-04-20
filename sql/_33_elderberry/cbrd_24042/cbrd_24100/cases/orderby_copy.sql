7bfd81638c0661b47780161760dac17ce0db8371
soyeon <38178158+sy052@users.noreply.github.com>
2022-04-20 20:20:15 +0900

    [CBRD-24100] Add new test case for view's order by is copied when view merging (#1166)

---
+++ sql/_33_elderberry/cbrd_24042/cbrd_24100/cases/orderby_copy.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS t_a, t_b;
+CREATE TABLE t_a
+  (
+     col_a VARCHAR(100),
+     col_b VARCHAR(100)
+  );
+CREATE TABLE t_b
+  (
+     col_a VARCHAR(100),
+     col_b VARCHAR(100)
+  );
+INSERT INTO t_a
+SELECT to_char(rownum mod 100) col_a,
+       to_char(rownum)         col_b
+FROM   TABLE({0,1,2,3,4,5,6,7,8,9}) a,
+       TABLE({0,1,2,3,4,5,6,7,8,9}) b,
+       TABLE({0,1,2,3,4,5,6,7,8,9}) c,
+       TABLE({0,1,2,3,4,5,6,7,8,9}) d,
+       TABLE({0,1,2,3,4,5,6,7,8,9}) e,
+       TABLE({0,1,2,3,4,5,6,7,8,9}) f limit 10;
+INSERT INTO t_b
+SELECT to_char(rownum mod 100) col_a,
+       to_char(rownum)         col_b
+FROM   TABLE({0,1,2,3,4,5,6,7,8,9}) a,
+       TABLE({0,1,2,3,4,5,6,7,8,9}) b,
+       TABLE({0,1,2,3,4,5,6,7,8,9}) c,
+       TABLE({0,1,2,3,4,5,6,7,8,9}) d,
+       TABLE({0,1,2,3,4,5,6,7,8,9}) e,
+       TABLE({0,1,2,3,4,5,6,7,8,9}) f limit 10;
+CREATE INDEX idx
+  ON t_a(col_a, col_b); 
+CREATE INDEX idx
+  ON t_b(col_a, col_b);
+CREATE OR replace VIEW v_a
+AS
+  SELECT col_a,
+         col_b
+  FROM   t_b
+  ORDER  BY col_b,
+            col_a;
+SELECT /*+ recompile */ Count(*)
+FROM   t_a a,
+       v_a b
+WHERE  a.col_a = b.col_a
+       AND b.col_b = 1; 
+ 
+DROP TABLE t_a, t_b;
