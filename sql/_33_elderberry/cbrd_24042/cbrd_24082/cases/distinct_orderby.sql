20b6988f65ce1f459b81047f5933dbd5e7fe45eb
soyeon <38178158+sy052@users.noreply.github.com>
2022-04-26 16:34:27 +0900

    [CBRD-24082] Add new test cases for view merging extension (#1172)

---
+++ sql/_33_elderberry/cbrd_24042/cbrd_24082/cases/distinct_orderby.sql
@@ -0,0 +1,51 @@
+DROP TABLE IF EXISTS t;
+
+CREATE TABLE t
+  (
+     col_a VARCHAR(10),
+     col_b VARCHAR(10)
+  );
+
+INSERT INTO t
+VALUES ('1',
+        '5');
+
+INSERT INTO t
+VALUES ('2',
+        '4');
+
+INSERT INTO t
+VALUES ('3',
+        '3');
+
+INSERT INTO t
+VALUES ('4',
+        '2');
+
+INSERT INTO t
+VALUES ('5',
+        '1');
+
+--distinct + order by
+CREATE OR REPLACE view v
+AS
+  SELECT col_a,
+         col_b
+  FROM t
+  ORDER BY col_b,
+           col_a;
+
+SELECT /*+ recompile */ col_a
+FROM v;
+SELECT /*+ recompile */ col_a
+FROM v
+ORDER  BY col_a;  
+SELECT /*+ recompile */ col_a,
+                        col_b
+FROM v;
+SELECT /*+ recompile */ DISTINCT col_a
+FROM   v; 
+SELECT /*+ recompile */ DISTINCT col_b,
+                                 col_a
+FROM   v; 
+DROP TABLE t; 
