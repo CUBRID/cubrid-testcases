4265016c97b3f2028f3b171a74c2674191c1385e
soyeon <soyeon@cubrid.com>
2022-06-13 21:48:39 +0900

    [CBRD-24379] Modified test cases for order by clause

--- sql/_01_object/_09_partition/_001_create/cases/1182.sql
+++ sql/_01_object/_09_partition/_001_create/cases/1182.sql
@@ -9,7 +9,7 @@ partition p1 values in (null, '10:00:00','10:10:00','10:20:00','10:30:00'),
 partition p2 values in  ('11:00:00','11:10:00','11:20:00','11:30:00'));
 
 
-select * from db_partition where class_name like '%za%' order by 1,2;
+select * from db_partition where class_name like '%za%' order by partition_name;
 
 
 drop class za;
