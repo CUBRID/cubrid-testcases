1855a03115b109352b4a76e5517c2e4769cc0246
soyeon <soyeon@cubrid.com>
2022-07-08 16:28:17 +0900

    modified create index clause

--- sql/_13_issues/_22_2h/cases/cbrd_24392.sql
+++ sql/_13_issues/_22_2h/cases/cbrd_24392.sql
@@ -1,7 +1,7 @@
 drop table if exists foo;
 create table foo (a int not null, b int not null, c char(10));
-create index idx1 on foo(c) where c > 'A';
-drop index idx1 on foo;
+create index idx2 on foo(c) where c > 'A';
 create index idx1 on foo(c);
 drop index idx1 on foo;
+drop index idx2 on foo;
 drop table foo;
