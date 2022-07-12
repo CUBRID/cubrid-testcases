a477c82e456635aee312e01bce1b7701ca4be842
soyeon <soyeon@cubrid.com>
2022-07-12 11:07:34 +0900

    [CUBRIDQA-1136] Revised test case path to issue directory

--- sql/_13_issues/_22_2h/cases/cbrd_24285.sql
+++ sql/_13_issues/_22_2h/cases/cbrd_24285.sql
@@ -30,4 +30,5 @@ CREATE INDEX i_u_j ON u(j);
 INSERT INTO t values (1,1,1),(2,2,2);
 INSERT INTO u values (1,1,1),(2,2,2);
 SELECT /*+ RECOMPILE */ * FROM u, t WHERE u.i = t.i AND u.j > 0 ORDER BY u.j LIMIT 2;
+DROP INDEX i_u_j ON u(j);
 DROP TABLE u,t;
