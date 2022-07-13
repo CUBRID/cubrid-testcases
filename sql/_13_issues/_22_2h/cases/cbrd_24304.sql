8320da656c1a3e8521188ecb660e0174c2724fa1
soyeon <soyeon@cubrid.com>
2022-07-13 15:52:03 +0900

    modified the update clause

--- sql/_13_issues/_22_2h/cases/cbrd_24304.sql
+++ sql/_13_issues/_22_2h/cases/cbrd_24304.sql
@@ -6,7 +6,7 @@ insert into t select rownum, rownum from _db_class a, _db_class b limit 1500;
 insert into u select rownum, rownum % 1500 + 1, rownum from _db_class a, _db_class b, _db_class c limit 2000;
 create table v (i int, j int, k int);
 insert into v select rownum, rownum % 1500 + 1, rownum from _db_class a, _db_class b, _db_class c limit 2000;
-update statistics on all classes;
+update statistics on u,t,v;
 
 select /*+ recompile */ u.k from u, t, v where u.j = t.i and u.j = v.j order by u.k limit 10;
 drop table u,t,v;
