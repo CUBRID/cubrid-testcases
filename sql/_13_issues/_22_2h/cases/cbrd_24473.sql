d75d58e39be23a3e2f164ebc8aa965acbf96aca9
zionyun <41980125+zionyun@users.noreply.github.com>
2022-10-14 09:28:30 +0900

    [CBRD-24473] Add new testcase for index scan when using stored function in where condition (#1302)
    refer to http://jira.cubrid.org/browse/CBRD-24473

---
+++ sql/_13_issues/_22_2h/cases/cbrd_24473.sql
@@ -0,0 +1,37 @@
+--test cases (Except for correlated subquery cases, all other query plans are correct)
+-- create table
+drop table if exists tbl;
+create table tbl(cola int, colb int);
+insert into tbl values(1,1),(2,2);
+create index idx on tbl(cola);
+
+-- create function
+create or replace FUNCTION FN_JOIN_TEST(p_code varchar) RETURN VARCHAR
+ as language java name 'FN_JOIN_TEST.sp_FN_JOIN_TEST1(java.lang.String)  return java.lang.String';
+
+set optimization level 514;
+
+--general case
+SELECT 1
+FROM tbl
+WHERE cola = FN_JOIN_TEST(1984);
+
+--sscan 
+SELECT 1
+FROM tbl
+WHERE cola = FN_JOIN_TEST(colb);
+
+--correlated subquery case (fixme : must be index scan)
+select (SELECT /*+ oredered */ 1 FROM tbl a WHERE a.cola = FN_JOIN_TEST(b.cola)) cola
+ from tbl b;
+
+--order by skip
+SELECT /*+ recompile ordered */ FN_JOIN_TEST(b.colb)
+FROM tbl a, tbl b
+where a.cola > 0
+   and a.colb = b.colb
+order by a.cola
+limit 1 ;
+
+drop table if exists tbl;
+drop function FN_JOIN_TEST;
