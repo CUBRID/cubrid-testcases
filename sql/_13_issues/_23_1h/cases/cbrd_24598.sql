b82f6bb9f337cabd0bc18d9d500a3b469e4cea87
tw-kang <54791480+tw-kang@users.noreply.github.com>
2023-01-05 17:47:58 +0900

    [CBRD-24598] Add new test case for memory allocation. (#1358)
    http://jira.cubrid.org/browse/CBRD-24598
    
    This issue only verified the core dump problem.
    But now the order of executing the 'execute q... ' query occurs an error caused by the executing order.
    ex )
    execute q using 'A'; /* success /
    execute q using 1; / success */
    
    execute q using 'A'; /* fail - ERROR: Cannot coerce a value of domain "character" to domain "double". */
    
    This will be discussed and fixed in another issue.
    Until fix the Actual result will be /* ERROR -181 : Cannot coerce value of domain "character" to domain "double". */.
    It will be the answer sheet and after the issue is solved it must be revised as success result.

---
+++ sql/_13_issues/_23_1h/cases/cbrd_24598.sql
@@ -0,0 +1,36 @@
+-- This test case verifies CBRD-24598 issue.
+-- Core occures because of not freed memory.
+-- Core occures in second execution query on all after 10.x version.
+-- Core does not occures in 9.x version.
+-- Allocated memory on the global heap must be freed on the global heap.
+-- After error fix both query should be executed normaly.
+
+-- prepare required 
+prepare q from 'select decode (?, '''', c, NULL, c, -1) from table ({1}) as t (c)'; 
+
+-- success
+execute q using 'A'; 
+
+-- success
+execute q using 1; 
+
+-- error : temporary
+-- known bug
+execute q using 'A'; 
+
+-- prepare required 
+prepare p from 'select decode (?, '''', c, NULL, c, ''Z'') from table ({''X''}) as t (c)';
+
+-- success
+execute p using 1;
+
+-- error : temporary
+-- known bug
+execute p using 'A';
+
+-- success
+execute p using 1;
+
+
+drop prepare q;
+drop prepare p;
