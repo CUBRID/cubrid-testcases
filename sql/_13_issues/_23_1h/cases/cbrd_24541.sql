1254f2d0f1e2b752bc7e3078aabaf2be1866fcf5
zionyun <seeon0001@naver.com>
2023-01-17 15:23:47 +0900

    Changed numbers to letters to check query plan

--- sql/_13_issues/_23_1h/cases/cbrd_24541.sql
+++ sql/_13_issues/_23_1h/cases/cbrd_24541.sql
@@ -1,21 +1,21 @@
 -- This test case verifies CBRD-24541 issue.
 -- Core dump occurs when 'EXISTS' operation or 'count stop' is used for index scan of partitioned table.
 
-drop table if exists t2, t1;
-create table t1(a int, b char(10) default 'abc');
-insert into t1(a, b) values(1, 'a');
-insert into t1(a, b) values(2, 'b');
-create table t2(a int, b char(10) default 'abc', primary key(a)) partition by hash(a) partitions 5;
-insert into t2(a,b) values (1, 'e');
-select t1.a, t1.b from t1 t1 where exists (select b from t2 where t2.a = t1.a); 
+drop table if exists ta, tb;
+create table ta(a int, b char(10) default 'abc');
+insert into ta(a, b) values(1, 'a');
+insert into ta(a, b) values(2, 'b');
+create table tb(a int, b char(10) default 'abc', primary key(a)) partition by hash(a) partitions 5;
+insert into tb(a,b) values (1, 'e');
+select ta.a, ta.b from ta ta where exists (select b from tb where tb.a = ta.a); 
 
-drop table if exists t2, t1;
-create table t1(a int, b char(10) default 'abc');
-insert into t1(a, b) values(1, 'a');
-insert into t1(a, b) values(2, 'b');
-create table t2(a int, b char(10) default 'abc') partition by hash(a) partitions 5;
-create index idx1 on t2(a);
-insert into t2(a,b) values (1, 'e');
-select t1.a, t1.b from t1 t1 where exists (select b from t2 where t2.a = t1.a limit 1); 
+drop table if exists tb, ta;
+create table ta(a int, b char(10) default 'abc');
+insert into ta(a, b) values(1, 'a');
+insert into ta(a, b) values(2, 'b');
+create table tb(a int, b char(10) default 'abc') partition by hash(a) partitions 5;
+create index idx1 on tb(a);
+insert into tb(a,b) values (1, 'e');
+select ta.a, ta.b from ta ta where exists (select b from tb where tb.a = ta.a limit 1); 
 
-drop table if exists t2, t1;
+drop table if exists ta, tb;
