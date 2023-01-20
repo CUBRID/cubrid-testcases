96167b27e91dbd456051f33506d6073452c6c9fb
tw-kang <54791480+tw-kang@users.noreply.github.com>
2023-01-20 15:10:50 +0900

    [CBRD-24563] Add new test case for regular expression library change. (#1368)
    http://jira.cubrid.org/browse/CBRD-24563
    
    Verified the answer modification with regex101.com
    Used Golang as FLAVOR to verify RE2 library.

---
+++ sql/_34_fig/cbrd_24563/cases/REGEXP_INSTR_unicode.sql
@@ -0,0 +1,23 @@
+-- CBRD-24563 : default regexp library change from cppstd to RE2
+set system parameters 'regexp_engine=re2';
+
+--REGEXP_INSTR
+SET NAMES utf8 COLLATE utf8_ko_cs;
+SELECT REGEXP_INSTR('삼성로 86길, 강남구, 서울특별시', ',[^,]+,', 1, 1);
+SELECT REGEXP_INSTR('삼성로 86길, 강남구, 서울특별시', '[[:alpha:]]+', 1, 3);
+SELECT REGEXP_INSTR('11억 8112만 5400원', '\d+[[:alpha:]]', 3, 1);
+SELECT REGEXP_INSTR('Kłak Aleksander', '[[:alpha:]]+', 1, 1);
+SELECT REGEXP_INSTR (_euckr'가나다라' COLLATE euckr_bin, _utf8' [가-나]{4}' COLLATE utf8_ko_cs);
+SELECT REGEXP_INSTR ('가나다라' COLLATE utf8_ko_cs,  _utf8' [த]' COLLATE utf8_tr_cs);
+
+--re2 library unicode verification
+SELECT REGEXP_INSTR('삼성로 86길, 강남구, 서울특별시', '\p{Hangul}+', 1, 3);
+SELECT REGEXP_INSTR('11억 8112만 5400원', '\d+\p{Hangul}', 3, 1);
+SELECT REGEXP_INSTR('Kłak Aleksander', '\p{Hangul}+', 1, 1);
+
+SET NAMES iso88591;
+SELECT REGEXP_INSTR('Kłak Aleksander', '[[:alpha:]]+', 1, 1);
+
+
+set system parameters 'regexp_engine=default';
+
---
+++ sql/_34_fig/cbrd_24563/cases/_01_basic_test_05.sql
@@ -0,0 +1,22 @@
+-- CBRD-24563 : default regexp library change from cppstd to RE2
+set system parameters 'regexp_engine=re2';
+
+-- POSIX-style word boundary does not support
+select ('a word file' regexp '[[:<:]]word[[:>:]]');
+
+-- Tests for ECMAScrpt word boundary syntax 
+select ('a word file' regexp '\bWord\b');
+select ('__pa word _file' regexp '\bWord\b');
+select ('__pa  word  _file' regexp '\bWord\b');
+select ('__pa  word _file' regexp '\bWord\b');
+select ('__pa wrd  _file' regexp '\bWo?rd\b');
+select ('__pa 7_1 _file' regexp '\b[^_a-z09]_[0-1]\b');
+
+select ('a word file' regexp binary '\bWord\b');
+select ('__pa wordddd _file' regexp '\bWord{2,3}\b');
+select ('__pa a _file' regexp '\b[^a-z]\b');
+select ('__pa _1 _file' regexp '\b[^_a-z09][0-1]\b');
+select ('__pa 71 _file' regexp '\b[^_a-z09]_[0-1]\b');
+
+set system parameters 'regexp_engine=default';
+
