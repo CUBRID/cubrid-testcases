262829799d8e8601bf17dc6dc44635f7eb6b4db9
tw-kang <54791480+tw-kang@users.noreply.github.com>
2023-02-03 14:43:29 +0900

    [CBRD-24252]revised answer for no_eliminate_join hint (#1386)
    http://jira.cubrid.org/browse/CBRD-24252
    
    The function from no_merge which make not reduce join is separated to no_eliminate_join hint.

--- sql/_33_elderberry/cbrd_24042/cbrd_24182/cases/no_merge.sql
+++ sql/_33_elderberry/cbrd_24042/cbrd_24182/cases/no_merge.sql
@@ -13,8 +13,25 @@ CREATE UNIQUE INDEX idx
   ON t(c_a, c_b);
 CREATE INDEX idx_a
   ON t(c_a, c_c);
+
+-- CBRD-24252 : function of stop reducing the number of join only works with no_eliminate_join.
+-- Therefor to compare the result of both hints, few more cases are added.
 -- no_merge
 SELECT /*+ recompile no_merge */ count(*)
+FROM   t a
+       LEFT OUTER JOIN t b
+                    ON a.c_a = b.c_a
+                       AND a.c_b = b.c_b
+WHERE  a.c_c = 1; 
+-- no_merge no_eliminate_join
+SELECT /*+ recompile no_merge no_eliminate_join */ count(*)
+FROM   t a
+       LEFT OUTER JOIN t b
+                    ON a.c_a = b.c_a
+                       AND a.c_b = b.c_b
+WHERE  a.c_c = 1; 
+-- no_eliminate_join
+SELECT /*+ recompile no_eliminate_join */ count(*)
 FROM   t a
        LEFT OUTER JOIN t b
                     ON a.c_a = b.c_a
