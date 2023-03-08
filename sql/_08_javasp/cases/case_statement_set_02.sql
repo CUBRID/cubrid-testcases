d90ba005e38404e1a2deea94cc9ec8ed74d90e7c
zionyun <seeon0001@naver.com>
2023-03-08 11:19:16 +0900

    add UNION ALL statement for time type

--- sql/_08_javasp/cases/case_statement_set_02.sql
+++ sql/_08_javasp/cases/case_statement_set_02.sql
@@ -15,14 +15,29 @@ UNION ALL SELECT test_fc2(col2) FROM test_tbl;
 --The result of a UNION ALL of time and timestamp types. fail.
 --ERROR: 'test_timestamp(timestamp '5/5/2005 1:1:1 am')' is not union compatible with 'test_time(timestamp '5/5/2005 1:1:1 am')'
 
-CREATE FUNCTION test_time(d time) RETURN time AS LANGUAGE JAVA NAME 'SpTest.testTime(java.sql.Time) return java.sql.Time';
-CREATE FUNCTION test_timestamp(d timestamp) RETURN timestamp AS LANGUAGE JAVA NAME 'SpTest.testTimestamp(java.sql.Timestamp) return java.sql.Timestamp';
+CREATE OR REPLACE FUNCTION test_date(d date) RETURN date AS LANGUAGE JAVA NAME 'SpTest.testDate(java.sql.Date) return java.sql.Date';
+CREATE OR REPLACE FUNCTION test_time(d time) RETURN time AS LANGUAGE JAVA NAME 'SpTest.testTime(java.sql.Time) return java.sql.Time';
+CREATE OR REPLACE FUNCTION test_timestamp(d timestamp) RETURN timestamp AS LANGUAGE JAVA NAME 'SpTest.testTimestamp(java.sql.Timestamp) return java.sql.Timestamp';
 
-SELECT test_time(timestamp'5/5/2005 1:1:1 am') 
-UNION ALL SELECT test_timestamp(timestamp'5/5/2005 1:1:1 am');
+--fail (time - timestamp) (Cannot coerce time to type timestamp)
+SELECT test_time(time'12:00:00 AM')
+UNION ALL SELECT test_timestamp(timestamp'5/5/2005 12:00:00 am');
+
+--success (timestamp - time)
+SELECT test_time(timestamp'5/5/2005 12:00:00 AM') 
+UNION ALL SELECT test_time(time'13:01:01 PM');
+
+--success (date - timestamp)
+SELECT test_date('2023-02-28')
+UNION ALL SELECT test_timestamp(timestamp'2022-06-30 19:30:13 pm');
+
+--success (timestamp - date)
+SELECT test_timestamp(timestamp'2022-05-05 12:00:00 am') 
+UNION ALL SELECT test_date('2022-05-05');
 
 DROP FUNCTION test_fc;
 DROP FUNCTION test_fc2;
 DROP FUNCTION test_time;
+DROP FUNCTION test_date;
 DROP FUNCTION test_timestamp;
 DROP TABLE IF EXISTS test_tbl;
