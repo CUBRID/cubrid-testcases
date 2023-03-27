59f55ab604d327fc9c4074980f7c2840be2984b3
tw-kang <twkang@cubrid.com>
2023-03-27 13:42:21 +0900

    add new test case

---
+++ sql/_13_issues/_23_1h/cases/cbrd_24645.sql
@@ -0,0 +1,22 @@
+-- This test case verifies CBRD-24645 issue.
+-- Rownum of main query is displayed incorrectly.
+-- Caused by order-by of subquery is copied to main query during view merging.
+
+drop table if exists t1;
+create table t1 (col1 int);
+insert into t1 values(3);
+insert into t1 values(2);
+insert into t1 values(1);
+
+select /*+ recompile */ rownum,col1 from (select col1 from t1 where col1 > 0 order by col1) a;
+
+--order by skip
+create index idx on t1(col1);
+select /*+ recompile */ rownum,col1 from (select col1 from t1 where col1 > 0 order by col1) a;
+
+--expr having instnum
+drop index idx on t1;
+select /*+ recompile */ rownum + 1,col1 from (select col1 from t1 where col1 > 0 order by col1) a;
+
+
+drop table t1; 
