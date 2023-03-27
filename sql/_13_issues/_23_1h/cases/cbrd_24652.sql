8aac9c364385e3354ed7248193a4ef203cd8f6d1
tw-kang <twkang@cubrid.com>
2023-03-27 15:16:21 +0900

    modified testcase refer to review

--- sql/_13_issues/_23_1h/cases/cbrd_24652.sql
+++ sql/_13_issues/_23_1h/cases/cbrd_24652.sql
@@ -2,18 +2,18 @@
 -- The problem performing hash list scan when VOBJECT is included in predicates.
 -- Hash list scan should not be used in the result.
 
-drop table if exists t1;
-drop view if exists v_t1;
+drop table if exists tbl;
+drop view if exists v_tbl;
 
-create table t1(col1 int, col2 int) dont_reuse_oid;
-insert into t1 values(1,1),(2,2);
-create or replace view  v_t1(p,q) as select col1,col2 from t1 union all select col1,col2 from t1;
+create table tbl(x int, y int) dont_reuse_oid;
+insert into tbl values(1,1),(2,2);
+create or replace view  v_tbl(p,q) as select x,y from tbl union all select x,y from tbl;
 
 -- include VOBJECT
 set trace on;
-select /*+ recompile */ t.a, t.a.p from table(set(select d from v_t1 d)) t(a);
+select /*+ recompile */ t.a, t.a.p from table(set(select d from v_tbl d)) t(a);
 show trace;
 set trace off;
 
-drop table t1;
-drop view v_t1;
+drop table tbl;
+drop view v_tbl;
