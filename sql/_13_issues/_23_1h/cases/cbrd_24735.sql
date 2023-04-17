ac9852ba59a7c007966f551196fc1d1f4ea2970a
tw-kang <54791480+tw-kang@users.noreply.github.com>
2023-04-17 18:56:49 +0900

    [CBRD-24735] add new test case (#1448)
    http://jira.cubrid.org/browse/CBRD-24735

---
+++ sql/_13_issues/_23_1h/cases/cbrd_24735.sql
@@ -0,0 +1,29 @@
+drop table if exists t_a, t_b;
+
+create table t_a (x int);
+insert into t_a values (1);
+
+create table t_b (x int, y int);
+insert into t_b
+select a.*, b.*
+from
+  table ({1}) as a,
+  table ({1, 2}) as b;
+create index idx_desc on t_b (x, y);
+
+update statistics on all classes with fullscan;
+
+
+select /*+ recompile ordered */ t.y
+from t_a inner join t_b as t on t_a.x = t.x and t_a.x = 1
+where t.x = 1
+order by t.y
+limit 1;
+
+select /*+ recompile ordered no_multi_range_opt */ t.y
+from t_a inner join t_b as t on t_a.x = t.x and t_a.x = 1
+where t.x = 1
+order by t.y
+limit 1;
+
+drop table t_a, t_b;
