b4fb641e6c8adb0b558f260557c58e355f246be8
sohn <ssihil@cubrid.com>
2023-04-19 13:44:43 +0900

    add comment

--- sql/_13_issues/_23_1h/cases/cbrd_24772.sql
+++ sql/_13_issues/_23_1h/cases/cbrd_24772.sql
@@ -1,3 +1,6 @@
+-- This test case verifies CBRD-24705 issue.
+-- A segfault occurs when using the connect by REGEXP_SUBSTR statement
+
 select 1
 from dual
 connect by REGEXP_SUBSTR(1,'1', 1,level)=1;
