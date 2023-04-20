bf4115fc199883dcf7663342b5789978dd353039
seungil-sohn <ssihil@cubrid.com>
2023-04-20 12:51:49 +0900

    [CBRD-24772] Backport from develop to 10.2 (#1461)
    refer to http://jira.cubrid.org/browse/CBRD-24772

---
+++ sql/_13_issues/_23_1h/cases/cbrd_24772.sql
@@ -0,0 +1,10 @@
+-- This test case verifies CBRD-24772 issue.
+-- A segfault occurs when using the connect by REGEXP_SUBSTR statement
+
+select 1
+from dual
+connect by REGEXP_SUBSTR(1,'1', 1,level)=1;
+
+select 1
+from dual
+connect by REGEXP_SUBSTR(level,'1', 1,1)=1;
