8a5ba3aaa452d3aa8ea68acc9d1e042abfa63fb0
tw-kang <twkang@cubrid.com>
2023-06-02 16:22:32 +0900

    reflect review from kwonhoil, ssihil

--- sql/_34_fig/cbrd_24796/cases/cbrd_24796.sql
+++ sql/_34_fig/cbrd_24796/cases/cbrd_24796.sql
@@ -5,19 +5,43 @@
 
 drop table if exists tbl;
 
-
+-- success case
 create table tbl(c1 int, c2 int, c3 int, index idx1 (c1) invisible comment 'invisible index');
-create index idx2 on tbl(c2) with online  comment 'visible index';
-create index idx3 on tbl(c3)  invisible  comment 'visible index';
+create index idx2 on tbl(c2) with online  comment 'with online index';
+create index idx3 on tbl(c3) invisible comment 'invisible index';
+create index idx4 on tbl(c2, c3) with online parallel 4 comment 'with online parallel 4 index';
 show index from tbl;
 
+drop index idx1 on tbl;
 drop index idx2 on tbl;
 drop index idx3 on tbl;
+drop index idx4 on tbl;
+
+alter table tbl add index idx1(c1) invisible comment 'alter add invisible index';
+
+-- error case
+alter table tbl add index idx2(c2) with online comment 'alter add with online index';
+alter table tbl add index idx3(c2,c3) with online parallel 4 comment 'alter add with online parallel 4 index';
+
+show index from tbl;
+drop tbl;
+
+-- error case
+create table tbl(c1 int, c2 int, c3 int, index idx1 (c1) comment 'invisible index' invisible);
+
+-- success case
+create table tbl(c1 int, c2 int, c3 int);
 
 -- error case
 create index idx2 on tbl(c2) comment 'visible index' with online;
-create index idx3 on tbl(c3) comment 'visible index' invisible;
-show index from tbl;
+create index idx3 on tbl(c3) comment 'invisible index' invisible;
+create index idx4 on tbl(c2, c3) comment 'with online and invisible index' with online PARALLEL 4 invisible;
 
+alter table tbl add index idx1(c1) comment 'alter add invisible index' invisible;
+alter table tbl add index idx2(c2) comment 'alter add with online index' with online;
+alter table tbl add index idx3(c2,c3) comment 'alter add with online parallel 4 index' with online parallel 4;
+
+show index from tbl;
 drop tbl;
+
 
