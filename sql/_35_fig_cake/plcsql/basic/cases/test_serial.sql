6c6d6cd69dd0ff61c708a01022b508b5deb7207a
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2023-06-19 14:31:11 +0900

    add serial and user defined function tests

---
+++ sql/_35_fig_cake/plcsql/basic/cases/test_serial.sql
@@ -0,0 +1,18 @@
+DROP SERIAL IF EXISTS demo_pl_serial;
+CREATE SERIAL demo_pl_serial;
+
+create or replace function demo_global_semantics_serial() return numeric as
+i numeric;
+begin
+    i := demo_pl_serial.NEXT_VALUE;
+    return i;
+end;
+
+CAll demo_global_semantics_serial ();
+CAll demo_global_semantics_serial ();
+CAll demo_global_semantics_serial ();
+CAll demo_global_semantics_serial ();
+
+DROP SERIAL IF EXISTS demo_pl_serial;
+
+CAll demo_global_semantics_serial ();
