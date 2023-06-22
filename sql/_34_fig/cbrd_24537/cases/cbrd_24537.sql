dfed7df857463e0dd122fe773e1008a3be15b362
zionyun <41980125+zionyun@users.noreply.github.com>
2023-06-22 14:27:26 +0900

    [CBRD-24537] Add new test case for plan of NOT MATCHED part of MERGE query (#1493)
    refer to http://jira.cubrid.org/browse/CBRD-24537

---
+++ sql/_34_fig/cbrd_24537/cases/cbrd_24537.sql
@@ -0,0 +1,18 @@
+-- This is a test case to verify the CBRD-24537 issue.
+-- When performing a NOT MATCHED check, use the NOT EXISTS query instead of the COUNT query.
+
+drop table if exists src;
+drop table if exists tt;
+
+create table src (s int);
+create table tt (t int, str varchar);
+insert into src values (1), (2), (3);
+
+--@queryplan
+merge /*+ recompile */ into tt using src d on 'a' = 'a'  when not matched then insert values (999, 'abcd') where d.s = 3;
+select * from src;
+select * from tt;
+
+drop table if exists src;
+drop table if exists tt;
+
