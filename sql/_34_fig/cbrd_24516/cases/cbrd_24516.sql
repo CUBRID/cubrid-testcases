ffaa03e3080806db9cd6da3fc126127d434edd50
tw-kang <twkang@cubrid.com>
2023-07-24 19:33:29 +0900

    reflect review from bwkim

--- sql/_34_fig/cbrd_24516/cases/cbrd_24516.sql
+++ sql/_34_fig/cbrd_24516/cases/cbrd_24516.sql
@@ -5,9 +5,10 @@
 
 DROP TABLE IF EXISTS tbl; 
 
-CREATE TABLE tbl (id INTEGER not null primary key , phone VARCHAR(20));
-INSERT INTO tbl VALUES (1, '123-456-789');
-INSERT INTO tbl VALUES (999, '999-999-999');
+CREATE TABLE tbl (id INTEGER not null primary key , phone VARCHAR(20), a INTEGER NOT NULL, b INTEGER, c INTEGER,  INDEX idx_mx (a, b));
+INSERT INTO tbl VALUES (1, '123-456-789', 1, 1, 1);
+INSERT INTO tbl VALUES (999, '999-999-999', 999, 999, 999);
+
 
 SET TRACE ON;
 
@@ -24,10 +25,19 @@ SHOW TRACE;
 SELECT MIN(id), MAX(id) FROM tbl;
 SHOW TRACE;
 
+SELECT MIN(id), MAX(a) FROM tbl;
+SHOW TRACE;
+
 SELECT COUNT(id), MAX(id) FROM tbl;
 SHOW TRACE;
 
+SELECT COUNT(a), MAX(b) FROM tbl;
+SHOW TRACE;
+
 SELECT COUNT(id), MIN(id), MAX(id) FROM tbl;
+SHOW TRACE;
+
+SELECT COUNT(a), MIN(b), MAX(c) FROM tbl;
 SHOW TRACE;
 
 SELECT COUNT( * ), MAX(id) FROM tbl;
@@ -38,6 +48,10 @@ SHOW TRACE;
 
 SELECT COUNT( * ), MIN(id), MAX(id) FROM tbl;
 SHOW TRACE;
+
+SELECT COUNT( * ), MIN(a), MAX(c) FROM tbl;
+SHOW TRACE;
+
 
 -- case 2: for not-optimized-scan
 SELECT COUNT(*) FROM tbl WHERE id > 1;
