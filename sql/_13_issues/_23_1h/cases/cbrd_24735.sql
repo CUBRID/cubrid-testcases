548922a310f66836cd11432a34ced6765e735fb9
tw-kang <54791480+tw-kang@users.noreply.github.com>
2023-08-08 13:32:45 +0900

    [CBRD-24914] Backport Exclude testcase caused by core dump. (#1534)
    #1528, http://jira.cubrid.org//browse/CBRD-24914
    
    Core dumped caused by http://jira.cubrid.org/browse/CBRD-24906.
    Exclude list for sql test does not exist, so after the core is fixed this case will be included again as soon as possible.

--- sql/_13_issues/_23_1h/cases/cbrd_24735.sql
+++ sql/_13_issues/_23_1h/cases/cbrd_24735.sql
@@ -14,11 +14,13 @@ create index idx_desc on t_b (x, y);
 update statistics on all classes with fullscan;
 
 
-select /*+ recompile ordered */ t.y
-from t_a inner join t_b as t on t_a.x = t.x and t_a.x = 1
-where t.x = 1
-order by t.y
-limit 1;
+-- CBRD-24914 : core dumped caused by revert cbrd-24735
+-- After core fixed this case will be included again.
+--select /*+ recompile ordered */ t.y
+--from t_a inner join t_b as t on t_a.x = t.x and t_a.x = 1
+--where t.x = 1
+--order by t.y
+--limit 1;
 
 select /*+ recompile ordered no_multi_range_opt */ t.y
 from t_a inner join t_b as t on t_a.x = t.x and t_a.x = 1
