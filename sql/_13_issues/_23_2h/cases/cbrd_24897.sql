653fb7853c08d8c4c14957b968d506ac1cfd9c2e
tw-kang <twkang@cubrid.com>
2023-08-09 18:44:42 +0900

    reflect review from younginj

--- sql/_13_issues/_23_2h/cases/cbrd_24897.sql
+++ sql/_13_issues/_23_2h/cases/cbrd_24897.sql
@@ -2,23 +2,51 @@
 -- Query rewriter can lose parent paths of a name in qo_collect_name_spec().
 -- Rewritten query should include commented phrase. 
 
--- au.grantee.[name]
-select /*+ recompile */ sum (set {[au].[class_of]}) from [_db_auth] as [au] where {[au].[grantee].[name]} subseteq (select set {current_user});
+drop table if exists target_info, target_list_info, object_info;
 
--- au.grantee.[name]
-select /*+ recompile */ sum (set {[au].[class_of]}) from [_db_auth] as [au] where {[au].[grantee].[name]} subseteq (select set {current_user}) and au.auth_type= 'SELECT';
+create table object_info (id int, status int, name varchar) dont_reuse_oid;
+insert into object_info
+values (1, 0, 'object_1'), (2, 0, 'object_2'), (3, 0, 'object_3'), (4, 0, 'object_4'), (5, 0, 'object_5');
 
--- c.[owner].[name]
-select /*+ recompile */ count (*) from _db_class c where c.owner.name = 'DBA' and class_type = 0 using index none;
+create table target_info (id int, status int, target object_info);
+insert into target_info (id, status)
+values (1, 0), (2, 0), (3, 0), (4, 0), (5, 0);
+update target_info as t set t.target = (select o from object_info as o where o.id = t.id);
 
--- c.class_name
-select /*+ recompile */ count (*) from _db_class c where nvl (c.class_name, '') = '_db_class' and class_type = 0 using index none;
+create table target_list_info (id int, status int, target_list set (object_info));
+insert into target_list_info (id, status)
+values (1, 0);
+update target_list_info as t set t.target_list = set (select o from object_info as o);
 
--- c.[owner].[name]
-select /*+ recompile */ count (*) from _db_class c where {c.owner.name} = {'DBA'} and class_type = 0 using index none;
+-- No path expressions
+/* expected: o.[name] */
+select /*+ recompile */ o.id, o.name from object_info as o where o.name = 'object_1' and o.status = 0;
 
--- c.[owner].[name]
-select /*+ recompile */ count (*) from _db_class c where nvl (c.owner.name, '') = 'DBA' and class_type = 0 using index none;
+/* expected: nvl(o.[name], '') */
+select /*+ recompile */ o.id, o.name from object_info as o where nvl (o.name, '') = 'object_2' and o.status = 0;
 
--- t.a.attr_name
-select /*+ recompile */ count (*) from _db_class c, table (c.inst_attrs) t (a) where nvl (t.a.attr_name, '') = 'class_name' and class_type = 0 using index none;
+/* expected: {o.[name]} */
+select /*+ recompile */ o.id, o.name from object_info as o where {o.name} = {'object_3'} and o.status = 0;
+
+/* expected: {o.[name]} */
+select /*+ recompile */ o.id, o.name from object_info as o where {o.name} subseteq set {'object_4'} and o.status = 0;
+
+-- path expressions
+/* expected: t.target.[name] */
+select /*+ recompile */ t.target.id, t.target.name from target_info as t where t.target.name = 'object_1' and t.status = 0;
+
+/* expected: nvl(t.target.[name], '') - AS-IS: nvl([name], '') */
+select /*+ recompile */ t.target.id, t.target.name from target_info as t where nvl (t.target.name, '') = 'object_2' and t.status = 0;
+
+/* expected: {t.target.[name]} - AS-IS: {[name]} */
+select /*+ recompile */ t.target.id, t.target.name from target_info as t where {t.target.name} = {'object_3'} and t.status = 0;
+
+/* expected: {t.target.[name]} - AS-IS: {[name]} */
+select /*+ recompile */ t.target.id, t.target.name from target_info as t where {t.target.name} subseteq set {'object_4'} and t.status = 0;
+
+/* expected: nvl(l.o.[name], '') - AS-IS: nvl([name], '') */
+select /*+ recompile */ l.o.id, l.o.name from target_list_info as t, table (t.target_list) as l (o) where nvl (l.o.name, '') = 'object_5' and t.status = 0;
+
+
+drop table if exists target_info, target_list_info, object_info;
+
