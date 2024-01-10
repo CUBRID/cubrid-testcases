182190a80b648880262069a90da513eb9e828a5d
hiclass <hiclasss@naver.com>
2024-01-10 17:19:33 +0900

    builtin function regexp

---
+++ sql/_05_plcsql/_01_testspec/_04_expression/_04_function_call/_03_builtin_function/_03_regexp_function/cases/04_04_03_03_bfn_regexp_regexp_instr_err.sql
@@ -0,0 +1,22 @@
+--+ server-message on
+
+-- normal: basic usage of a builtin function call
+
+create or replace procedure t () as
+begin
+    -- Should be null and not printed. not 0.
+    dbms_output.put_line(REGEXP_INSTR(NULL, 'abc'));
+    dbms_output.put_line(REGEXP_INSTR('12345abcdeabcde', NULL));
+    dbms_output.put_line(REGEXP_INSTR('12345abcdeabcde', 'abc', NULL));
+    dbms_output.put_line(REGEXP_INSTR('12345abcdeabcde', 'abc', 1, NULL));
+    dbms_output.put_line(REGEXP_INSTR('12345abcdeabcde', 'abc', 1, 1, NULL));
+end;
+
+select * from db_stored_procedure where sp_name = 't';
+select * from db_stored_procedure_args where sp_name = 't';
+
+call t();
+
+drop procedure t;
+
+--+ server-message off
