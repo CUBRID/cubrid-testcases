d57d69218694ed72e22e29a0e378161712f9ada3
swi0110 <joykevin@cubrid.com>
2024-03-20 17:22:54 +0900

    changed the test name and divide the test case

--- sql/_05_plcsql/_01_testspec/_01_basic_structure/_02_create_function/_13_language/cases/language.sql
+++ sql/_05_plcsql/_01_testspec/_01_basic_structure/_02_create_function/_13_language/cases/language.sql
@@ -1,3 +1,4 @@
+--+ server-message on
 /*
 Verification for CBRD-24811
 language statement extend
@@ -7,15 +8,22 @@ The 'language java' is legacy supported on stored procedure.
 but needs to be checked after extend the language statement by pl/csql.
 */
 
--- java sp
-create procedure pro_java(i string) as language java
-name 'SpTest2.typetestinteger(java.lang.String) return java.lang.Integer';
+-- plcsql
+create function func_plcsql() return int as language plcsql
+begin
+    dbms_output.put_line('test');
+	return 0;
+end;
 
+-- java sp
 create function func_java(i string) return string as language java
 name 'SpTest2.typetestinteger(java.lang.String) return java.lang.Integer';
 
-select pro_java('1') from dual;
-select func_java('2') from dual;
 
-drop procedure pro_java;
+select func_plcsql() from dual;
+select func_java('1') from dual;
+
+drop function func_plcsql;
 drop function func_java;
+
+--+ server-message off
