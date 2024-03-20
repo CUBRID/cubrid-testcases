493e1ccc2d81aa6f363abb37c761a9eb2462fa19
swi0110 <joykevin@cubrid.com>
2024-03-20 17:35:58 +0900

    add for CBRD-24818

---
+++ sql/_05_plcsql/_01_testspec/_01_basic_structure/_02_create_function/_03_wo_parameters/cases/no_param.sql
@@ -0,0 +1,28 @@
+--+ server-message on
+/*
+Verification for CBRD-24811
+if don't need to parameter then '()' is not need
+*/
+create function func_plcsql return int as language plcsql
+begin
+    dbms_output.put_line('no param');
+        return 0;
+end;
+
+select func_plcsql() from dual;
+
+drop function func_plcsql;
+
+-- no param
+create function func_plcsql return int as language plcsql
+begin
+    dbms_output.put_line('no param');
+	return 0;
+end;
+
+select func_plcsql() from dual;
+
+drop function func_plcsql;
+
+
+--+ server-message off
