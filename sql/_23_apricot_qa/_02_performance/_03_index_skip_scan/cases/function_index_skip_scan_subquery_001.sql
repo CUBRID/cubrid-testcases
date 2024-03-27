fde2b758e43deef06dfa0bb1748f0b8c88d4dd42
shparkcubrid <shpark@cubrid.com>
2024-03-27 17:54:32 +0900

    slip

--- sql/_23_apricot_qa/_02_performance/_03_index_skip_scan/cases/function_index_skip_scan_subquery_001.sql
+++ sql/_23_apricot_qa/_02_performance/_03_index_skip_scan/cases/function_index_skip_scan_subquery_001.sql
@@ -30,9 +30,9 @@ select /*+ recompile  */ count(*) from (select /*+ recompile INDEX_SS */ * from 
 --Test  mot use ISS index 
 select /*+ recompile  */ count(*) from (select /*+ recompile INDEX_SS */ * from t where j between 101 and 199 ) tt;
 --Test  not use ISS index 
-select /*+ recompile */ i,j,k,l,(select /*+ recompile INDEX_SS */ count(*) from t where j between 101 and 199 ) from t order by 1;
+select /*+ recompile */ i,j,k,l,(select /*+ recompile INDEX_SS */ count(*) from t where j between 101 and 199 ) from t order by 1,2,3;
 --Test   use ISS index 
-select /*+ recompile index_ss */ i,j,k,l,(select /*+ recompile INDEX_SS */ count(*) from t where j between 101 and 199 ) from t where j = 100 order by 1,2,3;
+select /*+ recompile index_ss */ i,j,k,l,(select /*+ recompile INDEX_SS */ count(*) from t where j between 101 and 199 ) from t where j = 100 order by 1;
 --Test  use ISS index 
 select /*+ recompile index_ss  */ i,j,k,l,(select /*+ recompile INDEX_SS */ count(*) from t where j between 101 and 199 ) from t where j = 100  limit 1;
 --Test  use ISS index 
