bea8fb685cb3934a596d2d28dc7c7cab2e69652c
swi0110 <joykevin@cubrid.com>
2024-03-27 11:00:41 +0900

    add for CBRD-24565

---
+++ sql/_05_plcsql/_01_testspec/_04_expression/_07_sql_rowcount/_01_basic/cases/normal_basic_truncate.sql
@@ -0,0 +1,22 @@
+--+ server-message on
+-- verified for CBRD-24782
+
+create table temp_table(col int);
+insert into temp_table values (0), (1), (2), (4);
+
+select * from temp_table;
+
+create or replace procedure trunc_test as
+begin
+	truncate temp_table;
+end;
+
+call trunc_test();
+
+select * from temp_table;
+
+drop procedure trunc_test;
+
+drop temp_table;
+
+--+ server-message off
