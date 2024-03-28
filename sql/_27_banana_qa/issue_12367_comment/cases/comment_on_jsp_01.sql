a9fdfc40af037563312f23287707aae185fd77a4
Na, Hyunik <hyunikn@gmail.com>
2024-03-28 19:59:42 +0900

    added order by clauses to select statements

--- sql/_27_banana_qa/issue_12367_comment/cases/comment_on_jsp_01.sql
+++ sql/_27_banana_qa/issue_12367_comment/cases/comment_on_jsp_01.sql
@@ -2,14 +2,14 @@
 create or replace function f(i in int comment 'arg i') return int
 as language java name 'SpTest.testInt(int) return int' comment 'this is the comment for function f';
 
-select sp_name, comment from db_stored_procedure;
-select sp_name, arg_name, comment from db_stored_procedure_args;
+select sp_name, comment from db_stored_procedure order by sp_name;
+select sp_name, arg_name, comment from db_stored_procedure_args order by sp_name;
 
 
 alter function f comment 'new comment for function f';
 
-select sp_name, comment from db_stored_procedure;
-select sp_name, arg_name, comment from db_stored_procedure_args;
+select sp_name, comment from db_stored_procedure order by sp_name;
+select sp_name, arg_name, comment from db_stored_procedure_args order by sp_name;
 
 -- should fail
 alter function f;
@@ -22,13 +22,13 @@ create or replace procedure p(i in int comment 'arg i')
 as language java name 'SpTest.testInt(int)'
 comment 'this is the comment for procedure p';
 
-select sp_name, comment from db_stored_procedure;
-select sp_name, arg_name, comment from db_stored_procedure_args;
+select sp_name, comment from db_stored_procedure order by sp_name;
+select sp_name, arg_name, comment from db_stored_procedure_args order by sp_name;
 
 alter procedure p comment 'new comment for procedure p';
 
-select sp_name, comment from db_stored_procedure;
-select sp_name, arg_name, comment from db_stored_procedure_args;
+select sp_name, comment from db_stored_procedure order by sp_name;
+select sp_name, arg_name, comment from db_stored_procedure_args order by sp_name;
 
 --should fail
 alter procedure p;
