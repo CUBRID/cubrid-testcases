1826a3c551c35091967b382dd57128d0e2a8f698
junsklee <jule1750@gmail.com>
2024-04-05 15:16:49 +0900

    [CBRD-25246] revised table and column names

--- sql/_35_fig_cake/cbrd_25246/cases/cbrd_25246.sql
+++ sql/_35_fig_cake/cbrd_25246/cases/cbrd_25246.sql
@@ -2,11 +2,11 @@
 -- Ensure that cur_pageid and volumeid columns exist in the SELECT_PAGE_INFO table
 -- when the hint /*+ select_page_info */ is used in a SELECT query
 
-drop table if exists t111;
-create table t111 (col1 varchar(20), col2 varchar(20), col3 varchar(20), col4 varchar(20), col5 numeric(20,10), col6 varchar(20), col7 varchar(20), col8 varchar(20));
-select /*+ select_page_info */ * from t111;
+drop table if exists tbl;
+create table tbl (col_a varchar(20), col_b varchar(20), col_c varchar(20), col_d varchar(20), col_e numeric(20,10), col_f varchar(20), col_g varchar(20), col_h varchar(20));
+select /*+ select_page_info */ * from tbl;
 
-insert into t111 select rownum, 1, mod(rownum,10), lpad(to_char(mod(rownum,100)),20,'0'), mod(rownum,1000), lpad(to_char(mod(rownum,10000)),20,'0'), lpad(to_char(mod(rownum,100000)),20,'0'), lpad(to_char(mod(rownum,100000)),20,'0') from db_class a, db_class b, db_class c, db_class d, db_class f, db_class g limit 100;
-select /*+ select_page_info */ * from t111;
+insert into tbl select rownum, 1, mod(rownum,10), lpad(to_char(mod(rownum,100)),20,'0'), mod(rownum,1000), lpad(to_char(mod(rownum,10000)),20,'0'), lpad(to_char(mod(rownum,100000)),20,'0'), lpad(to_char(mod(rownum,100000)),20,'0') from db_class a, db_class b, db_class c, db_class d, db_class f, db_class g limit 100;
+select /*+ select_page_info */ * from tbl;
 
-drop table t111;  
+drop table tbl;  
