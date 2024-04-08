dcd5c272698a6c18670e3e2ef5ec86cbe2a23f4e
swi0110 <joykevin@cubrid.com>
2024-04-08 18:53:19 +0900

    Add test case for CBRD-24785

---
+++ sql/_05_plcsql/_01_testspec/_04_expression/_21_limit/cases/01_limit.sql
@@ -0,0 +1,31 @@
+--+ server-message on
+-- Verified for CBRD-24785
+
+create table t(col varchar);
+insert into t values('limit test 1'),(2);
+
+create or replace procedure limit_test1 as
+	col varchar;
+begin
+	select col into col from t order by col desc limit 1;
+	dbms_output.put_line('result: ' || col);
+end;
+
+call limit_test1();
+
+
+
+create or replace procedure limit_test2 as
+	col varchar;
+begin
+	select 'limit test 2' into col from dual limit 1;
+	dbms_output.put_line('result: ' || col);
+end;
+
+call limit_test2();
+
+drop t;
+drop procedure limit_test1;
+drop procedure limit_test2;
+
+--+ server-message off
