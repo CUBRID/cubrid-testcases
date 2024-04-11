f3cd64c59617a0470a386ec163d2637d9badb2cf
swi0110 <joykevin@cubrid.com>
2024-04-11 14:28:00 +0900

    Add test case for CBRD-24903

---
+++ sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/01_drop_user.sql
@@ -0,0 +1,29 @@
+--+ server-message on
+-- Verified for CBRD-24903
+-- DROP USER does not check stored procedures owned by the user
+
+create user test;
+
+select 'change ''test'' user & create procedure' from dual;
+call login('test','') on class db_user;
+
+create or replace procedure test_proc as
+begin
+	dbms_output.put_line('owner user is test');
+end;
+
+select 'change ''dba'' user & drop test_proc' from dual;
+call login('dba','') on class db_user;
+drop user test;
+select * from db_stored_procedure where sp_name='test_proc';
+
+select 'change ''test'' user & drop test_proc' from dual;
+call login('test','') on class db_user;
+drop procedure test_proc;
+
+select 'change ''dba'' user & data clear' from dual;
+call login('dba','') on class db_user;
+drop user test;
+select * from db_stored_procedure where sp_name='test_proc';
+
+--+ server-message off
