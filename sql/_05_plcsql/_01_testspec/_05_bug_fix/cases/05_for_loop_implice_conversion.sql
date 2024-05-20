b947c48fc120d4a206dad7e1cf696184c99c482d
swi0110 <joykevin@cubrid.com>
2024-05-20 16:07:43 +0900

    reflected review

--- sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/05_for_loop_implice_conversion.sql
+++ sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/05_for_loop_implice_conversion.sql
@@ -15,9 +15,47 @@ BEGIN
 	END LOOP;
 END;
 
+CREATE OR REPLACE procedure loop_with_varchar AS 
+	j int := 0;
+	start_val varchar(10) := '0';
+	end_val  varchar(10) := '10';
+	step_val  varchar(10) := '5';
+BEGIN  
+	FOR i IN start_val .. end_val by step_val LOOP  
+		dbms_output.put_line('for with varchar ' || i);
+	END LOOP;
+ 
+	while j <= end_val LOOP  
+		dbms_output.put_line('while with varchar ' || j);
+		j := j + 5;
+	END LOOP;
+END;
+
+CREATE OR REPLACE procedure loop_with_int_val AS 
+	j int := 0;
+	start_val int := 0;
+	end_val  int := 10;
+	step_val  int := 5;
+BEGIN  
+	FOR i IN start_val .. end_val by step_val LOOP  
+		dbms_output.put_line('for with int ' || i);
+	END LOOP;
+ 
+	while j <= end_val LOOP  
+		dbms_output.put_line('while with int ' || j);
+		j := j + 5;
+	END LOOP;
+END;
+
+
 call poo();
+call loop_with_varchar();
+call loop_with_int_val();
+
 
 drop procedure poo;
+drop procedure loop_with_varchar;
+drop procedure loop_with_int_val;
 
 --+ server-message off
 
