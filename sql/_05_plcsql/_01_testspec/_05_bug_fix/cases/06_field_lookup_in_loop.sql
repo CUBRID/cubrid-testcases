be6c802b8524f7330334226505035ef6080f1555
swi0110 <joykevin@cubrid.com>
2024-05-22 10:11:44 +0900

    reflected review

--- sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/06_field_lookup_in_loop.sql
+++ sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/06_field_lookup_in_loop.sql
@@ -1,7 +1,8 @@
 --+ server-message on
 -- Verified for CBRD-25005
 
-create or replace procedure poo as s varchar;
+create or replace procedure poo as
+    s varchar;
 begin
     for r in (select coll_id id from db_collation) loop
        select coll_name into s from db_collation where coll_id = r.id;
