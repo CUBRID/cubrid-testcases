c87cf8b0eba08fd8ae0d6c0a013d9ed10be653e2
zionyun <seeon0001@naver.com>
2024-05-27 12:47:55 +0900

    add query

--- sql/_35_fig_cake/cbrd_25147/cases/cbrd_25147_3.sql
+++ sql/_35_fig_cake/cbrd_25147/cases/cbrd_25147_3.sql
@@ -9,36 +9,27 @@ CREATE USER test_user3;
 
 CALL login ('test_user1', '') ON CLASS db_user;
 ALTER USER public DROP MEMBERS test_user1;
-
 ALTER USER dba ADD MEMBERS test_user1;
-
 ALTER USER test_user1 ADD MEMBERS dba, test_user2;
-
 SELECT u.name, x.name as groups FROM db_user AS u, TABLE(u.direct_groups) AS g(x);
-
 ALTER USER test_user1 DROP MEMBERS dba;
-
 SELECT u.name, x.name as groups FROM db_user AS u, TABLE(u.direct_groups) AS g(x);
 
 CALL login ('test_user2', '') ON CLASS db_user;
 ALTER USER test_user1 ADD MEMBERS test_user2;
-
-
 ALTER USER test_user3 ADD MEMBERS test_user1;
-
 ALTER USER test_user2 ADD MEMBERS test_user1;
-
+ALTER USER test_user2 ADD MEMBERS test_user3, test_user1, test_user2;
 ALTER USER test_user2 ADD MEMBERS test_user3;
 
 SELECT u.name, x.name as groups FROM db_user AS u, TABLE(u.direct_groups) AS g(x);
-
+ALTER USER test_user2 DROP MEMBERS test_user3, test_user2;
 ALTER USER test_user2 DROP MEMBERS test_user3;
 
 SELECT u.name, x.name as groups FROM db_user AS u, TABLE(u.direct_groups) AS g(x);
-
 CALL login ('dba', '') ON CLASS db_user;
 ALTER USER test_user1 DROP MEMBERS test_user2;
+
 DROP USER test_user1;
 DROP USER test_user2;
 DROP USER test_user3;
-
