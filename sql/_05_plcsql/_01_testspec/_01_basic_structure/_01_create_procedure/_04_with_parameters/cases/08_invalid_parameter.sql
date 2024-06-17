7c3330e41339c15d848f1920e3af68b4cdcb61ed
swi0110 <joykevin@cubrid.com>
2024-06-17 13:40:49 +0900

    reflected review

--- sql/_05_plcsql/_01_testspec/_01_basic_structure/_01_create_procedure/_04_with_parameters/cases/08_invalid_parameter.sql
+++ sql/_05_plcsql/_01_testspec/_01_basic_structure/_01_create_procedure/_04_with_parameters/cases/08_invalid_parameter.sql
@@ -1,11 +1,20 @@
 --+ server-message on
 -- verified the CBRD-24126
 
+-- invalid type
 create or replace procedure test_proc(a invalid_param) as
 begin
 	dbms_output.put_line('check invalid param');
 end;
 
 drop test_proc;
+
+-- unsupported type
+create or replace procedure test_proc1(a json) as
+begin
+    dbms_output.put_line('check unsupported param');
+end;
+
+drop procedure test_proc1;
 
 --_ server-message off
