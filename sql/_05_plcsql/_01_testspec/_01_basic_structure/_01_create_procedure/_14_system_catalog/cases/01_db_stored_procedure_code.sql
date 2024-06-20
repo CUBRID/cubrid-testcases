ee5f23c0d39f6a6814dbcff23b26e75c4d0da148
swi0110 <joykevin@cubrid.com>
2024-06-20 11:18:55 +0900

    modified test case & supplement description

--- sql/_05_plcsql/_01_testspec/_01_basic_structure/_01_create_procedure/_14_system_catalog/cases/01_db_stored_procedure_code.sql
+++ sql/_05_plcsql/_01_testspec/_01_basic_structure/_01_create_procedure/_14_system_catalog/cases/01_db_stored_procedure_code.sql
@@ -1,5 +1,5 @@
 --+ server-message on
--- Verified for CBRD-25227
+-- Verified for CBRD-25227 with CBRD-24902, CBRD-25370
 
 create or replace procedure test_proc(p_char char(1)) as
     ch_1 character(1) := p_char;
@@ -8,8 +8,9 @@ begin
 end;
 
 select
-    name, if (created_time is not null,'no problem','problem') as "created_time",
-    owner, is_static, is_system_generated, stype, scode, otype, ocode
+    name, if (created_time is not null,'no_problem','problem') as "created_time",
+    owner, is_static, is_system_generated, stype, scode, otype,
+    if (ocode is not null,'no_problem','problem') as "ocode"
 from _db_stored_procedure_code;
 
 -- is not authorized
