aff765223a15288bda7b6a559d6671829f7537a2
junsklee <jun1750@github.com>
2024-07-31 10:25:48 +0900

    [CBRD-25318] revised column names for query plans

--- sql/_13_issues/_24_1h/cases/cbrd_25318.sql
+++ sql/_13_issues/_24_1h/cases/cbrd_25318.sql
@@ -3,51 +3,52 @@
 -- Tests primarily consist of scenarios in which orderby_num() is often used. 
 
 drop table if exists tbl;
-create table tbl(col1 int, col2 int);
+create table tbl(col_a int, col_b int);
 insert into tbl values(1,1),(2,2),(3,3),(4,4);
 
 update statistics on tbl with fullscan;
 
 --select '1. Single Subquery';
-select orderby_num() rrnum,rnum,col1,col2 
-from (select orderby_num() rnum,col1,col2 from tbl order by col1) 
+select orderby_num() rrnum,rnum,col_a,col_b 
+from (select orderby_num() rnum,col_a,col_b from tbl order by col_a) 
 where 1=1 order by rnum desc;
 
 --select '2. Subqueries';
-select /*+ recompile */ rrnum,rnum,col1,col2 
-from (select orderby_num() rrnum,rnum,col1,col2 
-from (select orderby_num() rnum,col1,col2 from tbl order by col1) 
-where 1=1 order by col2 desc)
+select /*+ recompile */ rrnum,rnum,col_a,col_b 
+from (select orderby_num() rrnum,rnum,col_a,col_b 
+from (select orderby_num() rnum,col_a,col_b from tbl order by col_a) 
+where 1=1 order by col_b desc)
 where 1=1 order by rrnum;
 
 --select '3. Subqueries - for orderby_num() between';
-select /*+ recompile */ rrnum,rnum,col1,col2 
-from (select orderby_num() rrnum,rnum,col1,col2 
-from (select orderby_num() rnum,col1,col2 from tbl order by col1) 
-where 1=1 order by col2 desc for orderby_num() between 1 and 3)
+select /*+ recompile */ rrnum,rnum,col_a,col_b 
+from (select orderby_num() rrnum,rnum,col_a,col_b 
+from (select orderby_num() rnum,col_a,col_b from tbl order by col_a) 
+where 1=1 order by col_b desc for orderby_num() between 1 and 3)
 where 1=1 order by rrnum;
 
 --select '4. Subqueries - order by limit (limit 3)';
-select /*+ recompile */ rrnum,rnum,col1,col2 
-from (select orderby_num() rrnum,rnum,col1,col2 
-from (select orderby_num() rnum,col1,col2 from tbl order by col1) 
-where 1=1 order by col2 desc limit 3)
+select /*+ recompile */ rrnum,rnum,col_a,col_b 
+from (select orderby_num() rrnum,rnum,col_a,col_b 
+from (select orderby_num() rnum,col_a,col_b from tbl order by col_a) 
+where 1=1 order by col_b desc limit 3)
 where 1=1 order by rrnum;
 
 --select '5. Subqueries - order by limit & offset (offset 1, limit 3)';
-select /*+ recompile */ rrnum,rnum,col1,col2 
-from (select orderby_num() rrnum,rnum,col1,col2 
-from (select orderby_num() rnum,col1,col2 from tbl order by col1) 
-where 1=1 order by col2 desc limit 1, 3)
+select /*+ recompile */ rrnum,rnum,col_a,col_b 
+from (select orderby_num() rrnum,rnum,col_a,col_b 
+from (select orderby_num() rnum,col_a,col_b from tbl order by col_a) 
+where 1=1 order by col_b desc limit 1, 3)
 where 1=1 order by rrnum;
 
 --select '6. 3 Subqueries';
-select /*+ recompile */ rrrnum,rrnum,rnum,col1,col2 
-from (select orderby_num() rrrnum,rrnum,rnum,col1,col2 
-from (select orderby_num() rrnum,rnum,col1,col2 
-from (select orderby_num() rnum,col1,col2 from tbl order by col1)
-where 1=1 order by col1)
-where 1=1 order by col2 desc)  
+select /*+ recompile */ rrrnum,rrnum,rnum,col_a,col_b 
+from (select orderby_num() rrrnum,rrnum,rnum,col_a,col_b 
+from (select orderby_num() rrnum,rnum,col_a,col_b 
+from (select orderby_num() rnum,col_a,col_b from tbl order by col_a)
+where 1=1 order by col_a)
+where 1=1 order by col_b desc)  
 where 1=1 order by rrrnum;
 
 drop table tbl;
+
