ca4dc56bb8dc218596c81bcb61097e59e2d53f75
tw-kang <twkang@cubrid.com>
2024-08-07 02:29:07 +0900

    reflect review

--- sql/_13_issues/_24_2h/cases/cbrd_25323.sql
+++ sql/_13_issues/_24_2h/cases/cbrd_25323.sql
@@ -1,4 +1,5 @@
 -- This test case verifies CBRD-25323 issue.
+-- Also, this test case is an extension of CBRD-25277.
 -- The 'Eliminate INNER JOIN' optimization is performed for the first table, and the join type for the second table is removed. 
 -- If the join type for the second table is OUTER JOIN, the 'Eliminate INNER JOIN' optimization is not performed for the first table.
 
@@ -6,7 +7,7 @@ drop table if exists tb_child;
 drop table if exists ta_parent;
 drop table if exists tc_dummy;
 
-select '' as "test data";
+evaluate 'test data';
 create table ta_parent (p_ca int primary key, p_cb int);
 create table tb_child (c_ca int primary key, c_cb int);
 alter table tb_child add c_parent_ca int not null references ta_parent (p_ca);
@@ -23,10 +24,10 @@ update statistics on tb_child with fullscan;
 update statistics on tc_dummy with fullscan;
 
 
-select 'AS-IS Eliminate INNER JOIN optimization is not performed.
-TO-BE
-- The join type of the second table is LEFT OUTER JOIN
-- With ANSI JOIN syntax' as "test case 7";
+--Eliminate INNER JOIN optimization is not performed.
+-- The join type of the second table is LEFT OUTER JOIN
+-- With ANSI JOIN syntax' 
+evaluate 'test case 7';
 select /*+ recompile ordered */
   c.c_cb, d.d_cb
 from
@@ -36,11 +37,10 @@ from
   inner join tb_child c on c.c_parent_ca = p.p_ca;
 
 
-select 'AS-IS Eliminate INNER JOIN optimization is performed.
-TO-BE
-- The join type of the second table is LEFT OUTER JOIN
-- With ORACLE JOIN syntax
-' as "test case 8";
+---Eliminate INNER JOIN optimization is performed.
+--The join type of the second table is LEFT OUTER JOIN
+--With ORACLE JOIN syntax
+evaluate 'test case 8';
 select /*+ recompile ordered */
   c.c_cb, d.d_cb
 from
@@ -52,11 +52,10 @@ where
   and c.c_parent_ca = p.p_ca;
 
 
-select 'AS-IS Eliminate INNER JOIN optimization is not performed.
-TO-BE
-- The join type of the second table is LEFT OUTER JOIN
-- With ORACLE JOIN syntax
-' as "test case 9";
+---Eliminate INNER JOIN optimization is not performed.
+--The join type of the second table is LEFT OUTER JOIN
+--With ORACLE JOIN syntax
+evaluate 'test case 9';
 select /*+ recompile ordered */
   c.c_cb, d.d_cb
 from
@@ -68,11 +67,10 @@ where
   and c.c_parent_ca = p.p_ca;
 
 
-select 'AS-IS Eliminate INNER JOIN optimization is not performed.
-TO-BE
-- The join type of the second table is NATURAL JOIN
-- With ANSI JOIN syntax
-' as "test case 10";
+---Eliminate INNER JOIN optimization is not performed.
+--The join type of the second table is NATURAL JOIN
+--With ANSI JOIN syntax
+evaluate 'test case 10';
 select /*+ recompile ordered */
   c.c_cb, t_p.p_cb
 from
@@ -81,11 +79,10 @@ from
   inner join tb_child c on c.c_parent_ca = p.p_ca;
 
 
-select 'AS-IS Eliminate INNER JOIN optimization is performed.
-TO-BE
-- The join type of the second table is NATURAL JOIN
-- With ANSI JOIN syntax
-' as "test case 11";
+---Eliminate INNER JOIN optimization is performed.
+--The join type of the second table is NATURAL JOIN
+--With ANSI JOIN syntax
+evaluate 'test case 11';
 select /*+ recompile ordered */
   c.c_cb, t_c.c_cb
 from
@@ -94,25 +91,23 @@ from
   inner join tb_child c on c.c_parent_ca = p.p_ca;
 
 
-select 'AS-IS Eliminate INNER JOIN optimization is not performed.
-TO-BE
-- The join type of the second table is RIGHT OUTER JOIN
-- With ANSI JOIN syntax
-' as "test case 12";
+---Eliminate INNER JOIN optimization is not performed.
+--The join type of the second table is RIGHT OUTER JOIN
+--With ANSI JOIN syntax
+evaluate 'test case 12';
 select /*+ recompile ordered */
   c.c_cb, d.d_cb
 from
   ta_parent p
   right outer join tc_dummy d /* With ANSI JOIN syntax */
-    on d.d_ca = 2 
+    on d.d_ca = 2
   inner join tb_child c on c.c_parent_ca = p.p_ca;
 
 
-select 'Eliminate INNER JOIN optimization is performed.
-TO-BE
-- The join type of the second table is RIGHT OUTER JOIN
-- With ORACLE JOIN syntax
-' as "test case 13";
+---Eliminate INNER JOIN optimization is performed.
+--The join type of the second table is RIGHT OUTER JOIN
+--With ORACLE JOIN syntax
+evaluate 'test case 13';
 select /*+ recompile ordered */
   c.c_cb, d.d_cb
 from
@@ -124,25 +119,22 @@ where
   and c.c_parent_ca = p.p_ca;
 
 
-select 'Eliminate INNER JOIN optimization is performed.
-TO-BE
-- The join type of the second table is CROSS JOIN
-- With ANSI JOIN syntax
- *
-' as "test case 14";
+---Eliminate INNER JOIN optimization is performed.
+--The join type of the second table is CROSS JOIN
+--With ANSI JOIN syntax
+evaluate 'test case 14';
 select /*+ recompile ordered */
   c.c_cb, d.d_cb
 from
   ta_parent p
   cross join tc_dummy d /* With ANSI JOIN syntax */
   inner join tb_child c on c.c_parent_ca = p.p_ca;
+	
 
-
-select 'Eliminate INNER JOIN optimization is performed.
-TO-BE
-- The join type of the second table is CROSS JOIN
-- With ORACLE JOIN syntax
-' as "test case 15";
+---Eliminate INNER JOIN optimization is performed.
+--The join type of the second table is CROSS JOIN
+--With ORACLE JOIN syntax
+evaluate 'test case 15';
 select /*+ recompile ordered */
   c.c_cb, d.d_cb
 from
