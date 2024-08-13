b7da711c37f02bb457839b5a043fd1371a9c867c
swi0110 <joykevin@cubrid.com>
2024-08-13 17:11:28 +0900

    reflected review

--- sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/12_ignore_precision_scale.sql
+++ sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/12_ignore_precision_scale.sql
@@ -3,11 +3,11 @@
 
 create or replace procedure p0(n numeric) as 
 begin
+    dbms_output.put_line('in p0');
     null;
 end;
 create or replace procedure p_test0 as
 begin
-    dbms_output.put_line('before p0');
     p0(7);
     dbms_output.put_line('after p0');
 end;
@@ -18,11 +18,11 @@ drop procedure p0;
 
 create or replace procedure p1(n char) as 
 begin
+    dbms_output.put_line('in p1');
     null;
 end;
 create or replace procedure p_test1 as
 begin
-    dbms_output.put_line('before p1');
     p1(7);
     dbms_output.put_line('after p1');
 end;
@@ -33,11 +33,11 @@ drop procedure p1;
 
 create or replace procedure p2(n varchar) as 
 begin
+    dbms_output.put_line('in p2');
     null;
 end;
 create or replace procedure p_test2 as
 begin
-    dbms_output.put_line('before p2');
     p2(7);
     dbms_output.put_line('after p2');
 end;
@@ -49,12 +49,12 @@ drop procedure p2;
 
 create or replace function f0 return numeric as
 begin
+    dbms_output.put_line('in f0');
     return null;
 end;
 create or replace procedure f_test0 as
     n numeric;
 begin
-    dbms_output.put_line('before f0');
     n := f0;
     dbms_output.put_line(n);
     dbms_output.put_line('after f0');
@@ -66,12 +66,12 @@ drop function f0;
 
 create or replace function f1 return char as
 begin
+    dbms_output.put_line('in f1');
     return null;
 end;
 create or replace procedure f_test1 as
     n numeric;
 begin
-    dbms_output.put_line('before f1');
     n := f1;
     dbms_output.put_line(n);
     dbms_output.put_line('after f1');
@@ -83,12 +83,12 @@ drop function f1;
 
 create or replace function f2 return varchar as
 begin
+    dbms_output.put_line('in f2');
     return null;
 end;
 create or replace procedure f_test2 as
     n numeric;
 begin
-    dbms_output.put_line('before f2');
     n := f2;
     dbms_output.put_line(n);
     dbms_output.put_line('after f2');
