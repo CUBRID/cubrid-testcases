7703780eb4da6a2402f91d7babe8295f90bd9a72
kiho-um <kiho@cubrid.com>
2024-09-03 12:57:19 +0900

    [CBRD-25090] modify

--- sql/_13_issues/_24_2h/cases/cbrd_25090.sql
+++ sql/_13_issues/_24_2h/cases/cbrd_25090.sql
@@ -1,3 +1,5 @@
+-- A core dump occurs when NA is specified in the select list in the create view statement
+
 drop table if exists tbl;
 
 create table tbl(id int, v int);
@@ -5,3 +7,4 @@ create table tbl(id int, v int);
 create view v1 as select NA x from tbl;
 
 drop view v1;
+drop table tbl;
