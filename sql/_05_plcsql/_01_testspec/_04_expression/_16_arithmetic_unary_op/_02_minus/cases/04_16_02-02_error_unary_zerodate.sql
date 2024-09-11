16eb82f4621e6498ed88c7654f5f3903af21a580
kwonhoil <hikwon@cubrid.com>
2024-09-11 15:10:38 +0900

    [CBRD-25150] The review content has been revised.

--- sql/_05_plcsql/_01_testspec/_04_expression/_16_arithmetic_unary_op/_02_minus/cases/04_16_02-02_error_unary_zerodate.sql
+++ sql/_05_plcsql/_01_testspec/_04_expression/_16_arithmetic_unary_op/_02_minus/cases/04_16_02-02_error_unary_zerodate.sql
@@ -1,48 +1,62 @@
---+ server-message on
-
--- normal: basic usage of unary operator '+' with zero date, datetime and time values
+-- error : basic usage of unary operator '-'  with zero date, datetime and time values
 -- Verified for CBRD-25015
 
 create or replace procedure t() as
     a date := date'0000-00-00';
-    b date := +a;
+    b date := -a;
 begin
     dbms_output.put_line(a);
     dbms_output.put_line(b);
 end;
-call t();
 
 
 create or replace procedure t() as
     a datetime := datetime'0000-00-00 00:00:00.000';
-    b datetime := +a;
+    b datetime := -a;
 begin
     dbms_output.put_line(a);
     dbms_output.put_line(b);
 end;
-call t();
 
 
 create or replace procedure t() as
     a datetime := timestamp'0000-00-00 00:00:00';
-    b datetime := +a;
+    b datetime := -a;
 begin
     dbms_output.put_line(a);
     dbms_output.put_line(b);
 end;
-call t();
 
 
 create or replace procedure t() as
     a time := time'00:00:00';
-    b time := +a;
+    b time := -a;
 begin
     dbms_output.put_line(a);
     dbms_output.put_line(b);
 end;
+
+
+create or replace procedure t as
+begin
+    dbms_output.put_line(date'0000-00-00' - 1);
+end;
+        
 call t();
 
-drop procedure t;
+create or replace procedure t as
+begin
+    dbms_output.put_line(datetime'0000-00-00 00:00:00.000' - 1);
+end; 
+
+call t();
+
+create or replace procedure t as
+begin
+    dbms_output.put_line(timestamp'0000-00-00 00:00:00' - 1);
+end;
+
+call t();
 
 --+ server-message off
 
