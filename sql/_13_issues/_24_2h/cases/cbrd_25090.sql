08c3f82f20f26dcd8397af93717360a526f6cc30
kiho-um <kiho@cubrid.com>
2024-09-11 12:47:16 +0900

    CBRD-25090 add alter view v1 query statements

--- sql/_13_issues/_24_2h/cases/cbrd_25090.sql
+++ sql/_13_issues/_24_2h/cases/cbrd_25090.sql
@@ -9,5 +9,19 @@ select NA x from tbl;
 create view v1 as select NA x from tbl;
 alter view v1 add query select null from tbl;
 
+-- view create is query statement without from clause
+alter view v1 add query select null;
+
+-- scalar query
+alter view v1 add query select (select null) from tbl;
+
+alter view v1 add query select (select (select null) ) from tbl;
+alter view v1 add query select (select (select (select null) ) ) from tbl;
+
+alter view v1 add query select (select (select (select (select (select null) ) ) ) ) from tbl;
+
+-- single quotation
+alter view v1 add query select '' from tbl;
+
 drop view v1;
 drop table tbl;
