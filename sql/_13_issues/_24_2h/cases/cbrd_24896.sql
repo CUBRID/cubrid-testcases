c969944554086b394ab3b2a96c23dca8bdb3b4a6
tw-kang <twkang@cubrid.com>
2024-09-24 18:05:54 +0900

    add new testcase

---
+++ sql/_13_issues/_24_2h/cases/cbrd_24896.sql
@@ -0,0 +1,18 @@
+-- create table
+drop table if exists tbl;
+create table tbl (col_a int, col_b int, col_c int, col_d int);
+insert into tbl values(1,1,1,1),(2,2,2,2),(3,3,3,3);
+create index idx on tbl(col_a,col_b,col_c);
+update statistics on tbl;
+
+EVALUATE 'index scan case';
+select /*+ recompile */ * from tbl where (col_a,col_b) in ((1,1),(2,2));
+select /*+ recompile */ * from tbl where (col_a,col_c) in ((1,1),(2,2));
+select /*+ recompile */ * from tbl where (col_a,col_d) in ((1,1),(2,2));
+
+EVALUATE 'sscan case';
+select /*+ recompile */ * from tbl where {col_a,col_b} in ((1,1),(2,2));
+select /*+ recompile */ * from tbl where {col_a} = {2};
+select /*+ recompile */ * from tbl where {col_a} = {};
+
+drop table tbl;
