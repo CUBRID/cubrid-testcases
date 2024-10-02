254ac38c2e8edb4336e959545ec34b11d2d8b91d
zionyun <seeon0001@naver.com>
2024-10-02 16:38:04 +0900

    add new testcase for cbrd-25393

---
+++ sql/_35_fig_cake/cbrd_25393/cases/cbrd_25393.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS a_tbl;
+DROP VIEW IF EXISTS a_view;
+
+CREATE TABLE a_tbl (
+    id INT NOT NULL,
+    phone VARCHAR(10)
+);
+INSERT INTO a_tbl VALUES(1,'111-1111'), (2,'222-2222'), (3, '333-3333'), (4, NULL), (5, NULL);
+
+SELECT * FROM a_tbl;
+
+CREATE VIEW a_view (
+    "ID", "PHONE", "EXTRA"
+) AS
+SELECT
+    id,
+    phone,
+    NULL AS extra
+FROM a_tbl;
+
+SELECT * FROM a_view;
+
+DROP TABLE IF EXISTS a_tbl;
+DROP VIEW IF EXISTS a_view;
+
