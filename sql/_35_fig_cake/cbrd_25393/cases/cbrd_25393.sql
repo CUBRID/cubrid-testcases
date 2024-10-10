e47b5a3ee8f3ab1574c0a27979ec9e139d974f4d
zionyun <seeon0001@naver.com>
2024-10-10 11:26:50 +0900

    add order by

--- sql/_35_fig_cake/cbrd_25393/cases/cbrd_25393.sql
+++ sql/_35_fig_cake/cbrd_25393/cases/cbrd_25393.sql
@@ -7,7 +7,7 @@ CREATE TABLE a_tbl (
 );
 INSERT INTO a_tbl VALUES(1,'111-1111'), (2,'222-2222'), (3, '333-3333'), (4, NULL), (5, NULL);
 
-SELECT * FROM a_tbl;
+SELECT * FROM a_tbl ORDER BY id;
 
 CREATE VIEW a_view (
     "ID", "PHONE", "EXTRA"
@@ -18,7 +18,7 @@ SELECT
     NULL AS extra
 FROM a_tbl;
 
-SELECT * FROM a_view;
+SELECT * FROM a_view ORDER BY id;
 
 DROP TABLE IF EXISTS a_tbl;
 DROP VIEW IF EXISTS a_view;
