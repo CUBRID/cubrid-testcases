4184f6b834370c20c4e48f27e777adc43063dcda
zionyun <seeon0001@naver.com>
2024-10-11 09:40:15 +0900

    including case

--- sql/_35_fig_cake/cbrd_25393/cases/cbrd_25393.sql
+++ sql/_35_fig_cake/cbrd_25393/cases/cbrd_25393.sql
@@ -1,3 +1,6 @@
+-- This test case verifies the following issue: CBRD-25393.
+-- Oracle-style CREATE VIEW statements with NULL values are supported.
+
 DROP TABLE IF EXISTS a_tbl;
 DROP VIEW IF EXISTS a_view;
 
@@ -20,6 +23,12 @@ FROM a_tbl;
 
 SELECT * FROM a_view ORDER BY id;
 
+--Add case : Check if no errors occur.
+CREATE OR REPLACE VIEW "b_view"("id", "name") AS
+SELECT 1 AS id, sysdate name FROM dual
+UNION ALL
+SELECT null AS id, null AS name FROM dual;
+
 DROP TABLE IF EXISTS a_tbl;
 DROP VIEW IF EXISTS a_view;
-
+DROP VIEW IF EXISTS b_view;
