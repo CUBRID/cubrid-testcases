5e3b8b9f3a1f373388a7eaca0fbd09be22639b28
junsklee <jule1750@gmail.com>
2024-10-11 15:54:23 +0900

    [CBRD-25396] added a view test and an inline where clause test

--- sql/_13_issues/_24_2h/cases/cbrd_25396.sql
+++ sql/_13_issues/_24_2h/cases/cbrd_25396.sql
@@ -157,3 +157,62 @@ FROM
         ORDER BY
             t.col_a
     ) X;
+
+--Verify NO_MERGE hint functions correctly in the WHERE clause by using an embedded inline view
+evaluate '6. NO_MERGE Hint in an inline view within the WHERE clause';
+SELECT /*+ RECOMPILE */
+    (
+        SELECT 
+            col_c
+        FROM
+            tbl_b 
+        WHERE
+            col_c = (
+                SELECT inline_view.col_a 
+                FROM (
+                    SELECT /*+ NO_MERGE */
+                        t.col_a
+                        ,( select 1 from tbl_a where col_a = t.col_a ) as col_x
+                    FROM
+                        tbl_a t
+                    GROUP BY
+                        t.col_a
+                    ORDER BY
+                        t.col_a
+                ) inline_view
+                WHERE ROWNUM = 1
+            )
+        AND ROWNUM = 1
+    ) AS X;
+
+
+evaluate '7. NO_MERGE Hint in a View';
+CREATE OR REPLACE VIEW view_a AS
+SELECT /*+ NO_MERGE */
+    t.col_a
+    ,( select 1 from tbl_a where col_a = t.col_a ) as col_x
+FROM
+    tbl_a t
+GROUP BY
+    t.col_a
+ORDER BY
+    t.col_a;
+
+SELECT /*+ RECOMPILE */
+    (
+        SELECT 
+            col_c
+        FROM
+            tbl_b 
+        WHERE
+            col_c = view_a.col_a
+        AND ROWNUM = 1
+    ) AS test
+FROM
+    view_a;
+
+
+drop view view_a;
+drop table tbl_a;
+drop table tbl_b;
+
