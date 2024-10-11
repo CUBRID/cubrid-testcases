522046e4202968d06cea192ca617b359e7d2be00
swi0110 <joykevin@cubrid.com>
2024-10-11 14:07:00 +0900

    reflected review

--- sql/_05_plcsql/_01_testspec/_04_expression/_22_grant/cases/01_grant_plcsql.sql
+++ sql/_05_plcsql/_01_testspec/_04_expression/_22_grant/cases/01_grant_plcsql.sql
@@ -1,6 +1,7 @@
 --+ server-message on
 -- verified the CBRD-25499, CBRD-25506
 
+
 -- GRANT test
 CREATE OR REPLACE FUNCTION sp1() return varchar as
 begin
@@ -9,13 +10,20 @@ end;
 CREATE USER t1 GROUPS dba;
 GRANT EXECUTE ON PROCEDURE sp1 TO t1;
 
+-- test with table&view
+create table test1 (col1 int);
+insert into test1 values(0),(2);
+create view view1 as select * from test1;
+GRANT SELECT ON test1 TO t1;
+GRANT SELECT ON view1 TO t1;
+
 CREATE OR REPLACE FUNCTION t1.sp2() return varchar as
 begin
     return 't1 hello';
 end;
 GRANT EXECUTE ON PROCEDURE t1.sp2 TO dba;
 
-SELECT * FROM db_auth WHERE grantee_name = 'T1';
+SELECT * FROM db_auth WHERE grantee_name = 'T1' ORDER BY object_name;
 SHOW GRANTS FOR T1;
 
 -- DBA needs not permission
@@ -24,7 +32,7 @@ SHOW GRANTS FOR DBA;
 call login('t1','') on class db_user;
 
 -- in t1
-SELECT * FROM db_auth WHERE grantee_name = 'T1';
+SELECT * FROM db_auth WHERE grantee_name = 'T1' ORDER BY object_name;
 SHOW GRANTS;
 
 drop function sp2;
@@ -38,5 +46,6 @@ SHOW GRANTS FOR T1;
 
 drop user t1;
 drop function sp1;
+drop test1, view1;
 
 --+ server-message off
