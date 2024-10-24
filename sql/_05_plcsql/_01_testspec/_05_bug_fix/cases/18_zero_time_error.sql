669c4cac8aaf7b11f96f3667805a8c7c3c4f0467
swi0110 <joykevin@cubrid.com>
2024-10-24 17:15:31 +0900

    reflected review

--- sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/18_zero_time_error.sql
+++ sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/18_zero_time_error.sql
@@ -1,7 +1,7 @@
 --+ server-message on
 -- verified the CBRD-25594
 
-evaluate 'test javasp';
+select 'test javasp' from dual;
 create or replace function func_ts(t timestamp) return timestamp as
 language java name 'SpTest.testTimestamp(java.sql.Timestamp) return java.sql.Timestamp';
 
@@ -11,9 +11,13 @@ language java name 'SpTest.testTime(java.sql.Time) return java.sql.Time';
 create or replace function func_date(t date) return date as
 language java name 'SpTest.testDate(java.sql.Date) return java.sql.Date';
 
+create or replace function func_dt(t datetime) return datetime as
+language java name 'SpTest.testTimestamp(java.sql.Timestamp) return java.sql.Timestamp';
+
 select func_ts(timestamp'0000-00-00 00:00:00') from dual;
 select func_time('00:00:00') from dual;
 select func_date('0000-00-00') from dual;
+select func_ts(datetime'0000-00-00 00:00:00') from dual;
 
 drop function func_ts;
 drop function func_time;
