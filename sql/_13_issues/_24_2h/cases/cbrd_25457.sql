40e1a805b808c1cd6907a0536f66702958fd6c73
kiho-um <kiho@cubrid.com>
2024-10-24 11:17:25 +0900

    [CBRD-25457] Add test case confirmation for column property modify db_attribute, db_serial

--- sql/_13_issues/_24_2h/cases/cbrd_25457.sql
+++ sql/_13_issues/_24_2h/cases/cbrd_25457.sql
@@ -13,10 +13,14 @@ CREATE TABLE tbl1 (col1 INTEGER);
 alter table tbl1 modify col1 int default 0;
 alter table tbl1 modify col1 int auto_increment;
 show create table tbl1;
+select attr_name, owner_name, data_type, default_value, comment from db_attribute where class_name = 'tbl1';
+select unique_name, name, owner, current_val, increment_val from db_serial where class_name = 'tbl1';
 
 --Then, modify it to the default value of null.
 alter table tbl1 modify col1 int default null;
 show create table tbl1;
+select attr_name, owner_name, data_type, default_value, comment from db_attribute where class_name = 'tbl1';
+select unique_name, name, owner, current_val, increment_val from db_serial where class_name = 'tbl1';
 
 -- 2. In the case of a column without an attribute using the alter statement, modify it to auto_increment and then modify the default value to 0 using the alter statement.
 CREATE TABLE tbl2 (col1 INTEGER);
@@ -24,22 +28,29 @@ CREATE TABLE tbl2 (col1 INTEGER);
 alter table tbl2 modify col1 int auto_increment;
 alter table tbl2 modify col1 int default 0;
 show create table tbl2;
+select attr_name, owner_name, data_type, default_value, comment from db_attribute where class_name = 'tbl2';
+select unique_name, name, owner, current_val, increment_val from db_serial where class_name = 'tbl2';
 
 -- Add column comment using alter statement.
 alter table tbl2 modify col1 int comment '2) test2';
 show create table tbl2;
+select attr_name, owner_name, data_type, default_value, comment from db_attribute where class_name = 'tbl2';
 
 -- 3. Modifying the default attribute in a column with the auto_increment attribute using the alter statement.
 CREATE TABLE tbl3 (col1 INTEGER AUTO_INCREMENT(1,1) NOT NULL);
 
 alter table tbl3 modify col1 int default 0;
 show create table tbl3;
+select attr_name, owner_name, data_type, default_value from db_attribute where class_name = 'tbl3';
+select unique_name, name, owner, current_val, increment_val from db_serial where class_name = 'tbl3';
 
 -- 4. Modify the auto_increment property on a column with the default property using the alter statement.
 CREATE TABLE tbl4 (col1 INTEGER DEFAULT 0);
 
 alter table tbl4 modify col1 int auto_increment;
 show create table tbl4;
+select attr_name, owner_name, data_type, default_value from db_attribute where class_name = 'tbl4';
+select unique_name, name, owner, current_val, increment_val from db_serial where class_name = 'tbl4';
 
 -- Modify column type using alter statement.
 ALTER TABLE tbl4 MODIFY col1 VARCHAR (200);
@@ -49,15 +60,19 @@ CREATE TABLE tbl5 (col1 INTEGER AUTO_INCREMENT(1,1));
 
 ALTER TABLE tbl5 ALTER COLUMN col1 SET default 0;
 show create table tbl5;
+select attr_name, owner_name, data_type, default_value from db_attribute where class_name = 'tbl5';
+select unique_name, name, owner, current_val, increment_val from db_serial where class_name = 'tbl5';
  
 -- 6. Using the alter statement, modify the default 0 attribute or default null in a column with the default attribute using the alter column statementi.
 CREATE TABLE tbl6 (col1 INTEGER DEFAULT 2);
 
 ALTER TABLE tbl6 ALTER COLUMN col1 SET default 0;
 show create table tbl6;
+select attr_name, owner_name, data_type, prec, default_value from db_attribute where class_name = 'tbl6';
 
 ALTER TABLE tbl6 MODIFY col1 VARCHAR (200) default null;
 show create table tbl6;
+select attr_name, owner_name, data_type, prec, default_value from db_attribute where class_name = 'tbl6';
 
 -- All table drop
 drop table if exists tbl1, tbl2, tbl3, tbl4, tbl5, tbl6;
