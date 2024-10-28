aa253e0d735a11fe9ad22a4f182a90bce109a43c
swi0110 <joykevin@cubrid.com>
2024-10-28 15:16:04 +0900

    reflected review

--- sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/19_user_cursor_system_view.sql
+++ sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/19_user_cursor_system_view.sql
@@ -25,7 +25,7 @@ call u1.test_sp1();
 drop procedure u1.test_sp1;
 
 
-evaluate 'case 2: search to db_class with dba function on cursor';
+evaluate 'case 2: u1 is search to db_class with dba function on cursor';
 CREATE or replace FUNCTION hello(i String) RETURN STRING
 AS LANGUAGE JAVA
 NAME 'SpTest.Hello(java.lang.String) return java.lang.String';
@@ -44,8 +44,37 @@ call u1.test_sp2();
 
 drop procedure u1.test_sp2;
 
-drop user u1;
 drop function hello;
+
+
+evaluate 'u1 is call the u2(=dba group) function then u1 is possible to watch the dba.t1 value';
+create table t1(col1 char(1));
+insert t1 values('z');
+
+create user u2 groups dba;
+
+create or replace procedure u2.test_sp1 as
+    cursor c1 is select col1 from dba.t1;
+begin
+    FOR r IN c1 LOOP
+        DBMS_OUTPUT.put_line('dba.t1 value: ' || r.col1);
+    END LOOP;
+end;
+
+grant execute on procedure u2.test_sp1 to u1;
+
+evaluate 'connect u1';
+call login('u1','') on class db_user;
+
+call u2.test_sp1();
+
+evaluate 'connect dba';
+call login('dba','') on class db_user;
+
+drop procedure u2.test_sp1;
+drop user u1;
+drop user u2;
+drop t1;
 
 
 --+ server-message off
