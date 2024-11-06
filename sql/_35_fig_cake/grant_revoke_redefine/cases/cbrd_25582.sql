9cd9b1c01ca8471f97b9178ab127000e712ce68c
swi0110 <joykevin@cubrid.com>
2024-11-06 17:45:13 +0900

    add test case cbrd-25582

---
+++ sql/_35_fig_cake/grant_revoke_redefine/cases/cbrd_25582.sql
@@ -0,0 +1,195 @@
+-- Verified the CBRD-25582
+-- bug fix: do not cascade on revoke
+
+create user u1;
+create user u2;
+create user u3;
+create user u4;
+create user u5;
+
+create table u1.tbl (col1 int);
+
+
+evaluate 'case 1: execute to revoke on dba';
+GRANT SELECT ON u1.TBL TO dba WITH GRANT OPTION;
+evaluate 'ERROR: Cannot issue GRANT/REVOKE to owner of a class';
+GRANT SELECT ON u1.TBL TO u1 WITH GRANT OPTION;
+
+evaluate 'connect to u1, u1.tbl grant to u2';
+call login('u1','') on class db_user;
+GRANT SELECT ON u1.TBL TO u2 WITH GRANT OPTION;
+
+evaluate 'connect to u2, u1.tbl grant to u3';
+call login('u2','') on class db_user;
+GRANT SELECT ON u1.TBL TO u3 WITH GRANT OPTION;
+
+evaluate 'connect to u3, u1.tbl grant to u4';
+call login('u3','') on class db_user;
+GRANT SELECT ON u1.TBL TO u4 WITH GRANT OPTION;
+
+evaluate 'connect to u4, u1.tbl grant to u5';
+call login('u4','') on class db_user;
+GRANT SELECT ON u1.TBL TO u5 WITH GRANT OPTION;
+
+select * from db_auth where grantee_name != 'PUBLIC' order by grantor_name;
+
+
+evaluate 'init test, connect to dba';
+call login('dba','') on class db_user;
+REVOKE SELECT ON u1.TBL FROM u2;
+
+select * from db_auth where grantee_name != 'PUBLIC' order by grantor_name;
+
+
+
+evaluate 'case 2: execute to revoke on owner';
+evaluate 'ERROR: Cannot issue GRANT/REVOKE to owner of a class';
+GRANT SELECT ON u1.TBL TO u1 WITH GRANT OPTION;
+
+GRANT SELECT ON u1.TBL TO u2 WITH GRANT OPTION;
+
+evaluate 'connect to u1, u1.tbl grant to u2, u3';
+call login('u1','') on class db_user;
+GRANT SELECT ON u1.TBL TO u2 WITH GRANT OPTION;
+GRANT SELECT ON u1.TBL TO u3 WITH GRANT OPTION;
+
+evaluate 'connect to u2, u1.tbl grant to u3';
+call login('u2','') on class db_user;
+GRANT SELECT ON u1.TBL TO u3 WITH GRANT OPTION;
+
+evaluate 'connect to u3, u1.tbl grant to u4';
+call login('u3','') on class db_user;
+GRANT SELECT ON u1.TBL TO u4 WITH GRANT OPTION;
+
+evaluate 'connect to u4, u1.tbl grant to u5';
+call login('u4','') on class db_user;
+GRANT SELECT ON u1.TBL TO u5 WITH GRANT OPTION;
+
+select * from db_auth where grantee_name != 'PUBLIC' order by grantor_name;
+
+
+evaluate 'connect to u1, revoke to u1.tbl from u2';
+call login('u1','') on class db_user;
+REVOKE SELECT ON u1.TBL FROM u2;
+
+select * from db_auth where grantee_name != 'PUBLIC' order by grantor_name;
+
+evaluate 'init test, connect to dba';
+REVOKE SELECT ON u1.TBL FROM u3;
+
+select * from db_auth where grantee_name != 'PUBLIC' order by grantor_name;
+
+call login('dba','') on class db_user;
+
+
+
+evaluate 'case 3: execute to revoke on grantable user (u2)';
+evaluate 'connect to u1, u1.tbl grant to u2';
+call login('u1','') on class db_user;
+GRANT SELECT ON u1.TBL TO u2 WITH GRANT OPTION;
+
+evaluate 'connect to u2, u1.tbl grant to u3';
+call login('u2','') on class db_user;
+GRANT SELECT ON u1.TBL TO u3 WITH GRANT OPTION;
+
+evaluate 'connect to u3, u1.tbl grant to u4';
+call login('u3','') on class db_user;
+GRANT SELECT ON u1.TBL TO u4 WITH GRANT OPTION;
+
+evaluate 'connect to u4, u1.tbl grant to u5';
+call login('u4','') on class db_user;
+GRANT SELECT ON u1.TBL TO u5 WITH GRANT OPTION;
+
+select * from db_auth where grantee_name != 'PUBLIC' order by grantor_name;
+
+
+evaluate 'connect to u2, revoke to u1.tbl from u3';
+call login('u2','') on class db_user;
+REVOKE SELECT ON u1.TBL FROM u3;
+
+select * from db_auth where grantee_name != 'PUBLIC'  order by grantor_name;
+
+
+evaluate 'init test';
+call login('u1','') on class db_user;
+REVOKE SELECT ON u1.TBL FROM u2;
+
+select * from db_auth where grantee_name != 'PUBLIC'  order by grantor_name;
+
+call login('dba','') on class db_user;
+
+
+
+evaluate 'case 4: grant to all user and execute revoke';
+call login('u1','') on class db_user;
+GRANT SELECT ON u1.TBL TO u2 WITH GRANT OPTION;
+GRANT SELECT ON u1.TBL TO u3 WITH GRANT OPTION;
+GRANT SELECT ON u1.TBL TO u4 WITH GRANT OPTION;
+GRANT SELECT ON u1.TBL TO u5 WITH GRANT OPTION;
+
+
+evaluate 'connect to u2';
+call login('u2','') on class db_user;
+evaluate 'ERROR: Cannot issue GRANT/REVOKE to owner of a class';
+GRANT SELECT ON u1.TBL TO u1 WITH GRANT OPTION;
+
+GRANT SELECT ON u1.TBL TO u3 WITH GRANT OPTION;
+GRANT SELECT ON u1.TBL TO u4 WITH GRANT OPTION;
+GRANT SELECT ON u1.TBL TO u5 WITH GRANT OPTION;
+
+
+evaluate 'connect to u3';
+call login('u3','') on class db_user;
+evaluate 'ERROR: Cannot issue GRANT/REVOKE to owner of a class';
+GRANT SELECT ON u1.TBL TO u1 WITH GRANT OPTION;
+
+GRANT SELECT ON u1.TBL TO u2 WITH GRANT OPTION;
+GRANT SELECT ON u1.TBL TO u4 WITH GRANT OPTION;
+GRANT SELECT ON u1.TBL TO u5 WITH GRANT OPTION;
+
+
+evaluate 'connect to u4';
+call login('u4','') on class db_user;
+evaluate 'ERROR: Cannot issue GRANT/REVOKE to owner of a class';
+GRANT SELECT ON u1.TBL TO u1 WITH GRANT OPTION;
+
+GRANT SELECT ON u1.TBL TO u2 WITH GRANT OPTION;
+GRANT SELECT ON u1.TBL TO u3 WITH GRANT OPTION;
+GRANT SELECT ON u1.TBL TO u5 WITH GRANT OPTION;
+
+
+evaluate 'connect to u5';
+call login('u5','') on class db_user;
+evaluate 'ERROR: Cannot issue GRANT/REVOKE to owner of a class';
+GRANT SELECT ON u1.TBL TO u1 WITH GRANT OPTION;
+
+GRANT SELECT ON u1.TBL TO u2 WITH GRANT OPTION;
+GRANT SELECT ON u1.TBL TO u3 WITH GRANT OPTION;
+GRANT SELECT ON u1.TBL TO u4 WITH GRANT OPTION;
+
+select * from db_auth where grantee_name != 'PUBLIC';
+
+
+
+evaluate 'connect to u1 and revoke u2, u3, u4';
+call login('u1','') on class db_user;
+REVOKE SELECT ON u1.TBL FROM u2;
+REVOKE SELECT ON u1.TBL FROM u3;
+REVOKE SELECT ON u1.TBL FROM u4;
+
+select * from db_auth where grantee_name != 'PUBLIC';
+
+evaluate 'connect to u1 and revoke u5';
+REVOKE SELECT ON u1.TBL FROM u5;
+
+select * from db_auth where grantee_name != 'PUBLIC';
+
+
+evaluate 'test done';
+call login('dba','') on class db_user;
+drop table u1.tbl;
+drop user u5;
+drop user u4;
+drop user u3;
+drop user u2;
+drop user u1;
