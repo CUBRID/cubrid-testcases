777e7f07ffed1260a9790f6b1a150d7f5deeea12
tw-kang <twkang@cubrid.com>
2024-11-25 14:10:37 +0900

    add comment

--- sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/21_identifier_length_over_222.sql
+++ sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/21_identifier_length_over_222.sql
@@ -1,3 +1,9 @@
+--+ server-message on
+-- Verified for CBRD-25367
+-- Stored procedure compile error: identifier length may not exceed 222
+-- error code: -1360
+-- [err] Stored procedure compile error: identifier length may not exceed 222
+
 create or replace procedure poo()
 as
     p_s string := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
@@ -6,3 +12,5 @@ begin
 end;
 
 call poo();
+
+--+ server-message off
