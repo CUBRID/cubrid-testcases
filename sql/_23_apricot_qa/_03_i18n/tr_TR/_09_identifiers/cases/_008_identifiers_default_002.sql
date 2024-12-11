2c73536ba72e8c35e7fc298aa1f49f9d2509bdd7
Byungwook-Kim <bwkim@cubrid.com>
2024-12-11 17:36:43 +0900

    [CBRD-25713] revert to MAX CHAR length

--- sql/_23_apricot_qa/_03_i18n/tr_TR/_09_identifiers/cases/_008_identifiers_default_002.sql
+++ sql/_23_apricot_qa/_03_i18n/tr_TR/_09_identifiers/cases/_008_identifiers_default_002.sql
@@ -1,9 +1,9 @@
 --test
-CREATE TABLE t(col1 VARCHAR(1073741823) DEFAULT 'Çç_Ğğ_İı_Öö_Şş_Üü', col2 CHAR(2049));
+CREATE TABLE t(col1 VARCHAR(1073741823) DEFAULT 'Çç_Ğğ_İı_Öö_Şş_Üü', col2 CHAR(268435456));
 --test
-CREATE TABLE t(col1 VARCHAR(1073741824) DEFAULT 'Çç_Ğğ_İı_Öö_Şş_Üü', col2 CHAR(2048));
+CREATE TABLE t(col1 VARCHAR(1073741824) DEFAULT 'Çç_Ğğ_İı_Öö_Şş_Üü', col2 CHAR(268435455));
 --test
-CREATE TABLE t(col1 VARCHAR(1073741824) DEFAULT 'Çç_Ğğ_İı_Öö_Şş_Üü', col2 CHAR(2049));
+CREATE TABLE t(col1 VARCHAR(1073741824) DEFAULT 'Çç_Ğğ_İı_Öö_Şş_Üü', col2 CHAR(268435456));
 --test
-CREATE TABLE t(col1 VARCHAR(1073741823) DEFAULT 'Çç_Ğğ_İı_Öö_Şş_Üü', col2 CHAR(2048));
+CREATE TABLE t(col1 VARCHAR(1073741823) DEFAULT 'Çç_Ğğ_İı_Öö_Şş_Üü', col2 CHAR(268435455));
 drop table t;
