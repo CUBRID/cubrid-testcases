e4520d8dd0789867da00ac4efc15ba8b8ed6846a
swi0110 <joykevin@cubrid.com>
2024-12-16 16:11:56 +0900

    supplement test case

--- sql/_05_plcsql/_01_testspec/_04_expression/_23_alter/cases/01_alter_compile.sql
+++ sql/_05_plcsql/_01_testspec/_04_expression/_23_alter/cases/01_alter_compile.sql
@@ -32,7 +32,54 @@ ALTER procedure test_tbl compile;
 
 call test_tbl();
 
+
+evaluate 'drop table & ERROR: Syntax: Unknown class dba.tbl';
 drop table tbl;
+
+call test_tbl();
+
 drop PROCEDURE test_tbl;
 
+
+evaluate 'create t1 & check for %type';
+create table t1(col int);
+
+create or replace procedure test_proc(a t1.col%type)
+as
+begin
+    DBMS_OUTPUT.put_line('args value: ' || a);
+end;
+
+call test_proc(1);
+
+evaluate 'changed column type on t1.col (int -> char(1))';
+alter table t1 modify column col char(1);
+
+evaluate 'before re-compile procedure';
+call test_proc('a');
+call test_proc(1);
+
+
+evaluate 'after re-compile procedure';
+ALTER procedure test_proc compile;
+call test_proc('a');
+call test_proc(1);
+
+evaluate 'ddd';
+alter table t1 modify column col int;
+ALTER procedure test_proc compile;
+call test_proc('a');
+call test_proc(1);
+
+evaluate 'drop t1';
+drop t1;
+call test_proc('a');
+call test_proc(1);
+
+evaluate 'after re-compile procedure & Error: Failed to get attribute information';
+ALTER procedure test_proc compile;
+call test_proc('a');
+call test_proc(1);
+
+drop procedure test_proc;
 --+ server-message off
