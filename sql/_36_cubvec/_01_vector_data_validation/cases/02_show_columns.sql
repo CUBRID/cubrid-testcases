2270b9f73e7e29657cac27377a20172aca900211
yjlee <yjlee@cubrid.com>
2025-02-12 15:57:14 +0900

    Delete todo files and add comments

--- sql/_36_cubvec/_01_vector_data_validation/cases/02_show_columns.sql
+++ sql/_36_cubvec/_01_vector_data_validation/cases/02_show_columns.sql
@@ -1,7 +1,6 @@
 drop table if exists vt;
 create table vt (vec VECTOR(3));
 
-;sc vt
 --show columns from vt;
 --desc vt;
 
--- sql/_36_cubvec/_01_vector_data_validation/cases/03_insert_select.sql
+++ sql/_36_cubvec/_01_vector_data_validation/cases/03_insert_select.sql
@@ -1,19 +1,23 @@
 drop table if exists vt;
 create table vt (vec VECTOR(3));
 
+-- Valid insertion of vector (Including concatenation)
 insert into vt values ('[1, 2, 3]');
 insert into vt values ('[' || '4, 5, 6' || ']');
 insert into vt values (concat ('[', '7, 8, 9', ']'));
 
+-- Syntax / Semantic errors from insert
 insert into vt values ([1,2,3]);
 insert into vt values '[1,2,3]';
 insert into vt values ('[1,2,3]';
 insert into vt values '[1,2,3]');
 
+-- Invalid vector elements
 insert into vt values ('[-2e+20, -3e+30, -4e+40]');
 insert into vt values ('[2e+20, 3e+30, 4e+40]');
 insert into vt values ('[1, 2, nan]');
-insert into vt values ('[1, 2, 'abc']');
+
+-- Invalid size of vector
 --insert into vt values ('[1, 2]');
 --insert into vt values ('[1, 2, 3, 4]');
 
