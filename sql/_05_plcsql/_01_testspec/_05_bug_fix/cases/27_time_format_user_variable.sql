27346a1a58ac8cd31c58f194f3ce450d4dc602e7
junsklee <50674368+junsklee@users.noreply.github.com>
2025-03-06 12:21:59 +0900

    added timezone revert

--- sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/27_time_format_user_variable.sql
+++ sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/27_time_format_user_variable.sql
@@ -4,6 +4,7 @@
 -- returns consistent results for various datetime values (datetime, datetimeltz, datetimetz)
 
 set time zone 'America/Sao_Paulo';
+
 prepare st from 'select time_format(?, ?)';
 set @v1=datetime'2025-12-31 22:30:45.999';
 set @v2=datetimeltz'2025-12-31 22:30:45.999';
@@ -29,5 +30,7 @@ select time_format(datetimetz'2025-12-31 22:30:45.999 America/Sao_Paulo', '%TZR 
 execute st using datetimetz'2025-12-31 22:30:45.999 America/Sao_Paulo', '%TZR %TZD %TZH %TZM'; 
 select time_format(@v4, '%TZR %TZD %TZH %TZM'); 
 execute st using @v4, '%TZR %TZD %TZH %TZM';
+
+set timezone 'Asia/Seoul';
 
 --+ server-message off
