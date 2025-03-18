acc5efd853eb862f76e8ecc9512224dc472e4e60
zionyun <zionyun>
2025-03-18 16:28:05 +0900

    add new testcase for cbrd-25920

---
+++ sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/28_repeated_cursor_open_close_error.sql
@@ -0,0 +1,32 @@
+--+ server-message on
+-- verified the CBRD-25920
+-- Previously, when repeatedly opening and closing a PL/CSQL cursor multiple times,  
+-- runtime error occurred with the message:  
+-- ERROR: Stored procedure execute error:  
+-- (line 6, column 9) Cannot allocate query entry any more. Maximum allocatable entries are 100.  
+
+-- This issue has now been fixed, and the error no longer occurs.  
+-- The expected behavior is that the execution completes successfully without any errors.  
+
+drop table if exists athlete_t;
+create table athlete_t (name varchar);
+insert into athlete_t values ('a'), ('b'), ('c');
+
+create or replace procedure poo as
+    cursor c is select name from athlete_t limit 1;
+    nm varchar(40);
+begin
+    for i in 1..2001 loop
+        open c;
+        fetch c into nm;
+        dbms_output.put_line(i);
+        close c;
+    end loop;
+end;
+
+call poo();
+
+drop table if exists athlete_t;
+drop procedure poo;
+
+--+ server-message off
