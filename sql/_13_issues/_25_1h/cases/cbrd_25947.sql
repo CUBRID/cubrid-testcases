a3786dcf5510d596eefe1118894ebbf88eef2449
zionyun <41980125+zionyun@users.noreply.github.com>
2025-03-25 21:03:17 +0900

    [CBRD-25947] Added test case for unnecessary rownum rewrite during view merge (#2152)
    refer to http://jira.cubrid.org/browse/CBRD-25947

---
+++ sql/_13_issues/_25_1h/cases/cbrd_25947.sql
@@ -0,0 +1,37 @@
+-- Verified for CBRD-25947
+-- Issue: The issue where rownum and orderby_num were unnecessarily rewritten during the transformation of a subquery's ORDER BY clause in view merging has been resolved.
+
+drop table if exists t1, t2;
+
+create table t1(c1 int, c2 int);
+insert into t1 values(1,1),(2,2),(3,3),(4,4);
+
+create table t2(c1 int);
+insert into t2 values(1),(2),(3),(4);
+
+evaluate '1. Main query: rownum replaced by orderby_num() when ORDER BY is added';
+select /*+ recompile */ rownum from (select c2 from t1 order by c1 desc);
+
+evaluate '2. Main query: rownum remains unchanged when no ORDER BY is added in the main query';
+select /*+ recompile */ * from t2
+    where c1 in (select rownum from (select c2 from t1 order by c1 desc));
+
+evaluate '3. Subquery: orderby_num() replaced by rownum when ORDER BY is removed from subquery';
+select /*+ recompile */ * from t2
+    where c1 in (select orderby_num from (select orderby_num() as orderby_num from t1 order by c1 desc));
+
+evaluate '4. Subquery: orderby_num() remains unchanged when ORDER BY is retained in subquery';
+select /*+ recompile */ orderby_num from (select orderby_num() as orderby_num from t1 order by c1 desc);
+
+evaluate '5. Subquery with LIMIT: view merge disabled and ordering preserved';
+select /*+ recompile */ * from t2
+    where c1 in (select c2 from (select c2 from t1 order by c1 desc limit 2));
+
+evaluate '6. Main query with no_merge';
+select rownum from (select /*+ no_merge */ * from t2 order by 1);
+
+evaluate '7. in-list, subquery contains no_merge';
+select /*+ recompile */ * from t1
+    where c1 in (select rownum from (select /*+ no_merge */ * from t2 order by 1));
+
+drop table if exists t1, t2;
