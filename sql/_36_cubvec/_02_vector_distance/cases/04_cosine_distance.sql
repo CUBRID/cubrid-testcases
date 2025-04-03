d17196abc1ae1d750715af27927843042220c17d
Daehyun Kim <kimdhyungg@gmail.com>
2025-04-03 17:13:49 +0900

    style: rename filename to have orderings

--- sql/_36_cubvec/_02_vector_distance/cases/04_cosine_distance.sql
+++ sql/_36_cubvec/_02_vector_distance/cases/04_cosine_distance.sql
@@ -5,21 +5,16 @@
 -- Syntax/ Valid
 SELECT COSINE_DISTANCE('[1,2,3]', '[3,2,1]')
 
+-- Syntax/ Number of arguments/ Error
+SELECT COSINE_DISTANCE();
+SELECT COSINE_DISTANCE('[1,2,3]');
+SELECT COSINE_DISTANCE('[1,2,3]', '[3,2,1]', '[1,2,3]');
+
 -------------------------------------------------------------------------------
 -- Semantics
 -------------------------------------------------------------------------------
 
--- Semantics/ Number of arguments/ Error
-SELECT COSINE_DISTANCE();
-SELECT COSINE_DISTANCE('[1,2,3]');
-
--- Semantics/ Number of arguments/ Valid
-SELECT COSINE_DISTANCE('[1,2,3]', '[3,2,1]');
-
--- Semantics/ Number of arguments/ Error
-SELECT COSINE_DISTANCE('[1,2,3]', '[3,2,1]', '[1,2,3]');
-
--- -- Spec/ NULL/ Valid
+-- -- Semantics/ NULL/ Valid
 -- SELECT COSINE_DISTANCE(NULL, NULL);
 -- SELECT COSINE_DISTANCE(NULL, '[3,2,1]');
 -- SELECT COSINE_DISTANCE('[1,2,3]', NULL);
@@ -30,9 +25,9 @@ SELECT COSINE_DISTANCE('[1,2,3]', 1);
 
 -- Semantics/ Wrong Argument Types Even Though in Vector Format?
 -- Is this possible in CSQL?
-SELECT COSINE_DISTANCE('[1,2,3]' AS VARCHAR, '[3,2,1]');
-SELECT COSINE_DISTANCE('[1,2,3]', '[3,2,1]' AS VARCHAR);
-SELECT COSINE_DISTANCE('[1,2,3]' AS VARCHAR, '[3,2,1]' AS VARCHAR);
+SELECT COSINE_DISTANCE(CAST('[1,2,3]' AS VARCHAR), '[3,2,1]');
+SELECT COSINE_DISTANCE('[1,2,3]', CAST('[3,2,1]' AS VARCHAR));
+SELECT COSINE_DISTANCE(CAST('[1,2,3]' AS VARCHAR), CAST('[3,2,1]' AS VARCHAR));
 
 -- Semantics/ Wrong Argument Formats/ Error
 SELECT COSINE_DISTANCE('hello world', '[3,2,1]');
