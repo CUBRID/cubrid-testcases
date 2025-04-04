b66ab898a574181446171cd59942be5bb80e24f3
Daehyun Kim <kimdhyungg@gmail.com>
2025-04-04 17:27:00 +0900

    fix: hnsw_parameters comment & add more cases

--- sql/_36_cubvec/_03_create_vector_index/cases/_hnsw_parameters.sql
+++ sql/_36_cubvec/_03_create_vector_index/cases/_hnsw_parameters.sql
@@ -13,19 +13,35 @@ INSERT INTO test_vector_index_hnsw VALUES (4, '[0.3,0.2,0.1]');
 --------------------------------------------------------------------------------
 -- Syntax
 --------------------------------------------------------------------------------
--- Syntax/ creation with wrong parameters/ Invalid
+-- Syntax/ creation with parameters/ Valid
+drop vector index idx_hnsw;
+CREATE VECTOR INDEX idx_hnsw ON test_vector_index_hnsw(embedding COSINE)
+  WITH (m = 40, ef_construction = 500);
+
+-- Syntax/ creation with parameters in reverse order/ Valid
+drop vector index idx_hnsw;
+CREATE VECTOR INDEX idx_hnsw ON test_vector_index_hnsw(embedding COSINE)
+  WITH (ef_construction = 500, m = 40);
+
+-- Syntax/ m and ef_construction must be integer/ Error
+drop vector index idx_hnsw;
+CREATE VECTOR INDEX idx_hnsw ON test_vector_index_hnsw(embedding COSINE)
+  WITH (ef_construction = 'hello', m = 'world');
+
+-- Syntax/ creation with wrong parameters/ Error
+drop vector index idx_hnsw;
 CREATE VECTOR INDEX idx_hnsw ON test_vector_index_hnsw(embedding COSINE)
   WITH (a = 5, b = 6);
 
---------------------------------------------------------------------------------
--- Semantics
---------------------------------------------------------------------------------
--- Syntax/ creation with parameters/ Valid
+-- Syntax/ additional parameters/ Error
+drop vector index idx_hnsw;
 CREATE VECTOR INDEX idx_hnsw ON test_vector_index_hnsw(embedding COSINE)
-  WITH (m = 40, ef_construction = 500);
--- Syntax/ creation with parameters in reverse order/ Valid
+  WITH (m = 40, ef_construction = 500, a = 5, b = 6);
+
+-- Syntax/ duplicate parameters/ Error
+drop vector index idx_hnsw;
 CREATE VECTOR INDEX idx_hnsw ON test_vector_index_hnsw(embedding COSINE)
-  WITH (ef_construction = 500, m = 40);
+  WITH (m = 40, ef_construction = 500, m = 40);
 
 --------------------------------------------------------------------------------
 -- Execution
