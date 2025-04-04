8db9af77546b8186abc33c27ede320a65ed85f7b
Daehyun Kim <kimdhyungg@gmail.com>
2025-04-04 21:09:08 +0900

    fix(01): check results with CTP

--- sql/_36_cubvec/_03_create_vector_index/cases/01_create_vector_index_syntax.sql
+++ sql/_36_cubvec/_03_create_vector_index/cases/01_create_vector_index_syntax.sql
@@ -2,7 +2,7 @@
 DROP IF EXISTS test_vector_index;
 CREATE TABLE test_vector_index (
     id INT,
-    vector_data VECTOR
+    vector_data VECTOR,
     varchar_data VARCHAR
 );
 INSERT INTO test_vector_index VALUES (1, '[1,2,3]', '[1,2,3]');
@@ -11,24 +11,41 @@ INSERT INTO test_vector_index VALUES (2, '[3,2,1]', '[3,2,1]');
 --------------------------------------------------------------------------------
 -- Syntax
 --------------------------------------------------------------------------------
--- Syntax/ Valid
+-- Syntax/ Create Index (pg compat)
+-- Syntax/ Create Index/ With Metric/ Valid
+-- TODO: Should be able to make vector index with create index statement too.
+-- CREATE INDEX idx_vector1 ON test_vector_index(vector_data COSINE);
+-- drop index idx_vector1;
+-- Syntax/ Create Index/ Without Metric/ Valid
+-- TODO: Should be able to make vector index with create index statement too.
+-- CREATE INDEX idx_vector1 ON test_vector_index(vector_data COSINE);
+-- DROP VECTOR INDEX idx_vector1 on tbl(vector_data);
+
+-- Syntax/ Create Vector Index
+-- Syntax/ Create Vector Index/ With Metric/Valid
 CREATE VECTOR INDEX idx_vector1 ON test_vector_index(vector_data COSINE);
+-- DROP VECTOR INDEX idx_vector1 on test_vector_index(vector_data);
 
--- Syntax/ Missing parens/ Error
-CREATE VECTOR INDEX idx_vector1 ON test_vector_index;
+-- Syntax/ Create Vector Index/ Without Metric/ Valid
+-- TODO: Should be able to create without metric.
+-- CREATE VECTOR INDEX idx_vector2 ON test_vector_index(vector_data);
+-- DROP VECTOR INDEX idx_vector2 on tbl(vector_data);
 
--- Syntax/ Missing column name/ Error
-CREATE VECTOR INDEX idx_vector1 ON test_vector_index();
+-- Syntax/ Create Vector Index/ Missing parens/ Error
+CREATE VECTOR INDEX idx_vector3 ON test_vector_index;
 
--- Syntax/ Missing column name/ Error
-CREATE VECTOR INDEX idx_vector1 ON test_vector_index(COSINE);
---
--- Syntax/ Missing metric name/ Error
-CREATE VECTOR INDEX idx_vector1 ON test_vector_index(vector_data);
+-- Syntax/ Create Vector Index/ Missing column name/ Error
+CREATE VECTOR INDEX idx_vector4 ON test_vector_index();
+
+-- Syntax/ Create Vector Index/ Missing column name/ Error
+CREATE VECTOR INDEX idx_vector5 ON test_vector_index(COSINE);
 
 --------------------------------------------------------------------------------
 -- Semantics
 --------------------------------------------------------------------------------
--- Semantics/ Create vector index on non-vector type/ Error
-CREATE VECTOR INDEX idx_vector1 ON test_vector_index(id COSINE);
-CREATE VECTOR INDEX idx_vector1 ON test_vector_index(varchar_data COSINE);
+-- Semantics/ Create Vector Index/Non-vector type/ Error
+-- TODO: Currently these statements pass
+-- CREATE VECTOR INDEX idx_vector6 ON test_vector_index(id COSINE);
+-- DROP VECTOR INDEX idx_vector6 on test_vector_index(id);
+-- CREATE VECTOR INDEX idx_vector7 ON test_vector_index(varchar_data COSINE);
+-- DROP VECTOR INDEX idx_vector7 on test_vector_index(varchar_data);
