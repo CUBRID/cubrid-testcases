76287006e073b692271e527c8efcf216b0d20171
Daehyun Kim <kimdhyungg@gmail.com>
2025-04-08 16:03:52 +0900

    fix(02): fix unintended syntax error on testing index recreation

--- sql/_36_cubvec/_03_create_vector_index/cases/02_recreate_vector_index.sql
+++ sql/_36_cubvec/_03_create_vector_index/cases/02_recreate_vector_index.sql
@@ -1,16 +1,19 @@
---------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 -- Execution
---------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+
 DROP IF EXISTS test_index_lifecycle;
 CREATE TABLE test_index_lifecycle (
     id INT,
-    vector_data VECTOR
+    vector_data VECTOR(3)
 );
-INSERT INTO test_index_lifecycle VALUES (1, '[7,8,9]');
-INSERT INTO test_index_lifecycle VALUES (2, '[9,8,7]');
-CREATE VECTOR INDEX idx_lifecycle ON test_index_lifecycle(vector_data);
+-- TODO: creating vector index on non-empty table not yet implemented.
+-- INSERT INTO test_index_lifecycle VALUES (1, '[7,8,9]');
+-- INSERT INTO test_index_lifecycle VALUES (2, '[9,8,7]');
+CREATE VECTOR INDEX idx_lifecycle ON test_index_lifecycle(vector_data EUCLIDEAN);
 DROP INDEX idx_lifecycle ON test_index_lifecycle; -- Drop index.
 
 -- Execution/ Re-create index after DROP/ Valid
-CREATE VECTOR INDEX idx_lifecycle ON test_index_lifecycle(vector_data);
+CREATE VECTOR INDEX idx_lifecycle ON test_index_lifecycle(vector_data EUCLIDEAN);
+DROP INDEX idx_lifecycle ON test_index_lifecycle; -- Drop index.
 
