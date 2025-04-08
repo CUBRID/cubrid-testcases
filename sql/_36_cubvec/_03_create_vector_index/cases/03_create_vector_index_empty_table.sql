6508305fa51f5ea380b1f1fc2b22122055e1f7de
Daehyun Kim <kimdhyungg@gmail.com>
2025-04-08 16:06:40 +0900

    fix(03): fix unintended syntax error and drop index after use.

--- sql/_36_cubvec/_03_create_vector_index/cases/03_create_vector_index_empty_table.sql
+++ sql/_36_cubvec/_03_create_vector_index/cases/03_create_vector_index_empty_table.sql
@@ -8,5 +8,6 @@ CREATE TABLE test_empty_table (
     id INT,
     vector_data VECTOR(3)
 );
-CREATE VECTOR INDEX idx_empty ON test_empty_table(vector_data);
+CREATE VECTOR INDEX idx_empty ON test_empty_table(vector_data COSINE);
+DROP INDEX idx_empty ON test_empty_table(vector_data);
 
