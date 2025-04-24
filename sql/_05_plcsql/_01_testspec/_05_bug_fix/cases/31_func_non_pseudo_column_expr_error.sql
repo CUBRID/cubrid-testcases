cedae16120501c51648ade74ed362c1face05334
kiho-um <kiho@cubrid.com>
2025-04-24 16:58:08 +0900

    [CBRD-26021] Add new test case for add fix NULL default when using non-pseudo column expres… (#2183)
    - user function 내에 저장 함수의 매개변수 기본값에 pseudo column이 아닌 일반 expression을 사용할 경우 NULL로 처리 및
    저장 함수의 기본값이 없을 시 에러 발생하도록.
    - non-pseudo column expression에 대해서 evaluate 한 후, 결과 DB_VALUE를 PT_VALUE 타입 출력.
    - SymbolStack에 추가되지 않은 LOG 함수.

---
+++ sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/31_func_non_pseudo_column_expr_error.sql
@@ -0,0 +1,32 @@
+--+ server-message on
+-- verified the CBRD-26021
+-- Fix NULL default when using non-pseudo column expressions in a stored function' parameter
+
+-- Error : -493 non-default
+CREATE OR REPLACE FUNCTION t (a int RAND()) RETURN int as begin return a; end;
+
+-- compile success : rand function
+CREATE OR REPLACE FUNCTION t (a int default RAND()) RETURN int as begin return a; end;
+
+-- CASE WHEN default_value is not null THEN OK ELSE NOK END  
+SELECT CASE WHEN default_value is not null THEN 'OK' ELSE 'NOK' END as default_value FROM db_stored_procedure_args WHERE sp_name = 't';
+
+-- Error : -493 non-default
+CREATE OR REPLACE function t1 (a int log(4,2)) return int as begin return a; end;
+
+-- compile success : add log function  
+CREATE OR REPLACE function t1 (a int default log(4,2)) return int as begin return a; end;
+
+-- default_value
+SELECT default_value FROM db_stored_procedure_args WHERE sp_name = 't1';
+
+-- Error : -493 invalid create statement
+CREATE OK REPLACE FUNCTION t2 (a int logk(4,2)) return int as begin return a; end;
+
+-- Error : -493 invalid create statement
+CREATE OK REPLACE FUNCTION t2 (a int default logk(4,2)) return int as begin return a; end;
+
+DROP FUNCTION t;
+DROP FUNCTION t1;
+
+--+ server-message off
