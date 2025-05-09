b1db525f4aef1fa05584fe788ac9c6fa17f9a3ef
zionyun <41980125+zionyun@users.noreply.github.com>
2025-05-09 18:14:09 +0900

    [CUBRIDQA-1136] Separated successful cases from PL/CSQL builtin function test cases (#2189)

--- sql/_05_plcsql/_01_testspec/_04_expression/_04_function_call/_03_builtin_function/_01_string_function/cases/04_04_03_01_bfn_string_substring.sql
+++ sql/_05_plcsql/_01_testspec/_04_expression/_04_function_call/_03_builtin_function/_01_string_function/cases/04_04_03_01_bfn_string_substring.sql
@@ -4,17 +4,25 @@
 
 create or replace procedure t () as
 begin
-    dbms_output.put_line(SUBSTRING(NULL, 0, 0));
-    dbms_output.put_line(SUBSTRING('12345abcdeabcde',6, 4));
-    dbms_output.put_line(SUBSTRING('12345abcdeabcde',6, 0));
-    dbms_output.put_line(SUBSTRING('12345abcdeabcde',-6, 4));
-    dbms_output.put_line(SUBSTRING('12345abcdeabcde',6, -4));
-
     -- parse error SUBSTRING() in FROM/FOR
     dbms_output.put_line(SUBSTRING('12345abcdeabcde' FROM 6 FOR 4));
     dbms_output.put_line(SUBSTRING('12345abcdeabcde' FROM 6 FOR 0));
     dbms_output.put_line(SUBSTRING('12345abcdeabcde' FROM -6 FOR 4));
     dbms_output.put_line(SUBSTRING('12345abcdeabcde' FROM 6 FOR -4));
+end;
+
+select count(*) from db_stored_procedure where sp_name = 't';
+select count(*) from db_stored_procedure_args where sp_name = 't';
+
+call t();
+
+create or replace procedure t () as
+begin
+    dbms_output.put_line(SUBSTRING(NULL, 0, 0));
+    dbms_output.put_line(SUBSTRING('12345abcdeabcde',6, 4));
+    dbms_output.put_line(SUBSTRING('12345abcdeabcde',6, 0));
+    dbms_output.put_line(SUBSTRING('12345abcdeabcde',-6, 4));
+    dbms_output.put_line(SUBSTRING('12345abcdeabcde',6, -4));
 end;
 
 select count(*) from db_stored_procedure where sp_name = 't';
