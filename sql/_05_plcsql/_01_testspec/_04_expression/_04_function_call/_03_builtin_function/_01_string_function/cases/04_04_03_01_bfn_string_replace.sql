fced98f61ff912ce1f132f180016bced15ea4e42
junsklee <50674368+junsklee@users.noreply.github.com>
2025-05-27 21:38:20 -0300

    [CUBRIDQA-1136] removed charset-related test from PL/CSQL sql test (#2204)
    A query using unsupported charsets has been removed from the TC. Chinese characters are not supported in charset in which SQL tests are run (iso88591), and that strictly applies to PL server as well (after (CUBRID/cubrid#5668).

--- sql/_05_plcsql/_01_testspec/_04_expression/_04_function_call/_03_builtin_function/_01_string_function/cases/04_04_03_01_bfn_string_replace.sql
+++ sql/_05_plcsql/_01_testspec/_04_expression/_04_function_call/_03_builtin_function/_01_string_function/cases/04_04_03_01_bfn_string_replace.sql
@@ -21,7 +21,6 @@ begin
     dbms_output.put_line(REPLACE('CUBRID DATABASE', 'D', 'd'));
     dbms_output.put_line(REPLACE('CUBRID DATABASE', 3.0));
     dbms_output.put_line(REPLACE('CUBRID DATABASE', 32000000));
-    dbms_output.put_line(REPLACE('你好我好大家好','好','开心'));
 end;
 
 select count(*) from db_stored_procedure where sp_name = 't';
