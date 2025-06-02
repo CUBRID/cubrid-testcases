ab15897b88d4bccfdd4ed5d3b081896fc62b1606
yjlee <yjlee@cubrid.com>
2025-06-02 16:41:55 +0900

    Add testcases for select vector distances

---
+++ sql/_36_cubvec/_04_select_vector_distance/cases/01_select_vector_distance.sql
@@ -0,0 +1,19 @@
+DROP TABLE items;
+CREATE TABLE items (id int, vc1 vector(3)) DONT_REUSE_OID;
+CREATE VECTOR INDEX idx_v ON items(vc1 EUCLIDEAN) WITH (m = 16, efConstruction = 64);
+
+INSERT INTO items VALUES (1, '[1,2,3]');
+INSERT INTO items VALUES (2, '[1,0,2]');
+INSERT INTO items VALUES (3, '[3,4,5]');
+INSERT INTO items VALUES (4, '[0,0,1]');
+INSERT INTO items VALUES (5, '[5,6,7]');
+INSERT INTO items VALUES (6, '[9,8,7]');
+INSERT INTO items VALUES (7, '[1,1,0]');
+INSERT INTO items VALUES (8, '[0,1,0]');
+INSERT INTO items VALUES (9, '[1,1,1]');
+INSERT INTO items VALUES (10, '[1,0,1]');
+INSERT INTO items VALUES (11, '[2,3,4]');
+INSERT INTO items VALUES (12, '[1,1,1]');
+
+SELECT id FROM items ORDER by l2_distance(vc1, '[0,0,0]') LIMIT 3;
+SELECT id FROM items WHERE id < 11 ORDER by l2_distance(vc1, '[1,2,3]') LIMIT 5;
