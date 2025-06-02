234efc33a0620654785554db7416e0f1c983897e
junsklee <50674368+junsklee@users.noreply.github.com>
2025-06-02 03:34:21 -0300

    [CUBRIDQA-1137] Extension: 10.2 backport for testcase modification for missing deallocate or drop on prepared statement (#2211)
    Extension of #2205 . (For the full list, see [11.3 bp]: #2056)
    
    *sql/_27_banana_qa/issue_16066_BINARY_charset/_02_coercibility/cases/_11_session_var_05.sql: modified TC logic to avoid stacking up prepared statements

--- sql/_27_banana_qa/issue_16066_BINARY_charset/_02_coercibility/cases/_11_session_var_05.sql
+++ sql/_27_banana_qa/issue_16066_BINARY_charset/_02_coercibility/cases/_11_session_var_05.sql
@@ -5,34 +5,39 @@
 set names binary;
 
 prepare st1 from 'select collation(upper(?)) collation, coercibility(upper(?)) coeribility';
-prepare st2 from 'select collation(concat(?, ?)) collation, coercibility(concat(?, ?)) coercibility';
-prepare st3 from 'select collation(reverse(?)) collation, coercibility(reverse(?)) coercibility';
-prepare st4 from 'select /*+ recompile */collation(replace(?, ?, ''b'')) collation, coercibility(replace(?, ?, ''b'')) coercibility';
-prepare st5 from 'select collation(lpad(?, 30, ?)) collation, coercibility(lpad(?, 30, ?)) coercibility';
-prepare st6 from 'select collation(repeat(?, 3)) collation, coercibility(repeat(?, 3)) coercibility';
-prepare st7 from 'select collation(elt(?, ?, ?)) collation, coercibility(elt(?, ?, ?)) coercibility';
-prepare st8 from 'select collation(trim(?)) collation, coercibility(trim(?)) coercibility';
-
-
-set names binary;
 execute st1 using 'a', 'a';
+deallocate prepare st1;
+
+prepare st2 from 'select collation(concat(?, ?)) collation, coercibility(concat(?, ?)) coercibility';
 execute st2 using 'a', 'b', 'a', 'b';
+deallocate prepare st2;
+
+prepare st3 from 'select collation(reverse(?)) collation, coercibility(reverse(?)) coercibility';
 execute st3 using 'b', 'b';
+deallocate prepare st3;
+
+prepare st4 from 'select /*+ recompile */collation(replace(?, ?, ''b'')) collation, coercibility(replace(?, ?, ''b'')) coercibility';
 execute st4 using 'abc', 'a', 'abc', 'a';
+deallocate prepare st4;
+
+prepare st5 from 'select collation(lpad(?, 30, ?)) collation, coercibility(lpad(?, 30, ?)) coercibility';
 execute st5 using '123', 12.3, '123', 12.3;
+deallocate prepare st5;
+
+prepare st6 from 'select collation(repeat(?, 3)) collation, coercibility(repeat(?, 3)) coercibility';
 execute st6 using 'a', 'a';
+deallocate prepare st6;
+
+prepare st7 from 'select collation(elt(?, ?, ?)) collation, coercibility(elt(?, ?, ?)) coercibility';
 execute st7 using 2, '1', 2, 2, '1', 2;
+deallocate prepare st7;
+
+prepare st8 from 'select collation(trim(?)) collation, coercibility(trim(?)) coercibility';
 execute st8 using '  a  ', '  a  ';
+deallocate prepare st8;
+
 select collation(upper('a')) collation, coercibility(upper('a')) coeribility;
 
-deallocate prepare st1;
-deallocate prepare st2;
-deallocate prepare st3;
-deallocate prepare st4;
-deallocate prepare st5;
-deallocate prepare st6;
-deallocate prepare st7;
-deallocate prepare st8;
 set names iso88591;
 commit;
 --+ holdcas off;
