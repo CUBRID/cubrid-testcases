a4e32072ebdc868544053cbd7735ff5aa69b1cdc
zionyun <seeon0001@naver.com>
2025-07-25 16:40:14 +0900

    modify tc and answer

--- sql/_23_apricot_qa/_03_i18n/es_ES/_01_string_date_func/cases/_001_strtodate.sql
+++ sql/_23_apricot_qa/_03_i18n/es_ES/_01_string_date_func/cases/_001_strtodate.sql
@@ -113,6 +113,6 @@ SELECT STR_TO_DATE('octubre 1, 2013 8:11:12 Am','%M %d,%Y %h:%i:%s %p') FROM db_
 
 SELECT STR_TO_DATE('octubre 1, 2013 8:11:12 Am','%M %d,%Y %r') FROM db_root;
 set system parameters 'intl_date_lang = en_US';
-set names iso88591;
+set names utf8;
 commit;
 --+ holdcas off;
