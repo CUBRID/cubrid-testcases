84d8f3233d15794351d17e9496336b30a9970b07
zionyun <seeon0001@naver.com>
2025-07-29 18:11:22 +0900

    modify tc

--- sql/_23_apricot_qa/_03_i18n/tr_TR/_12_partitioning/cases/007.sql
+++ sql/_23_apricot_qa/_03_i18n/tr_TR/_12_partitioning/cases/007.sql
@@ -11,13 +11,13 @@ create table range_test(id int,
 				test_datetime timestamp,
                 primary key(id, test_char))
 	PARTITION BY RANGE (test_char) (
-	    PARTITION p0 VALUES LESS THAN (_iso88591'şşş')
+	    PARTITION p0 VALUES LESS THAN (_utf8'şşş')
 	);
-	insert into range_test values(1,_iso88591'şşş','şşş',B'1',B'1011',N'şşş',N'şşş','şşşşşşşşşş','2007-01-01 09:00:00');
-	insert into range_test values(2,_iso88591'ĞĞĞ','ĞĞĞ',B'10',B'1100',N'ĞĞĞ',N'ĞĞĞ','ĞĞĞĞĞĞĞĞĞĞ','2007-01-01 09:00:00');
-	insert into range_test values(3,_iso88591'ÜÜÜ','ÜÜÜ',B'11',B'1101',N'ÜÜÜ',N'ÜÜÜ','ÜÜÜÜÜÜÜÜÜÜ','2007-01-01 09:00:00');
-	insert into range_test values(4,_iso88591'qqq','qqq',B'11',B'1101',N'qqq',N'qqq','qqqqqqqqqq','2007-01-01 09:00:00');
-	insert into range_test values(5,_iso88591'你你你','你你你',B'11',B'1101',N'你你你',N'你你你','你你你你你你你你你你','2007-01-01 09:00:00');
+	insert into range_test values(1,_utf8'şşş','şşş',B'1',B'1011',N'şşş',N'şşş','şşşşşşşşşş','2007-01-01 09:00:00');
+	insert into range_test values(2,_utf8'ĞĞĞ','ĞĞĞ',B'10',B'1100',N'ĞĞĞ',N'ĞĞĞ','ĞĞĞĞĞĞĞĞĞĞ','2007-01-01 09:00:00');
+	insert into range_test values(3,_utf8'ÜÜÜ','ÜÜÜ',B'11',B'1101',N'ÜÜÜ',N'ÜÜÜ','ÜÜÜÜÜÜÜÜÜÜ','2007-01-01 09:00:00');
+	insert into range_test values(4,_utf8'qqq','qqq',B'11',B'1101',N'qqq',N'qqq','qqqqqqqqqq','2007-01-01 09:00:00');
+	insert into range_test values(5,_utf8'你你你','你你你',B'11',B'1101',N'你你你',N'你你你','你你你你你你你你你你','2007-01-01 09:00:00');
 	insert into range_test values(9,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'2007-01-01 09:00:00');
 
 delete from range_test  where test_string   = 'şşş';
