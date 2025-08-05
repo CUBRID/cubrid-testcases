42595d7faccbb789590453ede08ecfe2f5ff3781
LyhengCHHAY <lyhengchhay.tech@gmail.com>
2025-08-05 21:30:28 +0900

    test case and answer for cbrd-25709

---
+++ sql/_13_issues/_25_2h/cases/cbrd_25709.sql
@@ -0,0 +1,16 @@
+-- Clean up
+DROP TRIGGER IF EXISTS trg_a;
+DROP TABLE IF EXISTS tbl_a;
+
+-- Create table with recursion control
+CREATE TABLE tbl_a(col_a INT, col_b INT, depth INT DEFAULT 1);
+
+-- Trigger with depth check
+CREATE TRIGGER trg_a
+DEFERRED INSERT ON tbl_a
+IF NEW.depth < 32
+EXECUTE INSERT INTO tbl_a(col_a, col_b, depth) VALUES (1, 2, NEW.depth + 1);
+
+-- Trigger the recursion
+INSERT INTO tbl_a(col_a, col_b) VALUES (1, 1);
+
