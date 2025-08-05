012b4def1d73a5ffefcc66e46018d0406a2afa0f
zionyun <seeon0001@naver.com>
2025-08-05 11:04:24 +0900

    modify tc _27_banana_qa -3

--- sql/_27_banana_qa/issue_5765_timezone_support/_04_existing_functions/_02_type_casting_functions/_01_date_format/cases/_04_fr_FR_dt_constant.sql
+++ sql/_27_banana_qa/issue_5765_timezone_support/_04_existing_functions/_02_type_casting_functions/_01_date_format/cases/_04_fr_FR_dt_constant.sql
@@ -45,6 +45,6 @@ deallocate prepare st;
 set timezone 'Asia/Seoul';
 set system parameters 'tz_leap_second_support=no';
 set system parameters 'intl_date_lang=en_US';
-set names iso88591;
+set names utf8;
 
 --+ holdcas off;
