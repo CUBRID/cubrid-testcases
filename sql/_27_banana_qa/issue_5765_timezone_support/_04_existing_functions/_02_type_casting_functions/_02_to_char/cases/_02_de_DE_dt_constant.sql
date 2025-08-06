642b5e4ffb219ab66cf80c502d02647e9b9f6699
zionyun <41980125+zionyun@users.noreply.github.com>
2025-08-06 10:10:13 +0900

    [CUBRIDQA-1287] basicdb: switch charset to UTF-8 and update SQL test cases - 20 (#2281)
    * modify tc _27_banana_qa -3
    
    * modify tc and answer _27_banana_qa-4

--- sql/_27_banana_qa/issue_5765_timezone_support/_04_existing_functions/_02_type_casting_functions/_02_to_char/cases/_02_de_DE_dt_constant.sql
+++ sql/_27_banana_qa/issue_5765_timezone_support/_04_existing_functions/_02_type_casting_functions/_02_to_char/cases/_02_de_DE_dt_constant.sql
@@ -124,7 +124,7 @@ deallocate prepare st;
 
 set timezone 'Asia/Seoul';
 set system parameters 'intl_date_lang=en_US';
-set names iso88591;
+set names utf8;
 set system parameters 'tz_leap_second_support=no';
 
 --+ holdcas off;
