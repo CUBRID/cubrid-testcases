6328fa45c2b169a8d90afcd77af4eb7e51951ba3
junsklee <50674368+junsklee@users.noreply.github.com>
2025-08-25 13:46:12 +0900

    [CBRD-24156] Revised sql answers for Clean up NCHAR and VARNCHAR related codes (#2259)
    http://jira.cubrid.org/browse/CBRD-24156
    
    This PR merges and commits the following (pre-verified):

--- sql/_01_object/_01_type/_001_nchar_nvarchar/cases/1005.sql
+++ sql/_01_object/_01_type/_001_nchar_nvarchar/cases/1005.sql
@@ -1,13 +1,13 @@
 -- create table with nchar data type of 1073741824 and  tests a create syntax with nchar data type and another constraint like unique,not null, shared and set default value
 
 CREATE CLASS ddl_0001(
-   col1       nchar(536870911) PRIMARY KEY,
-   col2       nchar(536870911) ,
-   col3       nchar(536870911) NOT NULL,
-   col4       nchar(536870911) NOT NULL,
-   col5       nchar(536870911) nOt nUlL,
-   col6       nchar(536870911) UNIQUE,
-   col7       nchar(536870911) UNIQUE NOT NULL,
+   col1       nchar(2048) PRIMARY KEY,
+   col2       nchar(2048) ,
+   col3       nchar(2048) NOT NULL,
+   col4       nchar(2048) NOT NULL,
+   col5       nchar(2048) nOt nUlL,
+   col6       nchar(2048) UNIQUE,
+   col7       nchar(2048) UNIQUE NOT NULL,
    col8       nchar SHARED NULL,
    col9       nchar SHARED N'a' NOT NULL
 );
