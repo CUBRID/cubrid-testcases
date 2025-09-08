917b1607a417eef99fd011cbd6eb6bd5756cdb6f
YeunjunLee <yjlee@cubrid.com>
2025-09-08 08:39:35 +0000

    Add ANN query on load vector index TC

--- sql/_36_cubvec/_03_create_vector_index/cases/07_load_vector_index.sql
+++ sql/_36_cubvec/_03_create_vector_index/cases/07_load_vector_index.sql
@@ -10,9 +10,19 @@ CREATE TABLE test_empty_table (
 );
 
 INSERT INTO test_empty_table VALUES (1, '[1,2,3]');
-INSERT INTO test_empty_table VALUES (2, '[4,5,6]');
-INSERT INTO test_empty_table VALUES (3, '[7,8,9]');
+INSERT INTO test_empty_table VALUES (2, '[1,0,2]');
+INSERT INTO test_empty_table VALUES (3, '[3,4,5]');
+INSERT INTO test_empty_table VALUES (4, '[0,0,1]');
+INSERT INTO test_empty_table VALUES (5, '[5,6,7]');
+INSERT INTO test_empty_table VALUES (6, '[9,8,7]');
+INSERT INTO test_empty_table VALUES (7, '[1,1,0]');
+INSERT INTO test_empty_table VALUES (8, '[0,1,0]');
+INSERT INTO test_empty_table VALUES (9, '[1,1,1]');
+INSERT INTO test_empty_table VALUES (10, '[1,0,1]');
+INSERT INTO test_empty_table VALUES (11, '[2,3,4]');
+INSERT INTO test_empty_table VALUES (12, '[1,1,1]');
 
-CREATE VECTOR INDEX idx_vector_index ON test_empty_table(vector_data COSINE);
+CREATE VECTOR INDEX idx_vector_index ON test_empty_table(vector_data EUCLIDEAN);
+SELECT id FROM test_empty_table ORDER by l2_distance(vector_data, '[0,0,0]') LIMIT 3;
 DROP INDEX idx_vector_index ON test_empty_table(vector_data);
 
