2963a953ffc7d4a128f3a11ca9ede4810d293aa3
YeunjunLee <yjlee@cubrid.com>
2025-09-08 08:21:01 +0000

    Modify index name

--- sql/_36_cubvec/_03_create_vector_index/cases/03_create_vector_index_empty_table.sql
+++ sql/_36_cubvec/_03_create_vector_index/cases/03_create_vector_index_empty_table.sql
@@ -12,20 +12,21 @@ CREATE VECTOR INDEX idx_empty ON test_empty_table(vector_data COSINE);
 DROP INDEX idx_empty ON test_empty_table(vector_data);
 
 -- Vector index shoud not be affected by other constraints such as primary key or not null.
+-- This TC is for verifying bugfix issue for dropping vector index which is occured by using BTID for vector index indication.
 
 DROP IF EXISTS test_primary_key_table;
 CREATE TABLE test_primary_key_table (
     id INT PRIMARY KEY,
     vector_data VECTOR(3)
 );
-CREATE VECTOR INDEX idx_primary_key ON test_primary_key_table(vector_data COSINE);
-DROP INDEX idx_primary_key ON test_primary_key_table(vector_data);
+CREATE VECTOR INDEX idx_v ON test_primary_key_table(vector_data COSINE);
+DROP INDEX idx_v ON test_primary_key_table(vector_data);
 
 DROP IF EXISTS test_not_null_table;
 CREATE TABLE test_not_null_table (
     id INT NOT NULL,
     vector_data VECTOR(3)
 );
-CREATE VECTOR INDEX idx_not_null ON test_not_null_table(vector_data COSINE);
-DROP INDEX idx_not_null ON test_not_null_table(vector_data);
+CREATE VECTOR INDEX idx_v ON test_not_null_table(vector_data COSINE);
+DROP INDEX idx_v ON test_not_null_table(vector_data);
 
