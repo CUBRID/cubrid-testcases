2453814c038dc5f2e82e132a99e1902a2f389814
kangmin <kangmin5505@gmail.com>
2025-09-10 17:19:04 +0900

    feature: replace * with explicit column names to avoid printing timestamp information for serial

--- sql/_01_object/_10_system_table/_017_db_serial/cases/1012.sql
+++ sql/_01_object/_10_system_table/_017_db_serial/cases/1012.sql
@@ -1,7 +1,7 @@
 --Create a serial with default attribute and retrieve information from db_serail
 create serial test_serial;
 
-select * from _db_serial
+select unique_name, name, owner, current_val, increment_val, max_val, min_val, start_val, cyclic, started, class_name, attr_name, cached_num, comment from _db_serial
 where name = 'test_serial';
 
 drop serial test_serial;
