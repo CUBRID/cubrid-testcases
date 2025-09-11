61dc20959e7d24b8d35efda8fd2e451668e6c4b5
oychanphallin <oychanphallin.tech@gmail.com>
2025-09-12 00:09:43 +0900

    Added evalute 4.

--- sql/_13_issues/_25_2h/cases/cbrd_26165.sql
+++ sql/_13_issues/_25_2h/cases/cbrd_26165.sql
@@ -3,10 +3,11 @@
  *  
  *  Coverage:
  *  Different Scenario
-1 - COMMIT executed inside a loop
-2 - COMMIT executed via EXECUTE IMMEDIATE inside a loop
-3 - COMMIT executed within a recursive call
- **/
+ *  1 - COMMIT executed inside a loop
+ *  2 - COMMIT executed via EXECUTE IMMEDIATE inside a loop
+ *  3 - COMMIT executed within a recursive call
+ *  4 - COMMIT executed in a nested procedure call
+ */
 
 evaluate '1 - COMMIT executed inside a loop';
 DROP TABLE IF EXISTS test_tbl;
@@ -85,7 +86,39 @@ SELECT foo(3);
 SELECT m, COUNT(*) FROM ttt GROUP BY m ORDER BY m;
 SELECT COUNT(*) FROM ttt;
 
+evaluate '4 - COMMIT executed in a nested procedure call';
+DROP TABLE IF EXISTS test_tbl;
+CREATE TABLE test_tbl(id INT);
+
+-- Inner procedure
+CREATE OR REPLACE PROCEDURE inner_proc(p INT)
+AS
+BEGIN
+    INSERT INTO test_tbl VALUES(p);
+    COMMIT;
+END;
+
+-- Outer procedure
+CREATE OR REPLACE PROCEDURE outer_proc()
+AS
+    i INT DEFAULT 1;
+BEGIN
+    WHILE i <= 5 LOOP
+        inner_proc(i);
+        i := i + 1;
+    END LOOP;
+
+    INSERT INTO test_tbl VALUES(999);
+    COMMIT;
+END;
+
+CALL outer_proc();
+SELECT * FROM test_tbl ORDER BY id;
+
+
 DROP PROCEDURE test_pl;
+DROP PROCEDURE inner_proc;
+DROP PROCEDURE outer_proc;
 DROP FUNCTION foo;
 DROP TABLE test_tbl;
 DROP TABLE ttt;
