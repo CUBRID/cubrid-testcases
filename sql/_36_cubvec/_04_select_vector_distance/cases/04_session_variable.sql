f3b074d25d8a8b98777c33f3235a2a84e37712e0
Hyung-Gyu <hgryoo@cubrid.com>
2025-09-16 15:48:29 +0900

    add comment, add DROP TABLE

--- sql/_36_cubvec/_04_select_vector_distance/cases/04_session_variable.sql
+++ sql/_36_cubvec/_04_select_vector_distance/cases/04_session_variable.sql
@@ -18,7 +18,11 @@ INSERT INTO tbl_sv VALUES (10, '[1,0,1]');
 INSERT INTO tbl_sv VALUES (11, '[2,3,4]');
 INSERT INTO tbl_sv VALUES (12, '[1,1,1]');
 
+-- Test 1: session variable is not initialized
 select /*+ recompile no_parallel_heap_scan */ id from tbl_sv order by vec <c> @a limit 5;
 
+-- Test 2: session variable is initialized
 set @a = (select vec from tbl_sv limit 1);
 select /*+ recompile no_parallel_heap_scan */ id from tbl_sv order by vec <c> @a limit 5;
+
+DROP TABLE tbl_sv;
