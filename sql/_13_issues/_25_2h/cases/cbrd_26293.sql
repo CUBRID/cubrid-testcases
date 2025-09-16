50e273df48fc1f2c345b4cb65f088639da8feb9f
zionyun <seeon0001@naver.com>
2025-09-16 09:33:48 +0900

    add reviews

--- sql/_13_issues/_25_2h/cases/cbrd_26293.sql
+++ sql/_13_issues/_25_2h/cases/cbrd_26293.sql
@@ -2,7 +2,7 @@
 -- ROWNUM incorrectly merged into subquery: wrong result can be returned.
 
 ----------------------------------------
--- Case 1: MAX variant
+-- Case 1: MAX with subquery ORDER BY
 ----------------------------------------
 select max(main.num)
   from (
@@ -23,7 +23,28 @@ select max(main.num)
  where rownum = 1;
 
 ----------------------------------------
--- Case 2: MIN variant (expanded scenario)
+-- Case 2: MAX with subquery ORDER BY desc
+----------------------------------------
+select max(maindesc.num)
+  from (
+        select sub.num
+          from (
+                select 3 as num from dual
+                union all
+                select 0 as num from dual
+                union all
+                select 1 as num from dual
+                union all
+                select 4 as num from dual
+                union all
+                select 2 as num from dual
+               ) sub
+         order by sub.num desc
+       ) maindesc
+ where rownum = 1;
+
+----------------------------------------
+-- Case 3: MIN with subquery ORDER BY 
 ----------------------------------------
 select min(main.num)
   from (
@@ -42,3 +63,38 @@ select min(main.num)
          order by sub.num asc
        ) main
  where rownum = 1;
+
+----------------------------------------
+-- Case 4: MIN with subquery ORDER BY desc
+----------------------------------------
+select min(maindesc.num)
+  from (
+        select sub.num
+          from (
+                select 3 as num from dual
+                union all
+                select 0 as num from dual
+                union all
+                select 1 as num from dual
+                union all
+                select 4 as num from dual
+                union all
+                select 2 as num from dual
+               ) sub
+         order by sub.num desc
+       ) maindesc
+ where rownum = 1;
+
+
+----------------------------------------
+-- Case 5: DISTINCT with subquery ORDER BY
+----------------------------------------
+select /*+ recompile */ distinct main.num2
+          from (select sub.num, sub.num2
+                    from (select 3 as num, 3 as num2 FROM dual
+                             union all
+                             select 0 as num, 0 as num2 FROM dual) sub
+                 order by sub.num asc) main
+where rownum = 1;
+
+
