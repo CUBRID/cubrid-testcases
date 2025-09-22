b422f168901e5556e818a013ef3cd94470b40920
zionyun <seeon0001@naver.com>
2025-09-22 17:45:45 +0900

    add scenario

--- sql/_33_elderberry/cbrd_24042/cbrd_26257/cases/cbrd_26257.sql
+++ sql/_33_elderberry/cbrd_24042/cbrd_26257/cases/cbrd_26257.sql
@@ -23,14 +23,23 @@ SELECT /*+ recompile */ * FROM ( SELECT ROWNUM rn, a.* FROM (select * from ta or
 evaluate '5. BETWEEN including NULL';
 SELECT /*+ recompile */ * FROM ( SELECT ROWNUM rn, a.* FROM (select * from ta order by cola) a ) Z WHERE rn BETWEEN NULL AND 10;
 
-evaluate '6. Combined with additional condition (AND)';
+evaluate '6. Combined with additional condition';
+SELECT /*+ recompile */ * FROM ( SELECT ROWNUM rn, a.* FROM (SELECT * FROM ta ORDER BY cola) a) Z WHERE colb % 2 = 0 AND rn BETWEEN 51 AND 60;
+
+evaluate '7. Combined with additional condition (between first)';
 SELECT /*+ recompile */ * FROM ( SELECT ROWNUM rn, a.* FROM (select * from ta order by cola) a ) Z WHERE rn BETWEEN 51 AND 60 AND colb % 2 = 0;
+
+evaluate '8. Two BETWEENs';
+SELECT /*+ recompile */ * FROM ( SELECT ROWNUM rn, a.* FROM (SELECT * FROM ta ORDER BY cola) a) Z WHERE rn BETWEEN 11 AND 20 AND cola BETWEEN 15 AND 18;
+
+evaluate '9. BETWEEN in WHERE + HAVING (groupby_num)';
+SELECT /*+ recompile */ cola FROM ( SELECT ROWNUM rn, a.* FROM (SELECT * FROM ta ORDER BY cola) a) Z WHERE rn BETWEEN 1 AND 20 GROUP BY cola DESC HAVING groupby_num() BETWEEN 2 AND 4;
 
 drop table if exists tb;
 create table tb (k int primary key);
 insert into tb select rownum from db_class limit 1000;
 
-evaluate '7. BETWEEN with join';
+evaluate '10. BETWEEN with join';
 SELECT /*+ recompile */ * FROM ( SELECT ROWNUM rn, a.* FROM (select ta.* from ta inner join tb on ta.cola = tb.k order by ta.cola) a) Z WHERE rn BETWEEN 1 AND 10;
 
 drop table if exists ta;
