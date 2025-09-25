dc26afb7bc01c4f5ce01b50640fac132abc9ee6d
LyhengCHHAY <lyhengchhay.tech@gmail.com>
2025-09-25 18:11:15 +0900

    added cleanup data

--- sql/_13_issues/_25_2h/cases/cbrd_25709.sql
+++ sql/_13_issues/_25_2h/cases/cbrd_25709.sql
@@ -1,4 +1,4 @@
--- Clean up
+-- Cleanup
 DROP TRIGGER IF EXISTS trg_a;
 DROP TABLE IF EXISTS tbl_a;
 
@@ -13,4 +13,11 @@ EXECUTE INSERT INTO tbl_a(col_a, col_b, depth) VALUES (1, 2, NEW.depth + 1);
 
 -- Trigger the recursion
 INSERT INTO tbl_a(col_a, col_b) VALUES (1, 1);
+
+-- Cleanup
+-- Drop the trigger first (so no deferred actions reference it anymore)
+DROP TRIGGER IF EXISTS trg_a;
+
+-- Then drop the table safely
+DROP TABLE IF EXISTS tbl_a;
 
