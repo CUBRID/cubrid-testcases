dd464762efffeb95484826b8e2a5bf08d50c8e55
zionyun <seeon0001@naver.com>
2026-01-19 13:57:04 +0900

    modify value

--- sql/_13_issues/_26_1h/cases/cbrd_26479.sql
+++ sql/_13_issues/_26_1h/cases/cbrd_26479.sql
@@ -7,7 +7,7 @@ create table u(i int, sv varchar(1024), sc char(1024));
 insert into u select rownum, repeat(rownum, 3), rownum from db_class a, db_class b, db_class c limit 100;
 
 -- Force an extremely small sort buffer to reproduce the previous crash scenario.
-set system parameters 'sort_buffer_size=1';
+set system parameters 'sort_buffer_size=16k';
 
 -- Previously, this query could raise an error or cause a core dump; it must complete successfully now.
 select /*+ parallel(4) */ i, sv, sc from u order by 1;
