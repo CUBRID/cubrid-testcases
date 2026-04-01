57b611061deecd7dc6e1bbc4fe763af75613908b
childyouth <39540545+childyouth@users.noreply.github.com>
2026-04-01 18:58:28 +0900

    [CBRD-26519] modify TC - auto_increment spec changed (#2505)
    * auto_increment spec changed
    
    * modify tc
    
    * remove expired tc

--- sql/_13_issues/_11_1h/cases/bug_bts_685.sql
+++ sql/_13_issues/_11_1h/cases/bug_bts_685.sql
@@ -12,14 +12,10 @@ select * from foo order by 1;
 
 alter table foo change b bb char(10) not null;
 alter table foo modify c monetary;
-alter table foo change c cc bigint auto_increment;
+alter table foo change c cc bigint;
 insert into foo(bb) values('ddd');
 select * from foo order by 1;
 
---[er]
-alter table foo auto_increment = 50;
-alter table foo modify cc bigint;
-alter table foo auto_increment = 50;
 insert into foo(bb, cc) values('hhh', 999);
 insert into foo(bb, cc) values('iii', 888);
 select * from foo order by 1;
