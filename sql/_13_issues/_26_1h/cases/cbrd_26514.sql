613b5afb5dc68aec00923587c72346bbd0d2ca66
Song won-ryong <38000047+swi0110@users.noreply.github.com>
2026-05-21 13:57:25 +0900

    [CBRD-26514] Add test case, fixed the error the procedure or function when create (11.5) (#2774)
    Refer to http://jira.cubrid.org/browse/CBRD-26514, http://jira.cubrid.org/browse/CBRD-26578
    
    이 PR은 2개의 issue를 함께 검증합니다.
    이는 CBRD-26578의 수정 사항을 검증하기 위서는 CBRD-26514가 선행되어야 하기 때문입니다.
    (참고: http://jira.cubrid.org/browse/CBRD-26578?focusedCommentId=4773751&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-4773751 )
    
    또한 이 tc는 https://github.com/CUBRID/cubrid-testcases-private-ex/pull/3095 sql test로 재구현한 내용입니다.
    때문에 이미 approved한 개발팀과 PM팀은 reviewer로 지정하지 않았습니다.
    
    
    **CBRD-26578**
    As-is
    - procedure 생성 시 create 구문 앞에 공백 문자가 있을 경우, body text가 잘못 저장 되어 procedure가 비정상적으로 생성 됨
    
    To-be
    - 공백 문자 허용 및 _db_stored_procedure_code 테이블의 scode에 body text가 정상적으로 저장되어, procedure가 정상적으로 생성되도록 수정
    
    
    **CBRD-26514**
    As-is
    - create procedure 혹은 function 시 <user>.<sp_name>로 owner를 지정한 후, alter procedure 혹은 function에서 owner를 수정할 때 internal error 발생
    
    To-be
    - ALTER 에 의해 변경될 수 있는 SP 의 owner와 comment 를 _db_stored_procedure_code.scode 에 포함되지 않도록 수정
    
    
    기타: 11.4 backport 필요

---
+++ sql/_13_issues/_26_1h/cases/cbrd_26514.sql
@@ -0,0 +1,116 @@
+/**
+ * This test case verifies CBRD-26514 (with CBRD-26578):
+ *  - CBRD-26514: When a stored procedure is created with an owner-qualified
+ *    name (e.g., dba.sp), ALTER ... OWNER TO failed with internal error
+ *    because _db_stored_procedure_code.scode embedded the original owner
+ *    name. Fix excludes the owner name and COMMENT clause from scode.
+ *  - CBRD-26578: SP body offset was miscalculated when whitespace appeared
+ *    before CREATE, causing the body text to be incorrectly stored.
+ *
+ * Coverage:
+ * 1 - Owner name is suppressed from scode
+ * 2 - COMMENT clause is suppressed from scode and kept in _db_stored_procedure.comment
+ * 3 - Combined owner + COMMENT suppression
+ * 4 - ALTER PROCEDURE OWNER TO works for owner-qualified SP (CBRD-26514 repro)
+ * 5 - Leading whitespace before CREATE keyword executes correctly (CBRD-26578)
+ * 6 - Leading whitespace + owner + COMMENT combined
+ */
+
+--+ server-message on
+
+create or replace procedure dba.sp_owner_test as
+begin
+    dbms_output.put_line('owner_test');
+end;
+
+create or replace procedure sp_comment_test as
+begin
+    dbms_output.put_line('comment_test');
+end
+comment 'this is a test comment';
+
+create or replace procedure dba.sp_combined_test(p_val int) as
+    v_result varchar(100);
+begin
+    v_result := 'combined_test: ' || p_val;
+    dbms_output.put_line(v_result);
+end
+comment 'combined test comment';
+
+evaluate 'Case 1: owner name is suppressed from scode';
+select case when locate('dba.sp_owner_test', code.scode) = 0
+            then 'OWNER_SUPPRESSED' else 'OWNER_LEAKED' end as result
+from _db_stored_procedure sp, _db_stored_procedure_code code
+where sp.unique_name = 'dba.sp_owner_test'
+  and sp.target_class = code.name
+  and sp.owner.name = code.owner.name;
+
+evaluate 'Case 2-1: COMMENT clause is suppressed from scode';
+select case when locate('this is a test comment', code.scode) = 0
+            then 'COMMENT_SUPPRESSED' else 'COMMENT_LEAKED' end as result
+from _db_stored_procedure sp, _db_stored_procedure_code code
+where sp.unique_name = 'dba.sp_comment_test'
+  and sp.target_class = code.name
+  and sp.owner.name = code.owner.name;
+
+evaluate 'Case 2-2: comment is stored in _db_stored_procedure.comment';
+select comment from _db_stored_procedure where unique_name = 'dba.sp_comment_test';
+
+evaluate 'Case 3: owner and COMMENT combined suppression in scode';
+select case when locate('dba.sp_combined_test', code.scode) = 0
+              and locate('combined test comment', code.scode) = 0
+            then 'BOTH_SUPPRESSED' else 'LEAKED' end as result
+from _db_stored_procedure sp, _db_stored_procedure_code code
+where sp.unique_name = 'dba.sp_combined_test'
+  and sp.target_class = code.name
+  and sp.owner.name = code.owner.name;
+
+evaluate 'Case 4: ALTER PROCEDURE OWNER TO must succeed without internal error';
+alter procedure dba.sp_owner_test owner to public;
+select owner.name from _db_stored_procedure where unique_name = 'public.sp_owner_test';
+
+evaluate 'Case 5-1: SP with leading spaces before CREATE executes correctly';
+    create or replace procedure sp_leading_space as
+begin
+    dbms_output.put_line('leading_space_ok');
+end;
+call sp_leading_space();
+
+evaluate 'Case 5-2: SP with leading tabs before CREATE executes correctly';
+		create or replace procedure sp_leading_tab as
+begin
+    dbms_output.put_line('leading_tab_ok');
+end;
+call sp_leading_tab();
+
+evaluate 'Case 6: leading whitespace + owner + COMMENT executes correctly';
+	  create or replace procedure dba.sp_mixed_ws(p_input varchar) as
+    v_out varchar(200);
+begin
+    v_out := 'mixed_ws: ' || p_input;
+    dbms_output.put_line(v_out);
+end
+comment 'mixed whitespace test';
+call sp_mixed_ws('hello');
+
+select case when locate('dba.sp_mixed_ws', code.scode) = 0
+              and locate('mixed whitespace test', code.scode) = 0
+            then 'BOTH_SUPPRESSED' else 'LEAKED' end as result
+from _db_stored_procedure sp, _db_stored_procedure_code code
+where sp.unique_name = 'dba.sp_mixed_ws'
+  and sp.target_class = code.name
+  and sp.owner.name = code.owner.name;
+
+
+evaluate 'excption, when fail a case 4';
+drop procedure dba.sp_owner_test;
+evaluate 'when pass a case 4';
+drop procedure public.sp_owner_test;
+
+drop procedure sp_comment_test;
+drop procedure sp_combined_test;
+drop procedure sp_leading_space;
+drop procedure sp_leading_tab;
+drop procedure sp_mixed_ws;
+
+--+ server-message off
