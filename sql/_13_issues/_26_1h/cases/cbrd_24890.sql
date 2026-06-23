9e1fe6661d79cded906dcf0be242c85e6ed16801
oychanphallin <oychanphallin.tech@gmail.com>
2026-06-23 16:18:20 +0900

    reflected reviews

--- sql/_13_issues/_26_1h/cases/cbrd_24890.sql
+++ sql/_13_issues/_26_1h/cases/cbrd_24890.sql
@@ -3,27 +3,27 @@
  *  This test case verifies that min()/max() are executed via index scan instead of full aggregate processing.
  */
 
-drop table if exists t2;
+drop table if exists tbl;
 
-create table t2 (
-  col1 int,
-  col2 int,
-  col3 varchar(100)
+create table tbl (
+  cola int,
+  colb int,
+  colc varchar(100)
 );
 
-insert into t2
+insert into tbl
 select
-  mod(rownum - 1, 20) + 1 as col1,
-  (rownum - 1) * 10 + 100 as col2,
+  mod(rownum - 1, 20) + 1 as cola,
+  (rownum - 1) * 10 + 100 as colb,
   concat('v', rownum)
 from db_class a, db_class b
 where rownum <= 500;
 
 -- Index combinations
-create index idx_col1_col2_asc_asc on t2(col1 asc, col2 asc);
-create index idx_col1_col2_asc_desc on t2(col1 asc, col2 desc);
-create index idx_col1_col2_desc_asc on t2(col1 desc, col2 asc);
-create index idx_col1_col2_desc_desc on t2(col1 desc, col2 desc);
+create index idx_cola_colb_asc_asc on tbl(cola asc, colb asc);
+create index idx_cola_colb_asc_desc on tbl(cola asc, colb desc);
+create index idx_cola_colb_desc_asc on tbl(cola desc, colb asc);
+create index idx_cola_colb_desc_desc on tbl(cola desc, colb desc);
 set trace on;
 
 -- ============================================================================
@@ -31,19 +31,19 @@ set trace on;
 -- ============================================================================
 
 evaluate 'Case 1';
-select /*+ recompile */ min(col2), max(col2) from t2 FORCE INDEX (idx_col1_col2_desc_asc) where col1 = 1;
+select /*+ recompile */ min(colb), max(colb) from tbl FORCE INDEX (idx_cola_colb_desc_asc) where cola = 1;
 show trace;
 
 evaluate 'Case 2';
-select /*+ recompile */ max(col2), min(col2) from t2 FORCE INDEX (idx_col1_col2_asc_asc) where col1 = 1;
+select /*+ recompile */ max(colb), min(colb) from tbl FORCE INDEX (idx_cola_colb_asc_asc) where cola = 1;
 show trace;
 
 evaluate 'Case 3';
-select /*+ recompile */ min(col2), max(col2) from t2 FORCE INDEX (idx_col1_col2_desc_desc) where col1 = 1;
+select /*+ recompile */ min(colb), max(colb) from tbl FORCE INDEX (idx_cola_colb_desc_desc) where cola = 1;
 show trace;
 
 evaluate 'Case 4';
-select /*+ recompile */ max(col2), min(col2) from t2 FORCE INDEX (idx_col1_col2_desc_asc) where col1 = 1;
+select /*+ recompile */ max(colb), min(colb) from tbl FORCE INDEX (idx_cola_colb_desc_asc) where cola = 1;
 show trace;
 
 -- ============================================================================
@@ -51,15 +51,15 @@ show trace;
 -- ============================================================================
 
 evaluate 'Case 5';
-select /*+ recompile */ min(col2) from t2 where col1 = 1 and col2 > 200;
+select /*+ recompile */ min(colb) from tbl where cola = 1 and colb > 200;
 show trace;
 
 evaluate 'Case 6';
-select /*+ recompile */ max(col2) from t2 where col1 = 1 and col2 < 500;
+select /*+ recompile */ max(colb) from tbl where cola = 1 and colb < 500;
 show trace;
 
 evaluate 'Case 7';
-select /*+ recompile */ min(col2), max(col2) from t2 FORCE INDEX (idx_col1_col2_desc_asc) where col1 = 1;
+select /*+ recompile */ min(colb), max(colb) from tbl FORCE INDEX (idx_cola_colb_asc_desc) where cola = 1;
 show trace;
 
 -- ============================================================================
@@ -67,11 +67,11 @@ show trace;
 -- ============================================================================
 
 evaluate 'Case 8';
-select /*+ recompile index(t2 idx_col1_col2_asc_asc) */ min(col2) from t2 where col1 > 1;
+select /*+ recompile index(tbl idx_cola_colb_asc_asc) */ min(colb) from tbl where cola > 1;
 show trace;
 
 evaluate 'Case 9';
-select /*+ recompile */ min(col2), max(-col2) from t2 FORCE INDEX (idx_col1_col2_desc_asc) where col1 = 1;
+select /*+ recompile */ min(colb), max(-colb) from tbl FORCE INDEX (idx_cola_colb_desc_asc) where cola = 1;
 show trace;
 
 -- ============================================================================
@@ -79,15 +79,15 @@ show trace;
 -- ============================================================================
 
 evaluate 'Case 10';
-select /*+ recompile */ min(col2) from t2 where col3 like 'v1%';
+select /*+ recompile */ min(colb) from tbl where colc like 'v1%';
 show trace;
 
 -- ============================================================================
--- Not optimizable: no col1 condition
+-- Not optimizable: no cola condition
 -- ============================================================================
 
 evaluate 'Case 11';
-select /*+ recompile */ min(col2) from t2;
+select /*+ recompile */ min(colb) from tbl;
 show trace;
 
 -- ============================================================================
@@ -95,42 +95,42 @@ show trace;
 -- ============================================================================
 
 evaluate 'Case 12';
-select /*+ recompile */ (select min(col2) from t2 where col1 = a.col1) from t2 a limit 1;
+select /*+ recompile */ (select min(colb) from tbl where cola = a.cola) from tbl a limit 1;
 show trace;
 
 evaluate 'Case 13';
-select /*+ recompile */(select max(col2) from t2 where col1 = a.col1) from t2 a limit 1;
+select /*+ recompile */(select max(colb) from tbl where cola = a.cola) from tbl a limit 1;
 show trace;
 
 evaluate 'Case 14';
-select /*+ recompile */(select min(col2) from t2 where col1 = a.col1 and col3 like 'v1%') from t2 a limit 1;
+select /*+ recompile */(select min(colb) from tbl where cola = a.cola and colc like 'v1%') from tbl a limit 1;
 show trace;
 
 set trace off;
 
-drop table if exists t2_part;
+drop table if exists tbl_part;
 
-create table t2_part (
-  col1 int,
-  col2 int,
-  col3 varchar(100)
+create table tbl_part (
+  cola int,
+  colb int,
+  colc varchar(100)
 )
-partition by range (col1) (
-  partition p1 values less than (6),
-  partition p2 values less than (11),
-  partition p3 values less than (16),
-  partition p4 values less than (21)
+partition by range (cola) (
+  partition pa values less than (6),
+  partition pb values less than (11),
+  partition pc values less than (16),
+  partition pd values less than (21)
 );
 
-insert into t2_part
+insert into tbl_part
 select
-  mod(rownum - 1, 20) + 1 as col1,
-  (rownum - 1) * 10 + 100 as col2,
+  mod(rownum - 1, 20) + 1 as cola,
+  (rownum - 1) * 10 + 100 as colb,
   concat('v', rownum)
 from db_class a, db_class b
 where rownum <= 500;
-create index idx_part_col1_col2_asc on t2_part(col1 asc, col2 asc);
-create index idx_part_col1_col2_desc on t2_part(col1 desc, col2 desc);
+create index idx_part_cola_colb_asc on tbl_part(cola asc, colb asc);
+create index idx_part_cola_colb_desc on tbl_part(cola desc, colb desc);
 set trace on;
 
 -- ============================================================================
@@ -138,9 +138,9 @@ set trace on;
 -- ============================================================================
 
 evaluate 'Case 15';
-select /*+ recompile */ min(col2), max(col2) from t2_part where col1 = 1;
+select /*+ recompile */ min(colb), max(colb) from tbl_part where cola = 1;
 show trace;
 
 set trace off;
-drop table t2;
-drop table t2_part;
+drop table tbl;
+drop table tbl_part;
