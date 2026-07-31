297c72cab949569b2f15a0cb728bbe2c9d7c5672
sjkimxxx <sjkim@cubrid.com>
2026-07-31 18:13:42 +0900

    ssihil's review - Remove numeric suffixes from table/column names

--- sql/_36_guava/cbrd_26345/cases/cbrd_26345.sql
+++ sql/_36_guava/cbrd_26345/cases/cbrd_26345.sql
@@ -6,11 +6,11 @@
 -- (different NDV ratios) under enabled/disabled memory limit settings.
 
 -- cleanup
-drop table if exists m1;
-drop table if exists m2;
+drop table if exists outer_tbl;
+drop table if exists inner_tbl;
 
-create table m1 (col1 int, col2 int, col3 int, col4 varchar(20), col5 numeric(20,10), col6 varchar(20), col7 varchar(20), col8 varchar(20));
-insert into m1 select
+create table outer_tbl (uniq_int int, low_ndv_int int, mid_ndv_int int, mid_ndv_varchar varchar(20), high_ndv_numeric numeric(20,10), uniq_varchar varchar(20), half_ndv_varchar varchar(20), third_ndv_varchar varchar(20));
+insert into outer_tbl select
   rownum,
   rownum % 10,
   rownum % 100,
@@ -22,56 +22,56 @@ insert into m1 select
 from db_class a, db_class b, db_class c, db_class d, db_class e, db_class f, db_class g
 limit 100000;
 
-create table m2 (col1 int);
-insert into m2 select rownum from db_class a, db_class b, db_class c, db_class d, db_class e, db_class f, db_class g limit 10;
-update statistics on m1, m2 with fullscan;
+create table inner_tbl (join_key int);
+insert into inner_tbl select rownum from db_class a, db_class b, db_class c, db_class d, db_class e, db_class f, db_class g limit 10;
+update statistics on outer_tbl, inner_tbl with fullscan;
 
 evaluate 'memoize enabled (memoize_memory_limit=64M)';
 
 set trace on;
 set system parameters 'memoize_memory_limit=64M';
 
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col1 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.uniq_int = inner_tbl.join_key;
 show trace;
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col2 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.low_ndv_int = inner_tbl.join_key;
 show trace;
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col3 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.mid_ndv_int = inner_tbl.join_key;
 show trace;
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col4 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.mid_ndv_varchar = inner_tbl.join_key;
 show trace;
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col5 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.high_ndv_numeric = inner_tbl.join_key;
 show trace;
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col6 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.uniq_varchar = inner_tbl.join_key;
 show trace;
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col7 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.half_ndv_varchar = inner_tbl.join_key;
 show trace;
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col8 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.third_ndv_varchar = inner_tbl.join_key;
 show trace;
 
 evaluate 'memoize disabled (memoize_memory_limit=0)';
 
 set system parameters 'memoize_memory_limit=0';
 
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col1 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.uniq_int = inner_tbl.join_key;
 show trace;
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col2 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.low_ndv_int = inner_tbl.join_key;
 show trace;
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col3 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.mid_ndv_int = inner_tbl.join_key;
 show trace;
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col4 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.mid_ndv_varchar = inner_tbl.join_key;
 show trace;
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col5 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.high_ndv_numeric = inner_tbl.join_key;
 show trace;
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col6 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.uniq_varchar = inner_tbl.join_key;
 show trace;
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col7 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.half_ndv_varchar = inner_tbl.join_key;
 show trace;
-select /*+ recompile parallel(0) */ count(*) from m1 inner join m2 on m1.col8 = m2.col1;
+select /*+ recompile parallel(0) */ count(*) from outer_tbl inner join inner_tbl on outer_tbl.third_ndv_varchar = inner_tbl.join_key;
 show trace;
 
 set trace off;
 set system parameters 'memoize_memory_limit=default';
 
 -- cleanup
-drop table if exists m1;
-drop table if exists m2;
+drop table if exists outer_tbl;
+drop table if exists inner_tbl;
