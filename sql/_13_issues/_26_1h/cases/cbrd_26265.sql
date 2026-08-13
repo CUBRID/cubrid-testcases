f318522c6c97fe8bd7779807c88f88fc89cbf167
oychanphallin <oychanphallin.tech@gmail.com>
2026-08-13 19:24:40 +0900

    added new cases

--- sql/_13_issues/_26_1h/cases/cbrd_26265.sql
+++ sql/_13_issues/_26_1h/cases/cbrd_26265.sql
@@ -6,34 +6,40 @@
  *      1. IS NOT NULL on NOT NULL column
  *      2. IS NOT NULL with additional filter
  *      3. IS NOT NULL in LIKE predicate
- *      4. IS NOT NULL in LIKE predicate with variable binding
+ *      4. IS NOT NULL in LIKE predicate with literal "?"
  *      5. IS NOT NULL in complex LIKE expression
- *      6. IS NOT NULL in complex LIKE expression with variable binding
+ *      6. IS NOT NULL in complex LIKE expression with literal "?"
  *      7. IS NOT NULL + complex LIKE on NOT NULL column (regression from review)
  *      8. OR combination (should not break logic)
- *      9. not null column, is not null
- *      10. not null column, like ''%''
- *      11. not null column, like ''%'' || '''' || ''%''
- *      12. type mismatch, not null column, is not null
- *      13. type mismatch, not null column, like ''%''
- *      14. type mismatch, not null column, like ''%'' || '''' || ''%''
- *      15. pk column, is not null
- *      16. pk column, like ''%''
- *      17. pk column, like ''%'' || '''' || ''%''
- *      18. join query, is not null
+ *      9. type mismatch, not null column, is not null
+ *      10. type mismatch, not null column, like ''%''
+ *      11. type mismatch, not null column, like ''%'' || '''' || ''%''
+ *      12. pk column, is not null
+ *      13. pk column, like ''%''
+ *      14. pk column, like ''%'' || '''' || ''%''
+ *      15. join query, is not null
+ *      16. not null column, like bind parameter, hostvar_peeking=no
+ *      17. not null column, like ''%'' || bind parameter || ''%'', hostvar_peeking=no
+ *      18. not null column, like bind parameter, hostvar_peeking=yes
+ *      19. not null column, like ''%'' || bind parameter || ''%'', hostvar_peeking=yes
+ *      20. type mismatch, not null column, like bind parameter, hostvar_peeking=yes
+ *      21. type mismatch, not null column, like ''%'' || bind parameter || ''%'', hostvar_peeking=yes
+ *      22. pk column, like bind parameter, hostvar_peeking=yes
+ *      23. pk column, like ''%'' || bind parameter || ''%'', hostvar_peeking=yes
+ *      24. join query, like ''%'' || bind parameter || ''%'', hostvar_peeking=yes
  */
 set trace on;
 
 evaluate concat('1. IS NOT NULL on NOT NULL column');
 drop table if exists tbl_w;
 create table tbl_w (
-    col1 int not null,
-    col2 int
+    cola int not null,
+    colb int
 );
 
-insert into tbl_w (col1, col2) values(1, 100);
+insert into tbl_w (cola, colb) values(1, 100);
 
-select * from tbl_w where col1 is not null;
+select * from tbl_w where cola is not null;
 show trace;
 
 evaluate concat('2. IS NOT NULL with additional filter');
@@ -64,7 +70,7 @@ insert into tbl_y (c, d, e) values(3, null, 'test');
 select * from tbl_y where e like '%';
 show trace;
 
-evaluate concat('4. IS NOT NULL in LIKE predicate with variable binding');
+evaluate concat('4. IS NOT NULL in LIKE predicate with literal "?"');
 select * from tbl_y where e like '?';
 show trace;
 
@@ -83,115 +89,174 @@ insert into tbl_z (x, y, z) values(3, null, 'test');
 select * from tbl_z where z like '%' || '' || '%';
 show trace;
 
-evaluate concat('6. IS NOT NULL in complex LIKE expression with variable binding');
+evaluate concat('6. IS NOT NULL in complex LIKE expression with literal "?"');
 select * from tbl_z where z like '?' || '' || '?';
 show trace;
 
 evaluate concat('7. IS NOT NULL + complex LIKE on NOT NULL column (regression from review)');
 drop table if exists tbl_r;
 create table tbl_r (
-    col1 varchar(20) not null,
-    col2 int
+    cola varchar(20) not null,
+    colb int
 );
 
 insert into tbl_r values ('a', 1);
 insert into tbl_r values ('b', 2);
 
-select /*+ recompile */ * from tbl_r where (col1 is not null and col1 like '%' || '' || '%');
+select /*+ recompile */ * from tbl_r where (cola is not null and cola like '%' || '' || '%');
 show trace;
 
 evaluate concat('8. OR combination (should not break logic)');
 drop table if exists tbl_or;
 create table tbl_or (
-    col1 int not null, 
-    col2 int
+    cola int not null, 
+    colb int
 );
 
 insert into tbl_or values (1, null);
 insert into tbl_or values (2, 10);
 
-select /*+ recompile */ * from tbl_or where (col1 is not null or col2 = 10);
+select /*+ recompile */ * from tbl_or where (cola is not null or colb = 10);
 show trace;
 
-set optimization level 513;
-
-drop table if exists t1;
-create table t1(
-    col1 varchar not null
+drop table if exists tbl_b;
+create table tbl_b(
+    cola int not null
 );
 
-insert into t1 values ('a');
+insert into tbl_b values (1);
 
-evaluate concat('9. not null column, is not null');
-select /*+ recompile */ * from t1 where col1 is not null;
+evaluate concat('9. type mismatch, not null column, is not null');
+select /*+ recompile */ * from tbl_b where cola is not null;
 show trace;
 
-evaluate concat('10. not null column, like ''%''');
-select /*+ recompile */ * from t1 where col1 like '%';
+evaluate concat('10. type mismatch, not null column, like ''%''');
+select /*+ recompile */ * from tbl_b where cola like '%';
 show trace;
 
-evaluate concat('11. not null column, like ''%'' || '''' || ''%''');
-select /*+ recompile */ * from t1 where col1 like '%' || '' || '%';
+evaluate concat('11. type mismatch, not null column, like ''%'' || '''' || ''%''');
+select /*+ recompile */ * from tbl_b where cola like '%' || '' || '%';
 show trace;
 
-drop table if exists t2;
-create table t2(
-    col1 int not null
+drop table if exists tbl_a;
+create table tbl_a (
+    cola varchar, 
+    primary key(cola)
 );
 
-insert into t2 values (1);
+insert into tbl_a (cola) values ('abc');
+insert into tbl_a (cola) values ('def');
 
-evaluate concat('12. type mismatch, not null column, is not null');
-select /*+ recompile */ * from t2 where col1 is not null;
+evaluate concat('12. pk column, is not null');
+select /*+ recompile */ * from tbl_a where cola is not null;
 show trace;
 
-evaluate concat('13. type mismatch, not null column, like ''%''');
-select /*+ recompile */ * from t2 where col1 like '%';
+evaluate concat('13. pk column, like ''%''');
+select /*+ recompile */ * from tbl_a where cola like '%';
 show trace;
 
-evaluate concat('14. type mismatch, not null column, like ''%'' || '''' || ''%''');
-select /*+ recompile */ * from t2 where col1 like '%' || '' || '%';
+evaluate concat('14. pk column, like ''%'' || '''' || ''%''');
+select /*+ recompile */ * from tbl_a where cola like '%' || '' || '%';
 show trace;
 
-drop table if exists t1;
-create table t1 (
-    col1 varchar, 
-    primary key(col1)
+evaluate concat('15. join query, is not null');
+drop table if exists tbl_a, tbl_b;
+create table tbl_a (
+    cola varchar(20) not null,
+    colb int
 );
 
-insert into t1 (col1) values ('abc');
-insert into t1 (col1) values ('def');
+insert into tbl_a values ('abc', 1);
+insert into tbl_a values ('def', 2);
 
-evaluate concat('15. pk column, is not null');
-select /*+ recompile */ * from t1 where col1 is not null;
-show trace;
-
-evaluate concat('16. pk column, like ''%''');
-select /*+ recompile */ * from t1 where col1 like '%';
-show trace;
-
-evaluate concat('17. pk column, like ''%'' || '''' || ''%''');
-select /*+ recompile */ * from t1 where col1 like '%' || '' || '%';
-show trace;
-
-evaluate concat('18. join query, is not null');
-drop table if exists t1, t2;
-create table t1 (
-    col1 varchar(20) not null,
-    col2 int
+create table tbl_b(
+    cola int
 );
 
-insert into t1 values ('abc', 1);
-insert into t1 values ('def', 2);
+insert into tbl_b (cola) values (111);
+insert into tbl_b (cola) values (222);
 
-create table t2(
-    col1 int
+select /*+ recompile */ * from tbl_a left outer join tbl_b on tbl_a.colb = tbl_b.cola and tbl_a.cola like '%' || '' || '%';
+show trace;
+
+drop table if exists tbl_a, tbl_b;
+create table tbl_a (
+    cola varchar not null
 );
 
-insert into t2 (col1) values (111);
-insert into t2 (col1) values (222);
+SET SYSTEM PARAMETERS 'hostvar_peeking=no';
 
-select /*+ recompile */ * from t1 left outer join t2 on t1.col2 = t2.col1 and t1.col1 like '%' || '' || '%';
+prepare q1 from 'select /*+ recompile */ * from tbl_a where cola like ?';
+prepare q2 from 'select /*+ recompile */ * from tbl_a where cola like ''%'' || ? || ''%''';
+
+evaluate concat('16. NOT NULL column with LIKE bind parameter, hostvar_peeking=no');
+execute q1 using '%';
+show trace;
+
+evaluate concat('17. NOT NULL column with complex LIKE bind parameter, hostvar_peeking=no');
+execute q2 using '';
+show trace;
+
+SET SYSTEM PARAMETERS 'hostvar_peeking=yes';
+
+evaluate concat('18. NOT NULL column with LIKE bind parameter, hostvar_peeking=yes');
+execute q1 using '%';
+show trace;
+
+evaluate concat('19. NOT NULL column with complex LIKE bind parameter, hostvar_peeking=yes');
+execute q2 using '';
+show trace;
+
+drop table if exists tbl_b;
+
+create table tbl_b (
+    cola int not null
+);
+
+prepare q1 from 'select /*+ recompile */ * from tbl_b where cola like ?';
+prepare q2 from 'select /*+ recompile */ * from tbl_b where cola like ''%'' || ? || ''%''';
+
+evaluate concat('20. Type mismatch with LIKE bind parameter, hostvar_peeking=yes');
+execute q1 using '%';
+show trace;
+
+evaluate concat('21. Type mismatch with complex LIKE bind parameter, hostvar_peeking=yes');
+execute q2 using '';
+show trace;
+
+drop table if exists tbl_a;
+
+create table tbl_a (
+    cola varchar,
+    primary key (cola)
+);
+
+prepare q1 from 'select /*+ recompile */ * from tbl_a where cola like ?';
+prepare q2 from 'select /*+ recompile */ * from tbl_a where cola like ''%'' || ? || ''%''';
+
+evaluate concat('22. Primary key column with LIKE bind parameter, hostvar_peeking=yes');
+execute q1 using '%';
+show trace;
+
+evaluate concat('23. Primary key column with complex LIKE bind parameter, hostvar_peeking=yes');
+execute q2 using '';
+show trace;
+
+drop table if exists tbl_a, tbl_b;
+
+create table tbl_a (
+    cola varchar not null,
+    colb int
+);
+
+create table tbl_b (
+    cola int
+);
+
+prepare q1 from 'select /*+ recompile */ * from tbl_a left outer join tbl_b on tbl_a.colb = tbl_b.cola and tbl_a.cola like ''%'' || ? || ''%''';
+
+evaluate concat('24. Join query with complex LIKE bind parameter, hostvar_peeking=yes');
+execute q1 using '';
 show trace;
 
 -- cleanup
@@ -200,7 +265,7 @@ drop table tbl_x;
 drop table tbl_y;
 drop table tbl_z;
 drop table tbl_r;
-drop table t1;
-drop table t2;
 drop table tbl_or;
+drop table tbl_a;
+drop table tbl_b;
 set trace off;
