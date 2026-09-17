0ab0643da7c74390e2e6c20596b09741544f3396
SOHEE_JUNG <soheejung@cubrid.com>
2026-09-17 19:54:21 +0900

    Revert "[CBRD-27043] Add db_histogram system view test cases (name columns, SELECT-privilege filter)"
    This reverts commit 4d1669aa1ac0538d9b8f2af12ae7c6bc1683cd28.

--- sql/_01_object/_10_system_table/_023_db_histogram/cases/1003.sql
+++
@@ -1,3 +0,0 @@
--- CBRD-27043: db_histogram column domains -- class_name is a string, not an object,
--- and null_frequency is NUMERIC(18,12) as the view spec casts it
-select attr_name, data_type, prec, scale from db_attribute where class_name = 'db_histogram' order by def_order;
